67940bc1d49456eaa088fedcd876b652aa2f4f13
Carl Yeksigian <carl@yeksigian.com>
2014-08-28 15:31:23 -0400

    [DEV-1361] Adding ontology service for searching
    - This uses ElasticSearch as its backing store
    - The queries are similar, but not identical, to the queries used by the
      data-use-prototype
    - Added an ES health check; it gets registered late, so the healthcheck
      warning can be safely ignored

---
+++ consent-autocomplete/src/main/java/org/genomebridge/consent/autocomplete/AutocompleteApplication.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright 2014 Broad Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.genomebridge.consent.autocomplete;
+
+import com.hubspot.dropwizard.guice.GuiceBundle;
+import io.dropwizard.Application;
+import io.dropwizard.ConfiguredBundle;
+import io.dropwizard.assets.AssetsBundle;
+import io.dropwizard.db.DataSourceFactory;
+import io.dropwizard.migrations.MigrationsBundle;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import org.genomebridge.consent.autocomplete.resources.AllTermsResource;
+import org.genomebridge.consent.autocomplete.resources.TermResource;
+
+/**
+ * Top-level entry point to the entire application.
+ *
+ * See the Dropwizard docs here:
+ *   https://dropwizard.github.io/dropwizard/manual/core.html
+ *
+ */
+public class AutocompleteApplication extends Application<AutocompleteConfiguration> {
+
+    public static void main(String[] args) throws Exception {
+        new AutocompleteApplication().run(args);
+    }
+
+    public void run(AutocompleteConfiguration config, Environment env) {
+        env.jersey().register(AllTermsResource.class);
+    }
+
+    public void initialize(Bootstrap<AutocompleteConfiguration> bootstrap) {
+
+        GuiceBundle<AutocompleteConfiguration> guiceBundle = GuiceBundle.<AutocompleteConfiguration>newBuilder()
+                .addModule(new AutocompleteModule())
+                .setConfigClass(AutocompleteConfiguration.class)
+                .build();
+
+        bootstrap.addBundle(guiceBundle);
+
+        bootstrap.addBundle(new AssetsBundle("/assets/", "/site"));
+    }
+}
---
+++ consent-autocomplete/src/main/java/org/genomebridge/consent/autocomplete/resources/TermResource.java
@@ -0,0 +1,10 @@
+package org.genomebridge.consent.autocomplete.resources;
+
+import java.util.List;
+
+public class TermResource {
+    public String id;
+    public String label;
+    public String definition;
+    public List<String> synonyms;
+}
