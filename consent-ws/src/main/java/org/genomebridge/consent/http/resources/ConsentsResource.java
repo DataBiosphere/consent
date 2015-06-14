37b803e8545f16181056abab316a53c95e7b5429
Greg Rushton <grushton@broadinstitute.org>
2015-06-14 15:38:46 -0400

    Add validation on user-supplied consent ids.

--- consent-ws/src/main/java/org/genomebridge/consent/http/resources/ConsentsResource.java
+++ consent-ws/src/main/java/org/genomebridge/consent/http/resources/ConsentsResource.java
@@ -3,10 +3,10 @@ package org.genomebridge.consent.http.resources;
 import com.google.common.base.Optional;
 import com.sun.jersey.api.NotFoundException;
 import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
 import org.genomebridge.consent.http.models.Consent;
 import org.genomebridge.consent.http.service.AbstractConsentAPI;
 import org.genomebridge.consent.http.service.ConsentAPI;
-import org.genomebridge.consent.http.service.UnknownIdentifierException;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -28,7 +28,20 @@ public class ConsentsResource extends Resource {
     @Produces("application/json")
     public Collection<Consent> retrieve(@QueryParam("ids") Optional<String> ids) {
         if (ids.isPresent()) {
-            List<String> splitIds = Arrays.asList(ids.get().split(","));
+            List<String> splitIds = new ArrayList<>();
+            for (String id : Arrays.asList(ids.get().split(","))) {
+                try {
+                    //noinspection ResultOfMethodCallIgnored
+                    UUID.fromString(id);
+                    splitIds.add(id);
+                } catch (IllegalArgumentException e) {
+                    logger().error("Invalid id: " + id);
+                }
+            }
+            if (splitIds.isEmpty()) {
+                logger().error("Unable to parse ids from provided consent ids: " + ids);
+                throw new NotFoundException("Cannot find some consents ids: " + ids);
+            }
             Collection<Consent> consents = api.retrieve(splitIds);
             List<String> foundIds = new ArrayList<>();
             for (Consent consent: consents) {
@@ -46,6 +59,11 @@ public class ConsentsResource extends Resource {
         else {
             throw new NotFoundException("Cannot find any consents without ids");
         }
+    }
+
+    @Override
+    protected Logger logger() {
+        return Logger.getLogger("ConsentsResource");
     }
 
 }
