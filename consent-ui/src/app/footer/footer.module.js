0cd1f367d827b61f4431aa56370e7810cccb6f0a
Nadya <nlopez@broadinstitute.org>
2015-08-11 17:20:15 -0300

    Landing Page UI

---
+++ consent-ui/src/app/footer/footer.module.js
@@ -0,0 +1,7 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('cmFooter', ['ui.bootstrap','cmUserRoles']);
+
+})();
---
+++ consent-ui/src/app/footer/footer.spec.js
@@ -0,0 +1,19 @@
+(function () {
+    'use strict';
+
+    describe('cmFooter module', function () {
+
+        beforeEach(module('cmFooter'));
+
+        describe('Footer controller', function () {
+
+            it('should ....', inject(function ($controller) {
+                //spec body
+                var footerCtrl = $controller('Footer');
+                expect(footerCtrl).toBeDefined();
+            }));
+
+        });
+    });
+
+})();
