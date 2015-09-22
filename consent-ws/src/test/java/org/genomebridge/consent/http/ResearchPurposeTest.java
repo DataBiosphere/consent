b8ccf1c636ee007a1a81d25eb5f43a91bcd30a2e
Diego Gil <diego.gil@maipucinos.com.ar>
2015-09-22 14:27:34 -0300

    Implementing MOngoDB in memory for testing

--- consent-ws/src/test/java/org/genomebridge/consent/http/ResearchPurposeTest.java
+++ consent-ws/src/test/java/org/genomebridge/consent/http/ResearchPurposeTest.java
@@ -1,5 +1,12 @@
 package org.genomebridge.consent.http;
 
+import com.mongodb.MongoClient;
+import de.flapdoodle.embedmongo.MongoDBRuntime;
+import de.flapdoodle.embedmongo.MongodExecutable;
+import de.flapdoodle.embedmongo.MongodProcess;
+import de.flapdoodle.embedmongo.config.MongodConfig;
+import de.flapdoodle.embedmongo.distribution.Version;
+import de.flapdoodle.embedmongo.runtime.Network;
 import io.dropwizard.testing.junit.DropwizardAppRule;
 import org.genomebridge.consent.http.models.ResearchPurpose;
 import org.genomebridge.consent.http.models.grammar.And;
@@ -17,13 +24,16 @@ import java.io.IOException;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import org.genomebridge.consent.http.db.mongo.MongoConsentDB;
+import org.genomebridge.consent.http.service.DatabaseResearchPurposeAPI;
+import org.junit.After;
+import org.junit.Before;
 
 public class ResearchPurposeTest extends ResearchPurposeServiceTest {
 
-    public static final int CREATED = Response.Status.CREATED.getStatusCode();
-    public static final int OK = Response.Status.OK.getStatusCode();
-    public static final int NOT_FOUND = Response.Status.NOT_FOUND.getStatusCode();
-
+    private MongodExecutable mongodExe;
+    private MongodProcess mongod;
+    private MongoClient mongo;
 
     @ClassRule
     public static final DropwizardAppRule<ConsentConfiguration> RULE = new DropwizardAppRule<>(
@@ -32,6 +42,33 @@ public class ResearchPurposeTest extends ResearchPurposeServiceTest {
     @Override
     public DropwizardAppRule<ConsentConfiguration> rule() {
         return RULE;
+    }
+
+    @Before
+    public void setup() throws Exception {
+        
+        // Creating Mongodbruntime instance
+        MongoDBRuntime runtime = MongoDBRuntime.getDefaultInstance();
+        
+        // Creating MongodbExecutable
+        mongodExe = runtime.prepare(new MongodConfig(Version.V2_1_2, 37017, Network.localhostIsIPv6()));
+        
+        // Starting Mongodb
+        mongod = mongodExe.start();
+        mongo = new MongoClient("localhost", 37017);
+
+        MongoConsentDB mongoi = new MongoConsentDB(mongo);
+
+        // configuring ResearchPurposeAPI instance to use in memory Mongo
+        DatabaseResearchPurposeAPI.clearInstance();
+        DatabaseResearchPurposeAPI.initInstance(mongoi);
+        
+    }
+
+    @After
+    public void teardown() throws Exception {
+        mongod.stop();
+        mongodExe.cleanup();
     }
 
     @Test
@@ -50,7 +87,7 @@ public class ResearchPurposeTest extends ResearchPurposeServiceTest {
         deleteResearchPurpose(created.getId());
     }
 
-    public void updateResearchPurpose(String id) throws IOException{
+    public void updateResearchPurpose(String id) throws IOException {
         Client client = ClientBuilder.newClient();
         ResearchPurpose purpose = new ResearchPurpose();
         UseRestriction useRestriction = new Or(new Named("IOID:1"), new Named("IOID:3"));
@@ -86,15 +123,17 @@ public class ResearchPurposeTest extends ResearchPurposeServiceTest {
         String createdLocation2 = checkHeader(response2, "Location");
         ResearchPurpose created1 = retrieveResearchPurpose(client, createdLocation);
         ResearchPurpose created2 = retrieveResearchPurpose(client, createdLocation2);
-        List<ResearchPurpose> researchPurposes = getJson(client, purposePath()+"?ids="+created1.getId()+","+created2.getId()).readEntity(new GenericType<List<ResearchPurpose>>() {});;
+        List<ResearchPurpose> researchPurposes = getJson(client, purposePath() + "?ids=" + created1.getId() + "," + created2.getId()).readEntity(new GenericType<List<ResearchPurpose>>() {
+        });;
         assertThat(researchPurposes.size() == 2);
         deleteResearchPurpose(created1.getId());
-        researchPurposes = getJson(client, purposePath()+"?ids="+created1.getId()+","+created2.getId()).readEntity(new GenericType<List<ResearchPurpose>>() {});;
+        researchPurposes = getJson(client, purposePath() + "?ids=" + created1.getId() + "," + created2.getId()).readEntity(new GenericType<List<ResearchPurpose>>() {
+        });;
         assertThat(researchPurposes.size() == 1);
         deleteResearchPurpose(created2.getId());
-        researchPurposes = getJson(client, purposePath()+"?ids="+created1.getId()+","+created2.getId()).readEntity(new GenericType<List<ResearchPurpose>>() {});;
+        researchPurposes = getJson(client, purposePath() + "?ids=" + created1.getId() + "," + created2.getId()).readEntity(new GenericType<List<ResearchPurpose>>() {
+        });;
         assertThat(researchPurposes.size() == 0);
     }
-
 
 }
