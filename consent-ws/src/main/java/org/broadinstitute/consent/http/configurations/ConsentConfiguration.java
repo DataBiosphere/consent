613c44d1a02f7629d9cdd47661a2584b32a4ecff
Anabella Racioppi <araciopp@broadinstitute.org>
2016-06-02 15:37:20 -0300

    DUOS-134: Change Ontology wrong term

--- consent-ws/src/main/java/org/broadinstitute/consent/http/configurations/ConsentConfiguration.java
+++ consent-ws/src/main/java/org/broadinstitute/consent/http/configurations/ConsentConfiguration.java
@@ -20,7 +20,7 @@ public class ConsentConfiguration extends Configuration {
 
     private final UseRestrictionConfig ontology = new UseRestrictionConfig(
         "http://www.broadinstitute.org/ontologies/DURPO/methods_research",
-        "http://www.broadinstitute.org/ontologies/DURPO/aggregate_analysis",
+        "http://www.broadinstitute.org/ontologies/DURPO/aggregate_research",
         "http://www.broadinstitute.org/ontologies/DURPO/control",
         "http://www.broadinstitute.org/ontologies/DURPO/population_structure",
         "http://www.broadinstitute.org/ontologies/DURPO/male",
