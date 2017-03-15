2fcdbeeaadce4e6f9874222bb4ecfd1e23784e1a
Greg Rushton <grushton@broadinstitute.org>
2017-03-15 09:58:31 -0400

    check list of service accounts on incoming OAuth tokens

--- consent-ws/src/main/java/org/broadinstitute/consent/http/configurations/GoogleOAuth2Config.java
+++ consent-ws/src/main/java/org/broadinstitute/consent/http/configurations/GoogleOAuth2Config.java
@@ -1,13 +1,17 @@
 package org.broadinstitute.consent.http.configurations;
 
 import javax.validation.constraints.NotNull;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings({"WeakerAccess", "unused"})
 public class GoogleOAuth2Config {
 
     @NotNull
     public String clientId;
+
+    @NotNull
+    public List<String> authorizedAccounts;
 
     public String getClientId() {
         return clientId;
@@ -17,8 +21,18 @@ public class GoogleOAuth2Config {
         this.clientId = clientId;
     }
 
+    public List<String> getAuthorizedAccounts() {
+        return authorizedAccounts;
+    }
+
+    public void setAuthorizedAccounts(List<String> authorizedAccounts) {
+        this.authorizedAccounts = authorizedAccounts;
+    }
+
     public List<String> getAuthorizedServiceAccounts() {
-        return Collections.singletonList(clientId);
+        List<String> serviceAccounts = new ArrayList<>(getAuthorizedAccounts());
+        serviceAccounts.add(getClientId());
+        return serviceAccounts;
     }
 
 }
