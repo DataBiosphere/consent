de8cce5e127b9466f54a278eb74207ce8cb66096
Adam Nichols <anichols@broadinstitute.org>
2017-06-28 17:21:32 -0400

    Update deprecated Mongo health check

--- consent-ws/src/main/java/org/broadinstitute/consent/http/MongoHealthCheck.java
+++ consent-ws/src/main/java/org/broadinstitute/consent/http/MongoHealthCheck.java
@@ -1,20 +1,33 @@
 package org.broadinstitute.consent.http;
 
 import com.codahale.metrics.health.HealthCheck;
-import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.BasicDBObject;
+import com.mongodb.MongoException;
 
 public class MongoHealthCheck extends HealthCheck {
  
-    private final Mongo mongo;
+    private final MongoClient mongo;
+    private final String databaseName;
  
-    protected MongoHealthCheck(Mongo mongo) {
+    protected MongoHealthCheck(MongoClient mongo, String databaseName) {
         super();
         this.mongo = mongo;
+        this.databaseName = databaseName;
     }
  
     @Override
     protected Result check() throws Exception {
-        mongo.getDatabaseNames();
+
+        // https://stackoverflow.com/a/16823314/818054
+        BasicDBObject ping = new BasicDBObject("ping", "1");
+
+        try {
+            mongo.getDatabase(this.databaseName).runCommand(ping);
+        } catch (MongoException e) {
+            return Result.unhealthy("Exception querying Mongo instance: " + e.getMessage());
+        }
+
         return Result.healthy();
     }
  
