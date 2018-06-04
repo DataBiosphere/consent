cc865cab9bda78bd6dd33f8f9133e928e1a5235f
vernicavicario <vernicavicario@yahoo.com.ar>
2018-06-04 16:32:56 -0300

    mapping researcher values

--- consent-ws/src/main/java/org/broadinstitute/consent/http/service/users/handler/DatabaseResearcherAPI.java
+++ consent-ws/src/main/java/org/broadinstitute/consent/http/service/users/handler/DatabaseResearcherAPI.java
@@ -121,6 +121,8 @@ public class DatabaseResearcherAPI implements ResearcherAPI{
         rpForDAR.put(ResearcherFields.ACADEMIC_BUSINESS_EMAIL.getValue(), properties.containsKey(ResearcherFields.ACADEMIC_BUSINESS_EMAIL.getValue()) ? properties.get(ResearcherFields.ACADEMIC_BUSINESS_EMAIL.getValue()) : null);
         rpForDAR.put(ResearcherFields.SCIENTIFIC_URL.getValue(), properties.containsKey(ResearcherFields.SCIENTIFIC_URL.getValue()) ? properties.get(ResearcherFields.SCIENTIFIC_URL.getValue()) : null);
         rpForDAR.put(ResearcherFields.ARE_YOU_PRINCIPAL_INVESTIGATOR.getValue(), properties.containsKey(ResearcherFields.ARE_YOU_PRINCIPAL_INVESTIGATOR.getValue()) ? properties.get(ResearcherFields.ARE_YOU_PRINCIPAL_INVESTIGATOR.getValue()) : null);
+        rpForDAR.put(ResearcherFields.PI_NAME.getValue(), properties.containsKey(ResearcherFields.PI_NAME.getValue()) ? properties.get(ResearcherFields.PI_NAME.getValue()) : null);
+        rpForDAR.put(ResearcherFields.PI_NAME.getValue(), properties.containsKey(ResearcherFields.PI_EMAIL.getValue()) ? properties.get(ResearcherFields.PI_EMAIL.getValue()) : null);
         rpForDAR.put(ResearcherFields.COMPLETED.getValue(), properties.containsKey(ResearcherFields.COMPLETED.getValue()) ? properties.get(ResearcherFields.COMPLETED.getValue()) : null);
         return rpForDAR;
     }
