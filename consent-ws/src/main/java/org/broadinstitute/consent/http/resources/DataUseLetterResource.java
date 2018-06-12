4e13a417c199d96835304948d5e8183e833a489a
leoforconesi <lforcone@broadinstitute.org>
2018-06-12 14:59:57 -0300

    correct election values verification

--- consent-ws/src/main/java/org/broadinstitute/consent/http/resources/DataUseLetterResource.java
+++ consent-ws/src/main/java/org/broadinstitute/consent/http/resources/DataUseLetterResource.java
@@ -135,7 +135,7 @@ public class DataUseLetterResource extends Resource {
             if (StringUtils.isNotEmpty(consent.getLastElectionStatus())) {
                 election = api.retrieveElection(electionId, consentId);
             }
-            String fileUrl = (election != null && !election.getDulName().isEmpty()) ? election.getDataUseLetter() : consent.getDataUseLetter();
+            String fileUrl = (election != null && !election.getDataUseLetter().isEmpty()) ? election.getDataUseLetter() : consent.getDataUseLetter();
             String fileName = (election != null && !election.getDulName().isEmpty()) ? election.getDulName() : consent.getDulName();
             HttpResponse r = store.getStorageDocument(fileUrl);
             File targetFile = new File(fileName);
