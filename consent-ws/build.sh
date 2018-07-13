db89f57dd3cae3549e4f9026cf4107f6b8cd8916
Jackie Roberti <jacqueline.roberti93@gmail.com>
2018-07-13 10:42:32 -0400

    hash tag all images

--- consent-ws/build.sh
+++ consent-ws/build.sh
@@ -4,19 +4,17 @@ set -ex
 
 function docker_cmd() {
 	if [ $DOCKER_CMD = "build" ] || [ $DOCKER_CMD = "push" ]; then
-		if [ "$ENV" != "dev" ] && [ "$ENV" != "alpha" ] && [ "$ENV" != "staging" ] && [ "$ENV" != "perf" ]; then
-			DOCKER_TAG=${BRANCH}
-		else
-			GIT_SHA=$(git rev-parse origin/$BRANCH)
-			echo GIT_SHA=$GIT_SHA > env.properties
-			DOCKER_TAG=${GIT_SHA:0:12}
-		fi
-		echo "building $REPO:$DOCKER_TAG..."
-		docker build -t $REPO:$DOCKER_TAG --file consent-ws/Dockerfile .
+		echo GIT_SHA=$GIT_SHA > env.properties
+		HASH_TAG=${GIT_SHA:0:12}
+		
+		echo "building $REPO:$HASH_TAG..."
+		docker build -t $REPO:$HASH_TAG --file consent-ws/Dockerfile .
 
 		if [ $DOCKER_CMD = "push" ]; then
-			echo "pushing $REPO:$DOCKER_TAG..."
-			docker push $REPO:$DOCKER_TAG
+			echo "pushing $REPO:$HASH_TAG..."
+			docker push $REPO:$HASH_TAG
+			docker tag $REPO:$HASH_TAG $REPO:$BRANCH
+			docker push $REPO:$BRANCH
 		fi
 	else
 		echo "Not a valid docker option! Choose either build or push (which includes build)"
@@ -26,6 +24,7 @@ function docker_cmd() {
 PROJECT=consent
 BRANCH=${BRANCH:-$(git rev-parse --abbrev-ref HEAD)}
 REPO=broadinstitute/$PROJECT
+GIT_SHA=$(git rev-parse origin/$BRANCH)
 
 while [ "$1" != "" ]; do
     case $1 in
