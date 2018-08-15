6dc252ef6ca741fa22a1e0fb140d072481bd9762
Jackie Roberti <jacqueline.roberti93@gmail.com>
2018-08-15 12:28:44 -0400

    consent -ws

--- consent-ws/build.sh
+++ consent-ws/build.sh
@@ -92,7 +92,7 @@ function docker_cmd()
         HASH_TAG=${GIT_SHA:0:12}
 
         echo "Building $DOCKERHUB_REGISTRY:$HASH_TAG"
-        docker build -t $DOCKERHUB_REGISTRY:${HASH_TAG} .
+        docker build -t $DOCKERHUB_REGISTRY:${HASH_TAG} consent-ws
 
         if [ $DOCKER_CMD = "push" ]; then
             echo "pushing $PROJECT docker image..."
