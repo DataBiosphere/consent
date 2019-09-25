f7fc8ef8484695cd225f50e9ce005bf6f76bb213
Greg Rushton <rushtong@users.noreply.github.com>
2019-09-25 16:02:31 -0400

    [DUOS-444][risk=low] Remove deprecated APIs (#362)
    * remove deprecated APIs, unused code

--- src/main/java/org/broadinstitute/consent/http/resources/UserResource.java
+++ src/main/java/org/broadinstitute/consent/http/resources/UserResource.java
@@ -2,20 +2,20 @@ package org.broadinstitute.consent.http.resources;
 
 
 import io.dropwizard.auth.Auth;
-import io.dropwizard.jersey.PATCH;
 import org.broadinstitute.consent.http.models.AuthUser;
 import org.broadinstitute.consent.http.models.DACUser;
 import org.broadinstitute.consent.http.models.dto.Error;
-import org.broadinstitute.consent.http.models.dto.PatchOperation;
 import org.broadinstitute.consent.http.service.users.UserAPI;
 
 import javax.annotation.security.PermitAll;
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import java.net.URI;
-import java.util.List;
 
 @Path("{api : (api/)?}user")
 public class UserResource extends Resource {
@@ -44,31 +44,6 @@ public class UserResource extends Resource {
             return Response.status(Response.Status.BAD_REQUEST).entity(new Error(e.getMessage(), Response.Status.BAD_REQUEST.getStatusCode())).build();
         } catch (Exception e){
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new Error(e.getMessage(), Response.Status.INTERNAL_SERVER_ERROR.getStatusCode())).build();
-        }
-    }
-
-    @PUT
-    @Consumes("application/json")
-    @Produces("application/json")
-    @PermitAll
-    public Response update(String json, @Auth AuthUser user) {
-        DACUser userToUpdate = new DACUser(json);
-        try {
-            return Response.ok().entity(userAPI.updateUser(userToUpdate, user.getName())).build();
-        } catch (Exception e){
-            return createExceptionResponse(e);
-        }
-    }
-
-    @PATCH
-    @Consumes("application/json")
-    @Produces("application/json")
-    @PermitAll
-    public Response partialUpdate(List<PatchOperation> patchOperations, @Auth AuthUser user) {
-        try {
-            return Response.ok().entity(userAPI.updatePartialUser(patchOperations, user.getName())).build();
-        } catch (Exception e){
-            return createExceptionResponse(e);
         }
     }
 
