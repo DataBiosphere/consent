78dddbd96040e2c7e2c1c3930c912cc7bae5b36d
Greg Rushton <grushton@broadinstitute.org>
2019-11-05 07:44:22 -0500

    temp election filtering testing

--- src/main/java/org/broadinstitute/consent/http/util/DacFilterable.java
+++ src/main/java/org/broadinstitute/consent/http/util/DacFilterable.java
@@ -3,6 +3,7 @@ package org.broadinstitute.consent.http.util;
 import org.broadinstitute.consent.http.db.DACUserDAO;
 import org.broadinstitute.consent.http.db.DacDAO;
 import org.broadinstitute.consent.http.db.DataSetDAO;
+import org.broadinstitute.consent.http.enumeration.ElectionType;
 import org.broadinstitute.consent.http.enumeration.UserRoles;
 import org.broadinstitute.consent.http.models.AuthUser;
 import org.broadinstitute.consent.http.models.Consent;
@@ -82,12 +83,53 @@ public interface DacFilterable {
             return Collections.emptyList();
         }
 
-        List<Integer> dataSetIds = dataSetDAO.findDataSetsByAuthUserEmail(authUser.getName()).
+        List<Integer> userDataSetIds = dataSetDAO.findDataSetsByAuthUserEmail(authUser.getName()).
                 stream().
                 map(DataSet::getDataSetId).
                 collect(Collectors.toList());
+
+//        List<String> referenceIds = elections.
+//                stream().
+//                map(Election::getReferenceId).
+//                collect(Collectors.toList());
+//
+//        List<Integer> consentDataSetIds = dataSetDAO.
+//                findDataSetsByConsentIds(referenceIds).
+//                stream().
+//                map(DataSet::getDataSetId).
+//                collect(Collectors.toList());
+//
+//        /*
+//            Types:
+//            DATA_ACCESS("DataAccess"),
+//            TRANSLATE_DUL("TranslateDUL"),
+//            RP("RP"),
+//            DATA_SET("DataSet");
+//
+//            Election -> TranslateDUL -> filter by dataset ids in consents (reference id = consent id)
+//                     -> DATA_ACCESS -> filter by DAR dataset IDs in DAR (reference id = dar id)
+//                     -> RP -> Same as DATA_ACCESS ... we're voting on the RP of the DAR.
+//                     -> DATA_SET -> filter by dataset ids in election (dataset id = dataset id)
+//         */
+//
+//        elections.stream().filter(e -> {
+//            ElectionType type = ElectionType.valueOf(e.getElectionType());
+//            switch (type) {
+//                case DATA_ACCESS:
+//                    return true;
+//                case RP:
+//                    return true;
+//                case TRANSLATE_DUL:
+//                    return true;
+//                case DATA_SET:
+//                    return true;
+//                default:
+//                    return true;
+//            }
+//        }).collect(Collectors.toList());
+
         return elections.stream().
-                filter(e -> dataSetIds.contains(e.getDataSetId())).
+                filter(e -> userDataSetIds.contains(e.getDataSetId())).
                 collect(Collectors.toList());
     }
 
