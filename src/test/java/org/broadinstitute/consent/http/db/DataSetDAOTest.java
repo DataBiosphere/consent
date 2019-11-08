0c00c35d1549f73f41956a3a8039749ce9c3d1b7
Greg Rushton <grushton@broadinstitute.org>
2019-11-08 11:55:48 -0500

    tests

--- src/test/java/org/broadinstitute/consent/http/db/DataSetDAOTest.java
+++ src/test/java/org/broadinstitute/consent/http/db/DataSetDAOTest.java
@@ -7,6 +7,10 @@ import org.apache.commons.lang3.RandomUtils;
 import org.broadinstitute.consent.http.AbstractTest;
 import org.broadinstitute.consent.http.ConsentApplication;
 import org.broadinstitute.consent.http.configurations.ConsentConfiguration;
+import org.broadinstitute.consent.http.enumeration.UserRoles;
+import org.broadinstitute.consent.http.models.Consent;
+import org.broadinstitute.consent.http.models.DACUser;
+import org.broadinstitute.consent.http.models.Dac;
 import org.broadinstitute.consent.http.models.DataSet;
 import org.junit.After;
 import org.junit.Assert;
@@ -15,15 +19,18 @@ import org.junit.ClassRule;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 public class DataSetDAOTest extends AbstractTest {
 
     // TODO: We have to track what we add because there are tests that rely on existing data.
-    // Once those are replaced, this can be replaced with a delete all.
+    // Once those are replaced, these can be replaced with a delete all.
     private List<Integer> createdDataSetIds = new ArrayList<>();
+    private List<Integer> createdDacIds = new ArrayList<>();
+    private List<String> createdConsentIds = new ArrayList<>();
+    private List<String> createdDacUserEmails = new ArrayList<>();
 
     @SuppressWarnings("UnstableApiUsage")
     @ClassRule
@@ -35,16 +42,35 @@ public class DataSetDAOTest extends AbstractTest {
         return RULE;
     }
 
+    private DacDAO dacDAO;
+    private DACUserDAO dacUserDAO;
+    private ConsentDAO consentDAO;
     private DataSetDAO dataSetDAO;
+    private UserRoleDAO userRoleDAO;
 
     @Before
     public void setUp() {
+        dacDAO = getApplicationJdbi().onDemand(DacDAO.class);
+        dacUserDAO = getApplicationJdbi().onDemand(DACUserDAO.class);
         dataSetDAO = getApplicationJdbi().onDemand(DataSetDAO.class);
+        consentDAO = getApplicationJdbi().onDemand(ConsentDAO.class);
+        userRoleDAO = getApplicationJdbi().onDemand(UserRoleDAO.class);
     }
 
     @After
     public void tearDown() {
+        // Order is important for FK constraints
+        createdConsentIds.forEach(id -> {
+            consentDAO.deleteAllAssociationsForConsent(id);
+            consentDAO.deleteConsent(id);
+        });
         dataSetDAO.deleteDataSets(createdDataSetIds);
+        createdDacUserEmails.forEach(email -> {
+            userRoleDAO.findRolesByUserEmail(email).
+                    forEach(ur -> userRoleDAO.removeSingleUserRole(ur.getUserId(), ur.getRoleId()));
+            dacUserDAO.deleteDACUserByEmail(email);
+        });
+        createdDacIds.forEach(id -> dacDAO.deleteDac(id));
     }
 
     @Test
@@ -54,10 +80,7 @@ public class DataSetDAOTest extends AbstractTest {
 
     @Test
     public void testFindDatasetById() {
-        Integer id = createDataset();
-        DataSet dataSet = dataSetDAO.findDataSetById(id);
-        Assert.assertNotNull(dataSet);
-        dataSetDAO.deleteDataSets(Collections.singletonList(id));
+        // No-op ... tested in `createDataset()`
     }
 
     @Test
@@ -65,7 +88,70 @@ public class DataSetDAOTest extends AbstractTest {
         // No-op ... tested in `tearDown()`
     }
 
-    private Integer createDataset() {
+    // User -> UserRoles -> DACs -> Consents -> Consent Associations -> DataSets
+    @Test
+    public void testFindDataSetsByAuthUserEmail() {
+        DataSet dataset = createDataset();
+        Dac dac = createDac();
+        Consent consent = createConsent(dac.getDacId());
+        createAssociation(consent.getConsentId(), dataset.getDataSetId());
+        DACUser user = createDacUser();
+        createUserRole(UserRoles.CHAIRPERSON.getRoleId(), user.getDacUserId(), dac.getDacId());
+
+        List<DataSet> dataSets = dataSetDAO.findDataSetsByAuthUserEmail(user.getEmail());
+        Assert.assertFalse(dataSets.isEmpty());
+        Assert.assertEquals(1, dataSets.size());
+    }
+
+    @Test
+    public void testFindNonDACDataSets() {
+        // TODO
+    }
+
+    private void createUserRole(Integer roleId, Integer userId, Integer dacId) {
+        dacDAO.addDacMember(roleId, userId, dacId);
+    }
+
+    private DACUser createDacUser() {
+        String name = RandomStringUtils.random(10, true, false);
+        String email = name + "@test.org";
+        createdDacUserEmails.add(email);
+        Integer id = dacUserDAO.insertDACUser(email, name, new Date());
+        return dacUserDAO.findDACUserById(id);
+    }
+
+    private void createAssociation(String consentId, Integer datasetId) {
+        consentDAO.insertConsentAssociation(consentId, "sampleSet", datasetId);
+    }
+
+    private Consent createConsent(Integer dacId) {
+        String consentId = UUID.randomUUID().toString();
+        consentDAO.insertConsent(consentId,
+                false,
+                "{\"type\":\"everything\"}",
+                "{\"generalUse\": true }",
+                "dul",
+                consentId,
+                "dulName",
+                new Date(),
+                new Date(),
+                "Everything",
+                true,
+                "Group",
+                dacId);
+        createdConsentIds.add(consentId);
+        return consentDAO.findConsentById(consentId);
+    }
+
+    private Dac createDac() {
+        Integer id = dacDAO.createDac(
+                "Test_" + RandomStringUtils.random(20, true, true),
+                "Test_" + RandomStringUtils.random(20, true, true),
+                new Date());
+        createdDacIds.add(id);
+        return dacDAO.findById(id);
+    }
+    private DataSet createDataset() {
         DataSet ds = new DataSet();
         ds.setName("Name_" + RandomStringUtils.random(20, true, true));
         ds.setCreateDate(new Date());
@@ -74,7 +160,7 @@ public class DataSetDAOTest extends AbstractTest {
         ds.setAlias(RandomUtils.nextInt(1, 1000));
         Integer id = dataSetDAO.insertDataset(ds.getName(), ds.getCreateDate(), ds.getObjectId(), ds.getActive(), ds.getAlias());
         createdDataSetIds.add(id);
-        return id;
+        return dataSetDAO.findDataSetById(id);
     }
 
 }
