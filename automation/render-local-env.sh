c983fc191bb155c91a4f2c0572e273149a6e204a
Greg Rushton <grushton@broadinstitute.org>
2020-01-14 10:07:29 -0500

    script updates

--- automation/render-local-env.sh
+++ automation/render-local-env.sh
@@ -13,31 +13,37 @@ VAULT_TOKEN=$(cat ~/.vault-token)
 ENV=${1:-$ENV}
 VAULT_TOKEN=${2:-$VAULT_TOKEN}
 
-# render application.conf
 docker pull broadinstitute/dsde-toolbox:dev
-docker run -it --rm -e VAULT_TOKEN=${VAULT_TOKEN} \
-    -e ENVIRONMENT=${ENV} \
-    -e ROOT_DIR=${WORKING_DIR} \
-    -e OUT_PATH=/output/src/test/resources \
-    -v $PWD:/output \
-    -e INPUT_PATH=/input \
-    -v $PWD/configs:/input \
-    broadinstitute/dsde-toolbox:dev render-templates.sh
 
-# render role service account credentials
+# render application.conf
+docker run -it --rm -e VAULT_TOKEN="${VAULT_TOKEN}" \
+  -e ENVIRONMENT="$ENV" \
+  -e ROOT_DIR="${WORKING_DIR}" \
+  -e OUT_PATH=/output/src/test/resources \
+  -v "$PWD":/output \
+  -e INPUT_PATH=/input \
+  -v "$PWD"/configs:/input \
+  broadinstitute/dsde-toolbox:dev render-templates.sh
+
+# render role service account credential files
+if [ $ENV == "local" ]; then
+  secretPath="/secret/dsde/firecloud/dev/consent/automation"
+else
+  secretPath="/secret/dsde/firecloud/${ENV}/consent/automation"
+fi
+
 listOfRoles="admin
 chair
 member
 researcher"
 
-for role in $listOfRoles
-  do
-    echo "Writing $role file... to src/test/resources/accounts/"
-    docker run -it --rm \
-        -v $HOME:/root \
-        broadinstitute/dsde-toolbox:dev vault read \
-        --format=json \
-        /secret/dsde/firecloud/dev/consent/automation/duos-automation-$role.json \
-        | jq .data \
-        > src/test/resources/accounts/duos-automation-$role.json
-  done
+for role in $listOfRoles; do
+  echo "Writing $role file from $secretPath/duos-automation-$role.json"
+  docker run -it --rm \
+    -v "$HOME":/root \
+    broadinstitute/dsde-toolbox:dev vault read \
+    --format=json \
+    "$secretPath"/duos-automation-"$role".json |
+    jq .data \
+    >src/test/resources/accounts/duos-automation-"$role".json
+done
