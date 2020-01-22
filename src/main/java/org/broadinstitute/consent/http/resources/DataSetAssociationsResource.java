d92e34addf2784d4ddb733966a289745d25d39f0
Greg Rushton <grushton@broadinstitute.org>
2020-01-22 16:58:15 -0500

    reverting changes - split into new PR

--- src/main/java/org/broadinstitute/consent/http/resources/DataSetAssociationsResource.java
+++ src/main/java/org/broadinstitute/consent/http/resources/DataSetAssociationsResource.java
@@ -1,45 +1,32 @@
 package org.broadinstitute.consent.http.resources;
 
-import org.broadinstitute.consent.http.models.DACUser;
-import org.broadinstitute.consent.http.models.DatasetAssociation;
 import org.broadinstitute.consent.http.service.AbstractDataSetAssociationAPI;
 import org.broadinstitute.consent.http.service.DataSetAssociationAPI;
-
 import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
-import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 
-@Path("api/datasetAssociation")
+@Path("{api : (api/)?}datasetAssociation")
 public class DataSetAssociationsResource extends Resource {
 
 
     private final DataSetAssociationAPI api;
-
     public DataSetAssociationsResource() {
         this.api = AbstractDataSetAssociationAPI.getInstance();
     }
 
 
     @POST
-    @Path("/{datasetId}")
+    @Path("/{dataSetId}")
     @Consumes("application/json")
     @Produces("application/json")
     @RolesAllowed(ADMIN)
-    public Response associateDatasetWithUsers(@PathParam("datasetId") Integer datasetId, List<Integer> userIdList) {
+    public Response associateDatasetWithUsers(@PathParam("dataSetId") Integer dataSetId, List<Integer> usersIdList) {
         try {
-            List<DatasetAssociation> associations = api.createDatasetUsersAssociation(datasetId, userIdList);
-            return Response.status(Response.Status.CREATED).entity(associations).build();
-        } catch (Exception e) {
+            return  Response.status(Response.Status.CREATED).entity(api.createDatasetUsersAssociation(dataSetId, usersIdList)).build() ;
+        } catch (Exception e){
             return createExceptionResponse(e);
         }
     }
@@ -51,8 +38,7 @@ public class DataSetAssociationsResource extends Resource {
     @PermitAll
     public Response getDatasetAssociations(@PathParam("dataSetId") Integer dataSetId) {
         try {
-            Map<String, Collection<DACUser>> userMap = api.findDataOwnersRelationWithDataset(dataSetId);
-            return Response.ok().entity(userMap).build();
+            return Response.ok(api.findDataOwnersRelationWithDataset(dataSetId)).build();
         } catch (Exception e) {
             return createExceptionResponse(e);
         }
@@ -63,11 +49,10 @@ public class DataSetAssociationsResource extends Resource {
     @Consumes("application/json")
     @Produces("application/json")
     @RolesAllowed(ADMIN)
-    public Response updateDatasetAssociations(@PathParam("dataSetId") Integer dataSetId, List<Integer> userIdList) {
+    public Response updateDatasetAssociations(@PathParam("dataSetId") Integer dataSetId, List<Integer> usersIdList) {
         try {
-            List<DatasetAssociation> associations = api.updateDatasetAssociations(dataSetId, userIdList);
-            return Response.ok().entity(associations).build();
-        } catch (Exception e) {
+            return  Response.ok(api.updateDatasetAssociations(dataSetId, usersIdList)).build() ;
+        } catch (Exception e){
             return createExceptionResponse(e);
         }
     }
