be6d84cdac5c44556c45551fa6c8b0710ad0961d
Greg Rushton <grushton@broadinstitute.org>
2020-01-23 07:10:49 -0500

    more tests

--- src/test/java/org/broadinstitute/consent/http/resources/DataSetAssociationsResourceTest.java
+++ src/test/java/org/broadinstitute/consent/http/resources/DataSetAssociationsResourceTest.java
@@ -13,11 +13,16 @@ import org.mockito.MockitoAnnotations;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.skife.jdbi.v2.exceptions.StatementException;
+import org.skife.jdbi.v2.exceptions.UnableToExecuteStatementException;
 
+import javax.ws.rs.BadRequestException;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.core.Response;
 import java.util.Collections;
 
 import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 
 @RunWith(PowerMockRunner.class)
@@ -37,26 +42,55 @@ public class DataSetAssociationsResourceTest {
         when(datasetAssociationAPI.findDataOwnersRelationWithDataset(any())).thenReturn(Collections.emptyMap());
         when(datasetAssociationAPI.updateDatasetAssociations(any(), any())).thenReturn(Collections.emptyList());
         when(AbstractDataSetAssociationAPI.getInstance()).thenReturn(datasetAssociationAPI);
+    }
+
+    private void initResource() {
         resource = new DataSetAssociationsResource();
     }
 
-
     @Test
     public void testAssociateDatasetWithUsers() {
+        initResource();
         Response response = resource.associateDatasetWithUsers(RandomUtils.nextInt(1, 100), Collections.emptyList());
         Assert.assertEquals(201, response.getStatus());
     }
 
     @Test
+    public void testAssociateDatasetWithUsers_NotFound() {
+        when(datasetAssociationAPI.createDatasetUsersAssociation(any(), any())).thenThrow(new NotFoundException());
+        initResource();
+        Response response = resource.associateDatasetWithUsers(RandomUtils.nextInt(1, 100), Collections.emptyList());
+        Assert.assertEquals(HttpStatusCodes.STATUS_CODE_NOT_FOUND, response.getStatus());
+    }
+
+    @Test
+    public void testAssociateDatasetWithUsers_BadRequest() {
+        when(datasetAssociationAPI.createDatasetUsersAssociation(any(), any())).thenThrow(new BadRequestException());
+        initResource();
+        Response response = resource.associateDatasetWithUsers(RandomUtils.nextInt(1, 100), Collections.emptyList());
+        Assert.assertEquals(HttpStatusCodes.STATUS_CODE_BAD_REQUEST, response.getStatus());
+    }
+
+    @Test
     public void testGetDatasetAssociations() {
+        initResource();
         Response response = resource.getDatasetAssociations(RandomUtils.nextInt(1, 100));
         Assert.assertEquals(HttpStatusCodes.STATUS_CODE_OK, response.getStatus());
     }
 
     @Test
     public void testUpdateDatasetAssociations() {
+        initResource();
         Response response = resource.updateDatasetAssociations(RandomUtils.nextInt(1, 100), Collections.emptyList());
         Assert.assertEquals(HttpStatusCodes.STATUS_CODE_OK, response.getStatus());
+    }
+
+    @Test
+    public void testUpdateDatasetAssociations_Notfound() {
+        when(datasetAssociationAPI.updateDatasetAssociations(any(), any())).thenThrow(new NotFoundException());
+        initResource();
+        Response response = resource.updateDatasetAssociations(RandomUtils.nextInt(1, 100), Collections.emptyList());
+        Assert.assertEquals(HttpStatusCodes.STATUS_CODE_NOT_FOUND, response.getStatus());
     }
 
 }
