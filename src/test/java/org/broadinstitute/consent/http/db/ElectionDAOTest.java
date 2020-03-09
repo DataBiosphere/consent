37a857d7d56a5884bc4acf3273c32c75efd0b6af
Greg Rushton <grushton@broadinstitute.org>
2020-03-09 09:47:44 -0400

    test

--- src/test/java/org/broadinstitute/consent/http/db/ElectionDAOTest.java
+++ src/test/java/org/broadinstitute/consent/http/db/ElectionDAOTest.java
@@ -7,7 +7,9 @@ import org.broadinstitute.consent.http.models.Election;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ElectionDAOTest extends DAOTestHelper {
 
@@ -81,6 +83,29 @@ public class ElectionDAOTest extends DAOTestHelper {
 
         List<Election> foundElections = electionDAO.findOpenElectionsByDacId(dac.getDacId());
         Assert.assertTrue(foundElections.isEmpty());
+    }
+
+    @Test
+    public void testFindDacsForConsentElections() {
+        Dac dac = createDac();
+        Consent consent = createConsent(dac.getDacId());
+        DataSet dataset = createDataset();
+        createAssociation(consent.getConsentId(), dataset.getDataSetId());
+        Election election = createElection(consent.getConsentId(), dataset.getDataSetId());
+        Dac dac2 = createDac();
+        Consent consent2 = createConsent(dac2.getDacId());
+        DataSet dataset2 = createDataset();
+        createAssociation(consent2.getConsentId(), dataset2.getDataSetId());
+        Election election2 = createElection(consent2.getConsentId(), dataset2.getDataSetId());
+        List<Integer> electionIds = new ArrayList<>();
+        electionIds.add(election.getElectionId());
+        electionIds.add(election2.getElectionId());
+
+        List<Dac> foundDacs = electionDAO.findDacsForElections(electionIds);
+        Assert.assertNotNull(foundDacs);
+        Assert.assertFalse(foundDacs.isEmpty());
+        Assert.assertTrue(foundDacs.stream().map(Dac::getDacId).collect(Collectors.toList()).contains(dac.getDacId()));
+        Assert.assertTrue(foundDacs.stream().map(Dac::getDacId).collect(Collectors.toList()).contains(dac2.getDacId()));
     }
 
 }
