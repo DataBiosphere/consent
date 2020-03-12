ac58a01ad44b7d3b349449e3dbdf0ca1f72e03ec
Morgan Taylor <marymorg@broadinstitute.org>
2020-03-12 16:16:22 -0400

    update swagger for whitelist

--- src/main/java/org/broadinstitute/consent/http/resources/WhitelistResource.java
+++ src/main/java/org/broadinstitute/consent/http/resources/WhitelistResource.java
@@ -14,6 +14,9 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Path("api/whitelist")
 public class WhitelistResource extends Resource {
@@ -31,9 +34,13 @@ public class WhitelistResource extends Resource {
     @RolesAllowed(ADMIN)
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     public Response postWhitelist(@Auth AuthUser user, @FormDataParam("fileData") String fileData) {
-        // define bucket (in a config?)
+        // get a timestamp to label the file
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy_HH-mm-ss");
+        String timestamp = dateFormat.format(new Date());
         // push file to bucket
+        String fileName = "lc_whitelist_"+timestamp+".tsv";
         try {
+//            this.gcsStore.postWhitelist(InputStream fileData, String fileName);
             logger.info(fileData);
             return Response.ok(fileData).build();
         } catch (Exception e) {
