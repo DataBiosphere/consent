db2e0190ef5f66b5e31d0e7c63913ddf40830c40
Greg Rushton <rushtong@users.noreply.github.com>
2020-03-24 12:31:09 -0400

    [DUOS-608][risk=no] Vote query fix (#563)
    * query fix
    
    * api-doc fix

--- src/main/java/org/broadinstitute/consent/http/db/VoteDAO.java
+++ src/main/java/org/broadinstitute/consent/http/db/VoteDAO.java
@@ -22,7 +22,7 @@ public interface VoteDAO extends Transactional<VoteDAO> {
     List<Vote> findVotesByReferenceId(@Bind("referenceId") String referenceId);
 
     @SqlQuery("select v.*, u.email, u.displayName from vote v inner join election on election.electionId = v.electionId inner join dacuser u on u.dacUserId = v.dacUserId "
-            + "where election.electionId = :electionId and v.type != 'CHAIRPERSON'")
+            + "where election.electionId = :electionId and lower(v.type) != 'chairperson'")
     @UseRowMapper(ElectionReviewVoteMapper.class)
     List<ElectionReviewVote> findElectionReviewVotesByElectionId(@Bind("electionId") Integer electionId);
 
