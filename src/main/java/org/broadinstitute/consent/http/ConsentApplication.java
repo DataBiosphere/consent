d35cce9a50138fc385fb47abe1315987035447b3
Greg Rushton <grushton@broadinstitute.org>
2020-04-01 09:32:49 -0400

    constructor bug fix

--- src/main/java/org/broadinstitute/consent/http/ConsentApplication.java
+++ src/main/java/org/broadinstitute/consent/http/ConsentApplication.java
@@ -302,7 +302,7 @@ public class ConsentApplication extends Application<ConsentConfiguration> {
         env.jersey().register(new ConsentCasesResource(electionService, pendingCaseService));
         env.jersey().register(new DataRequestCasesResource(electionService, pendingCaseService));
         env.jersey().register(new DacResource(dacService));
-        env.jersey().register(DACUserResource.class);
+        env.jersey().register(new DACUserResource(userService));
         env.jersey().register(ElectionReviewResource.class);
         env.jersey().register(new ConsentManageResource(consentService));
         env.jersey().register(new ElectionResource(voteService));
