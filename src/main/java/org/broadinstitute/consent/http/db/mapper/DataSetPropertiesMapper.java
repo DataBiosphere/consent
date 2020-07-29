2e4d0693087707eb6b0e219c2013b86c14164f24
Greg Rushton <grushton@broadinstitute.org>
2020-07-29 11:58:48 -0400

    handle case where dac id is not in results

--- src/main/java/org/broadinstitute/consent/http/db/mapper/DataSetPropertiesMapper.java
+++ src/main/java/org/broadinstitute/consent/http/db/mapper/DataSetPropertiesMapper.java
@@ -10,7 +10,7 @@ import org.broadinstitute.consent.http.models.dto.DataSetPropertyDTO;
 import org.jdbi.v3.core.mapper.RowMapper;
 import org.jdbi.v3.core.statement.StatementContext;
 
-public class DataSetPropertiesMapper implements RowMapper<DataSetDTO> {
+public class DataSetPropertiesMapper implements RowMapper<DataSetDTO>, RowMapperHelper {
 
   private final Map<Integer, DataSetDTO> dataSets = new LinkedHashMap<>();
   private static final String PROPERTY_KEY = "key";
@@ -19,14 +19,16 @@ public class DataSetPropertiesMapper implements RowMapper<DataSetDTO> {
   public DataSetDTO map(ResultSet r, StatementContext ctx) throws SQLException {
 
     DataSetDTO dataSetDTO;
-    int dacId = r.getInt("dac_id");
     Integer dataSetId = r.getInt("dataSetId");
     String consentId = r.getString("consentId");
     Integer alias = r.getInt("alias");
     if (!dataSets.containsKey(dataSetId)) {
       dataSetDTO = new DataSetDTO(new ArrayList<>());
-      if (dacId > 0) {
-        dataSetDTO.setDacId(dacId);
+      if (hasColumn(r, "dac_id")) {
+        int dacId = r.getInt("dac_id");
+        if (dacId > 0) {
+          dataSetDTO.setDacId(dacId);
+        }
       }
       dataSetDTO.setConsentId(consentId);
       dataSetDTO.setAlias(alias);
