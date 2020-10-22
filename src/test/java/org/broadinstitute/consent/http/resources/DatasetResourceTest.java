c0e22d89dd851a65c9e39d6b31327ae0d2b07b6d
Greg Rushton <rushtong@users.noreply.github.com>
2020-10-22 10:56:17 -0400

    [DUOS-797][risk=no] Return 409 Conflict error if dataset name in use (#793)
    * switch from 404 to 409 for dataset name in use
    
    Co-authored-by: Linda Zhang <linda@broadinstitute.org>

--- src/test/java/org/broadinstitute/consent/http/resources/DatasetResourceTest.java
+++ src/test/java/org/broadinstitute/consent/http/resources/DatasetResourceTest.java
@@ -136,7 +136,7 @@ public class DatasetResourceTest {
 
         Response responseNameInUse = resource.createDataset(authUser, uriInfo,
               "{\"properties\":[{\"propertyName\":\"Dataset Name\",\"propertyValue\":\"test\"}]}");
-        assertEquals(400, responseNameInUse.getStatus());
+        assertEquals(409, responseNameInUse.getStatus());
     }
 
     @Test
