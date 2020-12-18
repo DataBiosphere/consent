0286b8b1ed7d760507167b92118d543135fe81d2
Greg Rushton <rushtong@users.noreply.github.com>
2020-12-18 06:03:26 -0500

    [DUOS-895][risk=no] Add 'X-Content-Type-Options' header to outbound responses (#867)
    * add header to outbound responses

---
+++ src/main/java/org/broadinstitute/consent/http/filters/ResponseServerFilter.java
@@ -0,0 +1,21 @@
+package org.broadinstitute.consent.http.filters;
+
+import java.io.IOException;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class ResponseServerFilter implements ContainerResponseFilter {
+
+  @Override
+  public void filter(
+      ContainerRequestContext requestContext,
+      ContainerResponseContext responseContext) throws IOException {
+    // When the no-sniff header is on the swagger-ui files, it breaks the overall UI
+    if (!requestContext.getUriInfo().getPath().contains("swagger-ui")) {
+      responseContext.getHeaders().add("X-Content-Type-Options", "nosniff");
+    }
+  }
+}
