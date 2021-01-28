b947433ea14a39245fb7166c309807ec2e22713a
rfricke-asymmetrik <rfricke@asymmetrik.com>
2021-01-27 20:17:30 -0500

    Update interpret_results.py

--- automation/scripts/interpret_results.py
+++ automation/scripts/interpret_results.py
@@ -45,6 +45,73 @@ def get_failures(lines, className):
     failures = filter(lambda l: l.find("\tKO\t") > -1, lines)
     return list(map(lambda k: get_test_case(k, className), failures))
 
+def update_xml(assertions_xml, failures):
+    test_suite = assertions_xml['testsuite']
+    if len(failures) > 0:
+        test_suite.pop('testcase', None)
+        test_suite['testcase'] = failures
+        test_suite['@failures'] = len(failures)
+        test_suite['@tests'] = len(failures)
+        assertions_xml['testsuite'] = test_suite
+    return assertions_xml
+
+def strip_chars(str):
+    return str.replace('\n', '')
+
+def build_scenario_performance(simulation_lines):
+    scenario_performance_array = []
+    request_array = []
+    scenario_performance = {}
+
+    for line in simulation_lines:
+        line_split = line.split('\t')
+        
+        if line_split[0] == 'USER':
+            if line_split[2] == 'START':
+                request_array = []
+                scenario_performance['start'] = float(strip_chars(line_split[3]))
+            elif line_split[2] == 'END':
+                scenario_performance['end'] = float(strip_chars(line_split[3]))
+                scenario_performance['requests'] = request_array
+                scenario_performance['time'] = scenario_performance['end'] - scenario_performance['start']
+
+                scenario_matches = filter(lambda sp: sp['name'] == line_split[1],scenario_performance_array)
+                if len(scenario_matches) == 1:
+                    scn = scenario_matches[0]
+                    scn['users'].append(scenario_performance)
+                    scn['max'] = scn['max'] if scn['max'] >= scenario_performance['time'] else scenario_performance['time']
+                    scn['total'] += scenario_performance['time']
+                    scn['avg'] = scn['total'] / len(scn['users'])
+                    scn['min'] = scn['min'] if scn['min'] < scenario_performance['time'] else scenario_performance['time']
+                else:
+                    scenario_performance_array.append({
+                        'name': line_split[1],
+                        'max': scenario_performance['time'],
+                        'avg': scenario_performance['time'],
+                        'min': scenario_performance['time'],
+                        'total': scenario_performance['time'],
+                        'users': [scenario_performance]
+                    })
+        elif line_split[0] == 'REQUEST':
+            request_array.append({
+                'name': line_split[2],
+                'start': float(strip_chars(line_split[3])),
+                'end': float(strip_chars(line_split[4])),
+                'time': float(strip_chars(line_split[4])) - float(strip_chars(line_split[3])),
+                'result': line_split[5]
+            })
+
+    return scenario_performance_array
+
+def build_json(assertions_json, simulation_lines):
+    assertions_json['scenarios'] = build_scenario_performance(simulation_lines)
+    end_time = float(simulation_lines[len(simulation_lines) - 2].split('\t')[4])
+    assertions_json['end'] = end_time
+    assertions_json['time'] = end_time - assertions_json['start']
+    assertions_json.pop('assertions', None)
+
+    return assertions_json
+
 def parse_gatling_results(gatling_dir):
     scenarios = {}
     for file_path in os.listdir(os.path.join(gatling_dir, "gatling")):
@@ -57,18 +124,14 @@ def parse_gatling_results(gatling_dir):
 
         simulation = assertion_json['simulation']
 
-        failures = get_failures(simulation_log_lines, simulation)
+        assertions_xml = update_xml(get_assertions_xml(gatling_dir, simulation), get_failures(simulation_log_lines, simulation))
 
-        assertions_xml = get_assertions_xml(gatling_dir, simulation)
-        test_suite = assertions_xml['testsuite']
-        if len(failures) > 0:
-            test_suite.pop('testcase', None)
-            test_suite['testcase'] = failures
-            test_suite['@failures'] = len(failures)
-            test_suite['@tests'] = len(failures)
-            assertions_xml['testsuite'] = test_suite
+        performance_json = build_json(assertion_json, simulation_log_lines)
 
-        scenarios[simulation] = xmltodict.unparse(assertions_xml, pretty=True)
+        scenarios[simulation] = {
+            'xml': xmltodict.unparse(assertions_xml, pretty=True),
+            'json': json.dumps(performance_json, indent=4)
+        }
     return scenarios
 
 if __name__ == '__main__':
@@ -76,8 +139,14 @@ if __name__ == '__main__':
     for scn in r:
         sim_dir = os.path.join("target/test-reports", "TEST-" + scn + ".xml")
         with open(sim_dir, 'w') as wf:
-            wf.write(r[scn])
+            wf.write(r[scn]['xml'])
             wf.write('\n')
         
+        js_dir = os.path.join("target/test-reports", "TEST-" + scn + "-performance.json")
+        with open(js_dir, 'w') as jwf:
+            jwf.write(r[scn]['json'])
+            jwf.write('\n')
+        
         print(sim_dir)
+        print(js_dir)
         
