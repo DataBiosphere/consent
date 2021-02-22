a9655b054f08efa65682beea520908958aa74760
raejohanek <rjohanek@broadinstitute.org>
2021-02-22 08:57:33 -0500

    -undo changes to util -remove unused imports

--- src/main/java/org/broadinstitute/consent/http/db/mapper/DataSetMapper.java
+++ src/main/java/org/broadinstitute/consent/http/db/mapper/DataSetMapper.java
@@ -3,7 +3,6 @@ package org.broadinstitute.consent.http.db.mapper;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import org.broadinstitute.consent.http.models.DataSet;
-import org.broadinstitute.consent.http.util.DatasetUtil;
 import org.jdbi.v3.core.mapper.RowMapper;
 import org.jdbi.v3.core.statement.StatementContext;
 
