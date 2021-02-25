27cab75fbff4de7cc1b33cf794b2bbb8fe020a2a
Greg Rushton <grushton@broadinstitute.org>
2021-02-25 12:30:09 -0500

    test cleanup

--- src/test/java/org/broadinstitute/consent/http/util/DarManualReviewTest.java
+++ src/test/java/org/broadinstitute/consent/http/util/DarManualReviewTest.java
@@ -3,133 +3,99 @@ package org.broadinstitute.consent.http.util;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Collections;
 import org.broadinstitute.consent.http.models.DataAccessRequest;
 import org.broadinstitute.consent.http.models.DataAccessRequestData;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.MockitoAnnotations;
 
 public class DarManualReviewTest {
 
+  private DataAccessRequest dar;
+
   @Before
   public void setUp() {
-    MockitoAnnotations.initMocks(this);
+    dar = new DataAccessRequest();
+    dar.setData(new DataAccessRequestData());
   }
 
   @Test
   public void testManualReviewFalse() {
-    DataAccessRequest dar = new DataAccessRequest();
     assertFalse(dar.requiresManualReview());
-    DataAccessRequestData data = new DataAccessRequestData();
-    dar.setData(data);
+    // There are many fields we could check, but this one is enough to prove our logic.
+    dar.getData().setDatasetIds(Collections.singletonList(1));
     assertFalse(dar.requiresManualReview());
   }
 
   @Test
   public void testManualReviewPoa() {
-    DataAccessRequest dar = new DataAccessRequest();
-    DataAccessRequestData data = new DataAccessRequestData();
-    data.setPoa(true);
-    dar.setData(data);
+    dar.getData().setPoa(true);
     assertTrue(dar.requiresManualReview());
   }
 
   @Test
   public void testManualReviewPopulation() {
-    DataAccessRequest dar = new DataAccessRequest();
-    DataAccessRequestData data = new DataAccessRequestData();
-    data.setPopulation(true);
-    dar.setData(data);
+    dar.getData().setPopulation(true);
     assertTrue(dar.requiresManualReview());
   }
 
   @Test
   public void testManualReviewOther() {
-    DataAccessRequest dar = new DataAccessRequest();
-    DataAccessRequestData data = new DataAccessRequestData();
-    data.setOther(true);
-    dar.setData(data);
+    dar.getData().setOther(true);
     assertTrue(dar.requiresManualReview());
   }
 
   @Test
   public void testManualReviewOtherText() {
-    DataAccessRequest dar = new DataAccessRequest();
-    DataAccessRequestData data = new DataAccessRequestData();
-    data.setOtherText("true");
-    dar.setData(data);
+    dar.getData().setOtherText("true");
     assertTrue(dar.requiresManualReview());
   }
 
   @Test
   public void testManualReviewIllegalBehavior() {
-    DataAccessRequest dar = new DataAccessRequest();
-    DataAccessRequestData data = new DataAccessRequestData();
-    data.setIllegalBehavior(true);
-    dar.setData(data);
+    dar.getData().setIllegalBehavior(true);
     assertTrue(dar.requiresManualReview());
   }
 
   @Test
   public void testManualReviewIllegalAddiction() {
-    DataAccessRequest dar = new DataAccessRequest();
-    DataAccessRequestData data = new DataAccessRequestData();
-    data.setAddiction(true);
-    dar.setData(data);
+    dar.getData().setAddiction(true);
     assertTrue(dar.requiresManualReview());
   }
 
   @Test
   public void testManualReviewSexualDiseases() {
-    DataAccessRequest dar = new DataAccessRequest();
-    DataAccessRequestData data = new DataAccessRequestData();
-    data.setSexualDiseases(true);
-    dar.setData(data);
+    dar.getData().setSexualDiseases(true);
     assertTrue(dar.requiresManualReview());
   }
 
   @Test
   public void testManualReviewStigmatizedDiseases() {
-    DataAccessRequest dar = new DataAccessRequest();
-    DataAccessRequestData data = new DataAccessRequestData();
-    data.setStigmatizedDiseases(true);
-    dar.setData(data);
+    dar.getData().setStigmatizedDiseases(true);
     assertTrue(dar.requiresManualReview());
   }
 
   @Test
   public void testManualReviewVulnerablePopulation() {
-    DataAccessRequest dar = new DataAccessRequest();
-    DataAccessRequestData data = new DataAccessRequestData();
-    data.setVulnerablePopulation(true);
-    dar.setData(data);
+    dar.getData().setVulnerablePopulation(true);
     assertTrue(dar.requiresManualReview());
   }
 
   @Test
   public void testManualReviewPopulationMigration() {
-    DataAccessRequest dar = new DataAccessRequest();
-    DataAccessRequestData data = new DataAccessRequestData();
-    data.setPopulationMigration(true);
-    dar.setData(data);
+    dar.getData().setPopulationMigration(true);
     assertTrue(dar.requiresManualReview());
   }
 
   @Test
   public void testManualReviewPsychiatricTraits() {
-    DataAccessRequest dar = new DataAccessRequest();
-    DataAccessRequestData data = new DataAccessRequestData();
-    data.setPsychiatricTraits(true);
-    dar.setData(data);
+    dar.getData().setPsychiatricTraits(true);
     assertTrue(dar.requiresManualReview());
   }
 
   @Test
   public void testManualReviewNotHealth() {
-    DataAccessRequest dar = new DataAccessRequest();
-    DataAccessRequestData data = new DataAccessRequestData();
-    data.setNotHealth(true);
-    dar.setData(data);
+    dar.getData().setNotHealth(true);
     assertTrue(dar.requiresManualReview());
   }
 
