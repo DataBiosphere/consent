2ff2b717801a1c398b9a46831436c8c06ce306f2
Justin Variath Thomas <JVThomas@users.noreply.github.com>
2021-03-16 10:45:12 -0400

    DUOS-1104[risk=no]Cancel Election does not update all of the election's votes correctly (#968)
    * DUOS-1104 updateFinalVote now fetches all Final votes and updates them with election payload attributes(targeted)
    
    * DUOS-1104 removed updateFinalVote definition and implementation within DatabaseElectionAPI

--- src/main/java/org/broadinstitute/consent/http/service/DatabaseElectionAPI.java
+++ src/main/java/org/broadinstitute/consent/http/service/DatabaseElectionAPI.java
@@ -148,7 +148,6 @@ public class DatabaseElectionAPI extends AbstractElectionAPI {
         } else if (rec.getStatus().equals(ElectionStatus.CLOSED.getValue()) || rec.getStatus().equals(ElectionStatus.FINAL.getValue())) {
             rec.setFinalVoteDate(new Date());
         }
-        updateFinalVote(rec, electionId);
         Election election = electionDAO.findElectionWithFinalVoteById(electionId);
         if (election == null) {
             throw new NotFoundException("Election for specified id does not exist");
@@ -610,17 +609,6 @@ public class DatabaseElectionAPI extends AbstractElectionAPI {
         }
         if (CollectionUtils.isNotEmpty(ids)) {
             electionDAO.updateElectionStatus(ids, status);
-        }
-    }
-
-
-    private void updateFinalVote(Election rec, Integer electionId) {
-        if (rec.getFinalVote() != null) {
-            Vote vote = voteDAO.findVoteByElectionIdAndType(electionId, VoteType.CHAIRPERSON.getValue());
-            vote.setVote(rec.getFinalVote());
-            vote.setCreateDate(rec.getFinalVoteDate());
-            vote.setRationale(rec.getFinalRationale());
-            voteDAO.updateVote(vote.getVote(), vote.getRationale(), vote.getUpdateDate(), vote.getVoteId(), vote.getIsReminderSent(), vote.getElectionId(), vote.getCreateDate(), vote.getHasConcerns());
         }
     }
 
