117e67327d0147c53d6bb26b31466817b09f9fce
Justin Variath Thomas <JVThomas@users.noreply.github.com>
2021-03-31 15:12:21 -0400

    DUOS-977[risk=medium] Implemented Institution Resource (#985)
    * DUOS-977 added resource and service functions to handle INDEX, GET, and POST routes
    
    * DUOS-977 refactored service and resource, currently updating service tests
    
    * DUOS-977 corrected errors in InstitutionServiceTest, currently building up tests for InstitutionResourceTest
    
    * DUOS-977 corrected ExclusionStrategy conditional check, added test for INDEX endpoint
    
    * DUOS-977 finished up InstitutionResource tests
    
    * DUOS-977 corrected spelling on test method names
    
    * DUOS-977 updated api docs with institution info
    
    * DUOS-977 moved utility functions out of resource and into InstitutionUtil, refactored resource and tests
    
    * DUOS-977 refactored InstitutionUtil constructor, refactored Util and Resource tests
    
    * DUOS-977 removed uneeded Mockito reset function calls from service test
    
    * DUOS-977 removed unused Inject import in InstitutionUtilTest, added InsitutionUtil to ConsentApplication
    
    * DUOS-977 addressed PR feedback, updated docs, added additional tests for 404 responses
    
    * DUOS-977 refactor/PR feedback implementation, added more tests for 400 and 404 edge cases
    
    * DUOS-977 corrected api docs, refactored service tests, added comment to instution resource about GsonBuilder
    
    * DUOS-977 added new changelog to masterlist, added tests for unique constraint violations (name)

--- src/test/java/org/broadinstitute/consent/http/db/InstitutionDAOTest.java
+++ src/test/java/org/broadinstitute/consent/http/db/InstitutionDAOTest.java
@@ -4,7 +4,10 @@ import org.broadinstitute.consent.http.models.Institution;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertNull;
+
+import org.junit.Assert;
 import org.junit.Test;
+import org.postgresql.util.PSQLException;
 
 import java.util.Date;
 import java.util.List;
@@ -19,6 +22,26 @@ public class InstitutionDAOTest extends DAOTestHelper {
   }
 
   @Test
+  public void testInsertInstitutionDuplicateName() {
+    Institution institution = createInstitution();
+    Integer userId = institution.getCreateUser();
+    try{
+      institutionDAO.insertInstitution(
+        institution.getName(), 
+        institution.getItDirectorName(), 
+        institution.getItDirectorEmail(), 
+        userId,
+        institution.getCreateDate()
+      );
+      Assert.fail("CREATE should fail due to UNIQUE constraint violation (name)");
+    //JBDI wraps ALL SQL exceptions under the generic class UnableToExecuteStatementException
+    //Test is specifically looking for UNIQUE constraint violations, so I need to catch and unwrap the error to confirm
+    } catch(Exception e) {
+      assertEquals("23505", ((PSQLException)e.getCause()).getSQLState());
+    }
+  }
+
+  @Test
   public void testUpdateInstitutionById() {
     Integer userId = createUser().getDacUserId();
     String newValue = "New Value";
@@ -28,6 +51,18 @@ public class InstitutionDAOTest extends DAOTestHelper {
     assertEquals(updated.getName(), newValue);
     assertEquals(updated.getItDirectorName(), newValue);
     assertEquals(updated.getItDirectorEmail(), newValue);
+  }
+
+  @Test
+  public void testUpdateInstitutionByIdDuplicateName() {
+    Institution institution = createInstitution();
+    Institution secondInstitution = createInstitution();
+    try{
+      institutionDAO.updateInstitutionById(secondInstitution.getId(), institution.getName(), secondInstitution.getItDirectorName(), secondInstitution.getItDirectorEmail(), secondInstitution.getUpdateUser(), secondInstitution.getUpdateDate());
+      Assert.fail("UPDATE should fail due to UNIQUE constraint violation (name)");
+    }catch(Exception e) {
+      assertEquals("23505", ((PSQLException) e.getCause()).getSQLState());
+    }
   }
 
   @Test
