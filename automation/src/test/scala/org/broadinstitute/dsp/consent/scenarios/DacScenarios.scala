cfd282806d6cdb1530cc364dbc5970f32a135a1a
rfricke-asymmetrik <rfricke@asymmetrik.com>
2021-04-19 10:14:28 -0400

    DUOS-1163 Scala update log fixes

--- automation/src/test/scala/org/broadinstitute/dsp/consent/scenarios/DacScenarios.scala
+++ automation/src/test/scala/org/broadinstitute/dsp/consent/scenarios/DacScenarios.scala
@@ -1,15 +1,16 @@
 package org.broadinstitute.dsp.consent.scenarios
 
-import io.gatling.core.Predef.{Simulation, scenario}
+import io.gatling.core.Predef.{Simulation, scenario, exec}
 import org.broadinstitute.dsp.consent.{TestConfig, TestRunner}
 import org.broadinstitute.dsp.consent.requests.Requests
 import io.netty.handler.codec.http.HttpResponseStatus._
+import org.broadinstitute.dsp.consent.scenarios.GroupedScenarios._
 
 class DacScenarios  extends Simulation with TestRunner {
 
   runScenarios(
     List(
-      scenario("Dac List Scenario").exec(Requests.Dac.list(OK.code, TestConfig.adminHeader))
+      GroupedScenario("Dac List Scenario") { exec(Requests.Dac.list(OK.code, TestConfig.adminHeader)) }
     )
   )
 
