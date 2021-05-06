7dabdf9a7a577950c1cdc3682862cbae2595cdba
raejohanek <rjohanek@broadinstitute.org>
2021-05-06 14:24:45 -0400

    remove one bean mapper and map directly instead

--- src/main/java/org/broadinstitute/consent/http/db/mapper/InstitutionWithUsersReducer.java
+++ src/main/java/org/broadinstitute/consent/http/db/mapper/InstitutionWithUsersReducer.java
@@ -1,5 +1,6 @@
 package org.broadinstitute.consent.http.db.mapper;
 
+import java.sql.Timestamp;
 import java.util.Map;
 import java.util.Objects;
 
@@ -37,18 +38,14 @@ public class InstitutionWithUsersReducer implements LinkedHashMapRowReducer<Inte
     }
 
     User update_user = new User();
-    if (Objects.nonNull(rowView.getColumn("u2_dacuserid", Integer.class))) {
-        update_user = rowView.getRow(User.class);
-    }
-
-    // Status is an enum type and we need to get the string value
-    try {
-      if (Objects.nonNull(rowView.getColumn("u2_status", Integer.class))) {
-        update_user.setStatus(getStatus(rowView, "u2_status"));
-      }
-    } catch (MappingException e) {
-      // Ignore any attempt to map a column that doesn't exist
-    }
+    update_user.setDacUserId(rowView.getColumn("u2_dacuserid", Integer.class));
+    update_user.setEmail(rowView.getColumn("u2_email", String.class));
+    update_user.setDisplayName(rowView.getColumn("u2_displayname", String.class));
+    update_user.setCreateDate(rowView.getColumn("u2_createdate", Timestamp.class));
+    update_user.setAdditionalEmail(rowView.getColumn("u2_additional_email", String.class));
+    update_user.setEmailPreference(rowView.getColumn("u2_email_preference", Boolean.class));
+    update_user.setStatus(getStatus(rowView, "u2_status"));
+    update_user.setRationale(rowView.getColumn("u2_rationale", String.class));
 
     institution.setCreateUser(create_user);
     institution.setUpdateUser(update_user);
