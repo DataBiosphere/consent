737571adcf2d54e492415754a0f7a9175e4c76cb
raejohanek <rjohanek@broadinstitute.org>
2021-05-10 14:39:50 -0400

    register bean mapper and row reducer

--- src/main/java/org/broadinstitute/consent/http/db/LibraryCardDAO.java
+++ src/main/java/org/broadinstitute/consent/http/db/LibraryCardDAO.java
@@ -1,29 +1,29 @@
 package org.broadinstitute.consent.http.db;
+import org.broadinstitute.consent.http.db.mapper.LibraryCardReducer;
 import org.broadinstitute.consent.http.models.LibraryCard;
-import org.broadinstitute.consent.http.db.mapper.LibraryCardMapper;
-import org.jdbi.v3.sqlobject.config.RegisterRowMapper;
+import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
 import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.statement.GetGeneratedKeys;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+import org.jdbi.v3.sqlobject.statement.UseRowReducer;
 import org.jdbi.v3.sqlobject.transaction.Transactional;
 
 import java.util.Date;
 import java.util.List;
 
-@RegisterRowMapper(LibraryCardMapper.class)
 public interface LibraryCardDAO extends Transactional<LibraryCardDAO> {
 
   @SqlUpdate("INSERT INTO library_card " +
     " (user_id, institution_id, era_commons_id, user_name, user_email, create_user_id, create_date) " +
-    " VALUES (:userId, :institutionId, :eraCommonsId, :userName, :userEmail, :createUser, :createDate)")
+    " VALUES (:userId, :institutionId, :eraCommonsId, :userName, :userEmail, :createUserId, :createDate)")
   @GetGeneratedKeys
   Integer insertLibraryCard(@Bind("userId") Integer userId,
                             @Bind("institutionId") Integer institutionId,
                             @Bind("eraCommonsId") String eraCommonsId, 
                             @Bind("userName") String userName,
                             @Bind("userEmail") String userEmail,
-                            @Bind("createUser") Integer createUser,
+                            @Bind("createUserId") Integer createUserId,
                             @Bind("createDate") Date createDate);
 
   @SqlUpdate("UPDATE library_card SET " +
@@ -33,7 +33,7 @@ public interface LibraryCardDAO extends Transactional<LibraryCardDAO> {
     " era_commons_id = :eraCommonsId, " +
     " user_name = :userName, " +
     " user_email = :userEmail, " +
-    " update_user_id = :updateUser, " +
+    " update_user_id = :updateUserId, " +
     " update_date = :updateDate " +
     " WHERE id = :libraryCardId")
   void updateLibraryCardById(@Bind("libraryCardId") Integer libraryCardId,
@@ -42,18 +42,24 @@ public interface LibraryCardDAO extends Transactional<LibraryCardDAO> {
                          @Bind("eraCommonsId") String eraCommonsId,
                          @Bind("userName") String userName,
                          @Bind("userEmail") String userEmail,
-                         @Bind("updateUser") Integer updateUser,
+                         @Bind("updateUserId") Integer updateUserId,
                          @Bind("updateDate") Date updateDate);
 
   @SqlUpdate("DELETE FROM library_card WHERE id = :libraryCardId")
   void deleteLibraryCardById(@Bind("libraryCardId") Integer libraryCardId);
 
+  @RegisterBeanMapper(value = LibraryCard.class)
+  @UseRowReducer(LibraryCardReducer.class)
   @SqlQuery("SELECT * FROM library_card WHERE id = :libraryCardId")
   LibraryCard findLibraryCardById(@Bind("libraryCardId") Integer libraryCardId);
 
+  @RegisterBeanMapper(value = LibraryCard.class)
+  @UseRowReducer(LibraryCardReducer.class)
   @SqlQuery("SELECT * FROM library_card WHERE user_id = :userId")
   List<LibraryCard> findLibraryCardsByUserId(@Bind("userId") Integer userId);
 
+  @RegisterBeanMapper(value = LibraryCard.class)
+  @UseRowReducer(LibraryCardReducer.class)
   @SqlQuery("SELECT * FROM library_card")
   List<LibraryCard> findAllLibraryCards();
 
