e622e935002ddcd641370e55c399e607f02643cc
Rae Johanek <77015255+raejohanek@users.noreply.github.com>
2021-05-11 14:09:14 -0400

    DUOS-1062[risk=no]LC DB, DAO Classes (#1065)
    * create database table
    
    * created all DAO calls
    
    * add library card to DAO test helper
    
    * create model class for testing
    
    * create library card reducer and register in DAO
    
    * create tests

---
+++ src/main/java/org/broadinstitute/consent/http/db/LibraryCardDAO.java
@@ -0,0 +1,66 @@
+package org.broadinstitute.consent.http.db;
+import org.broadinstitute.consent.http.db.mapper.LibraryCardReducer;
+import org.broadinstitute.consent.http.models.LibraryCard;
+import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
+import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.statement.GetGeneratedKeys;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+import org.jdbi.v3.sqlobject.statement.UseRowReducer;
+import org.jdbi.v3.sqlobject.transaction.Transactional;
+
+import java.util.Date;
+import java.util.List;
+
+public interface LibraryCardDAO extends Transactional<LibraryCardDAO> {
+
+  @SqlUpdate("INSERT INTO library_card " +
+    " (user_id, institution_id, era_commons_id, user_name, user_email, create_user_id, create_date) " +
+    " VALUES (:userId, :institutionId, :eraCommonsId, :userName, :userEmail, :createUserId, :createDate)")
+  @GetGeneratedKeys
+  Integer insertLibraryCard(@Bind("userId") Integer userId,
+                            @Bind("institutionId") Integer institutionId,
+                            @Bind("eraCommonsId") String eraCommonsId, 
+                            @Bind("userName") String userName,
+                            @Bind("userEmail") String userEmail,
+                            @Bind("createUserId") Integer createUserId,
+                            @Bind("createDate") Date createDate);
+
+  @SqlUpdate("UPDATE library_card SET " +
+    " id = :libraryCardId, " +
+    " user_id = :userId, " +
+    " institution_id = :institutionId, " +
+    " era_commons_id = :eraCommonsId, " +
+    " user_name = :userName, " +
+    " user_email = :userEmail, " +
+    " update_user_id = :updateUserId, " +
+    " update_date = :updateDate " +
+    " WHERE id = :libraryCardId")
+  void updateLibraryCardById(@Bind("libraryCardId") Integer libraryCardId,
+                         @Bind("userId") Integer userId, 
+                         @Bind("institutionId") Integer institutionId,
+                         @Bind("eraCommonsId") String eraCommonsId,
+                         @Bind("userName") String userName,
+                         @Bind("userEmail") String userEmail,
+                         @Bind("updateUserId") Integer updateUserId,
+                         @Bind("updateDate") Date updateDate);
+
+  @SqlUpdate("DELETE FROM library_card WHERE id = :libraryCardId")
+  void deleteLibraryCardById(@Bind("libraryCardId") Integer libraryCardId);
+
+  @RegisterBeanMapper(value = LibraryCard.class)
+  @UseRowReducer(LibraryCardReducer.class)
+  @SqlQuery("SELECT * FROM library_card WHERE id = :libraryCardId")
+  LibraryCard findLibraryCardById(@Bind("libraryCardId") Integer libraryCardId);
+
+  @RegisterBeanMapper(value = LibraryCard.class)
+  @UseRowReducer(LibraryCardReducer.class)
+  @SqlQuery("SELECT * FROM library_card WHERE user_id = :userId")
+  List<LibraryCard> findLibraryCardsByUserId(@Bind("userId") Integer userId);
+
+  @RegisterBeanMapper(value = LibraryCard.class)
+  @UseRowReducer(LibraryCardReducer.class)
+  @SqlQuery("SELECT * FROM library_card")
+  List<LibraryCard> findAllLibraryCards();
+
+}
---
+++ src/test/java/org/broadinstitute/consent/http/db/LibraryCardDAOTest.java
@@ -0,0 +1,120 @@
+package org.broadinstitute.consent.http.db;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Date;
+import java.util.List;
+
+import com.google.gson.Gson;
+
+import org.postgresql.util.PSQLException;
+import org.postgresql.util.PSQLState;
+import org.apache.commons.lang3.RandomUtils;
+import org.broadinstitute.consent.http.models.LibraryCard;
+import org.junit.Test;
+
+public class LibraryCardDAOTest extends DAOTestHelper {
+
+  @Test
+  public void testInsertLibraryCard() {
+    LibraryCard card = createLibraryCard();
+    List<LibraryCard> all = libraryCardDAO.findAllLibraryCards();
+    assertTrue(all.contains(card));
+  }
+
+  @Test
+  public void testInsertLibraryCardNegative() {
+    Integer userId = createUser().getDacUserId();
+    Integer institutionId = createInstitution().getId();
+    String stringValue = "value";
+    try { 
+        libraryCardDAO.insertLibraryCard(0, institutionId, stringValue, stringValue, stringValue, userId, new Date());
+    } catch (Exception e) {
+        assertEquals(PSQLState.FOREIGN_KEY_VIOLATION.getState(), ((PSQLException)e.getCause()).getSQLState());
+    }
+    try { 
+        libraryCardDAO.insertLibraryCard(userId, 0, stringValue, stringValue, stringValue, userId, new Date());
+    } catch (Exception e) {
+        assertEquals(PSQLState.FOREIGN_KEY_VIOLATION.getState(), ((PSQLException)e.getCause()).getSQLState());
+    }
+    try { 
+        libraryCardDAO.insertLibraryCard(userId, institutionId, stringValue, stringValue, stringValue, 0, new Date());
+    } catch (Exception e) {
+        assertEquals(PSQLState.FOREIGN_KEY_VIOLATION.getState(), ((PSQLException)e.getCause()).getSQLState());
+    }
+  }
+
+  @Test
+  public void testUpdateLibraryCardById() {
+    Integer userId = createUser().getDacUserId();
+    String newValue = "New Value";
+    LibraryCard card = createLibraryCard();
+    Integer id = card.getId();
+    card.setUserName("name");
+    Integer institutionId = createInstitution().getId();
+    libraryCardDAO.updateLibraryCardById(id, userId, institutionId, newValue, newValue, newValue, userId, new Date());
+    LibraryCard updated = libraryCardDAO.findLibraryCardById(id);
+    assertEquals(newValue, updated.getEraCommonsId());
+    assertEquals(institutionId, updated.getInstitutionId());
+    assertEquals(newValue, updated.getUserName());
+    assertEquals(userId, updated.getUpdateUserId());
+  }
+
+  @Test
+  public void testUpdateLibraryCardByIdNegative() {
+    Integer userId = createUser().getDacUserId();
+    Integer institutionId = createInstitution().getId();
+    String newValue = "New Value";
+    try {
+      libraryCardDAO.updateLibraryCardById(0, userId, institutionId, newValue, newValue, newValue, userId, new Date());
+    } catch (Exception e) {
+        assertEquals(PSQLState.UNIQUE_VIOLATION.getState(), ((PSQLException)e.getCause()).getSQLState());
+    }
+  }
+
+  @Test
+  public void testDeleteLibraryCardById() {
+    LibraryCard card = createLibraryCard();
+    Integer id = card.getId();
+    libraryCardDAO.deleteLibraryCardById(id);
+    assertNull(libraryCardDAO.findLibraryCardById(id));
+  }
+
+  @Test
+  public void testDeleteLbraryCardByIdNegative() {
+    try{
+      libraryCardDAO.deleteLibraryCardById(RandomUtils.nextInt(1, 1000));
+    } catch (Exception e) {
+      assertEquals(PSQLState.UNIQUE_VIOLATION.getState(), ((PSQLException)e.getCause()).getSQLState());
+    }
+  }
+
+  @Test
+  public void testFindLibraryCardById() {
+    LibraryCard card = createLibraryCard();
+    Integer id = card.getId();
+    LibraryCard cardFromDAO = libraryCardDAO.findLibraryCardById(id);
+    assertEquals(cardFromDAO.getUserId(), card.getUserId());
+    assertEquals(cardFromDAO.getUserName(), card.getUserName());
+    assertEquals(cardFromDAO.getUserEmail(), card.getUserEmail());
+    assertEquals(cardFromDAO.getCreateUserId(), card.getCreateUserId());
+    assertEquals(cardFromDAO.getCreateDate(), card.getCreateDate());
+  }
+
+  @Test
+  public void testFindLibraryCardByIdNegative() {
+    LibraryCard cardFromDAO = libraryCardDAO.findLibraryCardById(RandomUtils.nextInt(100, 200));
+    assertNull(cardFromDAO);
+  }
+
+  @Test
+  public void testFindAllLibraryCards() {
+    List<LibraryCard> cardList = libraryCardDAO.findAllLibraryCards();
+    assertEquals(0, cardList.size());
+    createLibraryCard();
+    List<LibraryCard> cardListUpdated = libraryCardDAO.findAllLibraryCards();
+    assertEquals(1, cardListUpdated.size());
+  }
+}
