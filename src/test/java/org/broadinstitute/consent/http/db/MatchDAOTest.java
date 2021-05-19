c3a18176bb96a973b3431b76ea501a015c768c28
Greg Rushton <grushton@broadinstitute.org>
2021-05-19 16:23:23 -0400

    imports

--- src/test/java/org/broadinstitute/consent/http/db/MatchDAOTest.java
+++ src/test/java/org/broadinstitute/consent/http/db/MatchDAOTest.java
@@ -1,18 +1,12 @@
 package org.broadinstitute.consent.http.db;
 
-import org.broadinstitute.consent.http.models.Consent;
-import org.broadinstitute.consent.http.models.Dac;
-import org.broadinstitute.consent.http.models.DataAccessRequest;
 import org.broadinstitute.consent.http.models.Match;
 import org.junit.Test;
 
-import java.util.Date;
 import java.util.List;
-import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 public class MatchDAOTest extends DAOTestHelper {
 
