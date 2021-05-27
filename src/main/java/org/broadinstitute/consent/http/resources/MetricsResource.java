93f4fabd72ef7cca33890a571cefb45219a471ae
raejohanek <rjohanek@broadinstitute.org>
2021-05-27 12:26:38 -0400

    remove /api from path

--- src/main/java/org/broadinstitute/consent/http/resources/MetricsResource.java
+++ src/main/java/org/broadinstitute/consent/http/resources/MetricsResource.java
@@ -13,7 +13,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("api/metrics")
+@Path("/metrics")
 public class MetricsResource extends Resource {
 
   private final MetricsService metricsService;
