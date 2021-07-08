3d334d1a3797c1dbf135b0c0cad0f89f9413752c
Rae Johanek <77015255+raejohanek@users.noreply.github.com>
2021-07-08 10:22:57 -0400

    DUOS-1067[risk=no]Deprecate White List Service (#1137)
    * delete all whitelist files
    
    * delete documentation from api docs
    
    * delete refs from consent application
    
    * delete methods and tests relating to whitelist from GCS service

--- src/test/java/org/broadinstitute/consent/http/cloudstore/GCSServiceTest.java
+++ src/test/java/org/broadinstitute/consent/http/cloudstore/GCSServiceTest.java
@@ -7,25 +7,17 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import com.google.api.client.http.GenericUrl;
-import com.google.api.gax.paging.Page;
 import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
-import com.google.cloud.storage.Bucket;
 import com.google.cloud.storage.Storage;
 import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.util.Date;
-import java.util.List;
-import java.util.Spliterator;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import javax.ws.rs.core.MediaType;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.broadinstitute.consent.http.configurations.StoreConfiguration;
-import org.broadinstitute.consent.http.service.WhitelistService;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -39,15 +31,6 @@ public class GCSServiceTest {
 
     @Mock
     private Blob blob;
-
-    @Mock
-    private Bucket bucket;
-
-    @Mock
-    private Page<Blob> blobs;
-
-    @Mock
-    private Iterable<Blob> iterable;
 
     private StoreConfiguration config;
 
@@ -69,37 +52,9 @@ public class GCSServiceTest {
     }
 
     @Test
-    public void testPostWhitelist() {
-        String filename = "filename.txt";
-        when(blob.getMediaLink()).thenReturn(config.getEndpoint() + filename);
-        when(storage.create(any(BlobInfo.class), any())).thenReturn(blob);
-        when(storage.create(any(BlobInfo.class), any(), new Storage.BlobTargetOption[0])).thenReturn(blob);
-        initStore();
-
-        GenericUrl url = service.postWhitelist("content", filename);
-        assertNotNull(url);
-    }
-
-    @Test
-    public void testGetMostRecentWhitelist() throws Exception {
-        int count = 3; // Count value is used in Blob name, create time, and content
-        Spliterator<Blob> blobSpliterator = makeWhitelistBlobSpliterator(count);
-        when(iterable.spliterator()).thenReturn(blobSpliterator);
-        when(blobs.iterateAll()).thenReturn(iterable);
-        when(bucket.list()).thenReturn(blobs);
-        when(storage.get(any(String.class))).thenReturn(bucket);
-
-        initStore();
-        String whitelistData = service.getMostRecentWhitelist();
-        assertNotNull(whitelistData);
-        assertTrue(whitelistData.contains(String.valueOf(count)));
-    }
-
-    @Test
     public void testStoreDocument() throws Exception {
         String filename = "filename.txt";
         BlobId blobId = BlobId.of(config.getEndpoint(), filename);
-        when(blobId.getName()).thenReturn(filename);
         when(blob.getBlobId()).thenReturn(blobId);
         when(storage.create(any(BlobInfo.class), any())).thenReturn(blob);
         when(storage.create(any(BlobInfo.class), any(), new Storage.BlobTargetOption[0])).thenReturn(blob);
@@ -146,28 +101,6 @@ public class GCSServiceTest {
         initStore();
         boolean deleted = service.deleteDocument(RandomStringUtils.random(10));
         assertTrue(deleted);
-    }
-
-    /**
-     * Make an iterator of Blobs so we can mock out what Google will return for data.
-     * @return Iterator<Blob>
-     */
-    @SuppressWarnings("SameParameterValue")
-    private Spliterator<Blob> makeWhitelistBlobSpliterator(int i) {
-        List<Blob> blobs = IntStream.range(1, i + 1).
-                mapToObj(this::makeWhitelistBlob).
-                collect(Collectors.toList());
-        return blobs.spliterator();
-    }
-
-    private Blob makeWhitelistBlob(int i) {
-        Blob blob = mock(Blob.class);
-        when(blob.isDirectory()).thenReturn(false);
-        when(blob.getMediaLink()).thenReturn("http://localhost/bucket/blob" + i);
-        when(blob.getName()).thenReturn("bucket/blob_" + i + "_" + WhitelistService.WHITELIST_FILE_PREFIX + RandomStringUtils.random(10, false, true));
-        when(blob.getCreateTime()).thenReturn(Long.valueOf(i));
-        when(blob.getContent()).thenReturn(("blob_" + i).getBytes());
-        return blob;
     }
 
 }
