decf7007cb51a15bed1cdec3b3472570970742df
Greg Rushton <grushton@broadinstitute.org>
2021-07-20 15:47:04 -0400

    rm unused/deprecated and other cleanup

--- src/main/java/org/broadinstitute/consent/http/db/UserDAO.java
+++ src/main/java/org/broadinstitute/consent/http/db/UserDAO.java
@@ -1,15 +1,9 @@
 package org.broadinstitute.consent.http.db;
 
-import java.util.Collection;
-import java.util.Date;
-import java.util.List;
-import java.util.Set;
-import org.broadinstitute.consent.http.db.mapper.UserWithRolesReducer;
-import org.broadinstitute.consent.http.db.mapper.UserWithPropertiesReducer;
 import org.broadinstitute.consent.http.db.mapper.UserWithRolesMapper;
+import org.broadinstitute.consent.http.db.mapper.UserWithRolesReducer;
 import org.broadinstitute.consent.http.models.Institution;
 import org.broadinstitute.consent.http.models.User;
-import org.broadinstitute.consent.http.models.UserProperty;
 import org.broadinstitute.consent.http.models.UserRole;
 import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
 import org.jdbi.v3.sqlobject.customizer.Bind;
@@ -21,6 +15,11 @@ import org.jdbi.v3.sqlobject.statement.UseRowMapper;
 import org.jdbi.v3.sqlobject.statement.UseRowReducer;
 import org.jdbi.v3.sqlobject.transaction.Transactional;
 
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
 @SuppressWarnings("SqlDialectInspection")
 public interface UserDAO extends Transactional<UserDAO> {
 
@@ -31,7 +30,7 @@ public interface UserDAO extends Transactional<UserDAO> {
     @SqlQuery("SELECT "
         + "     u.dacuserid, u.email, u.displayname, u.createdate, u.additional_email, "
         + "     u.email_preference, u.status, u.rationale, u.institution_id, "
-        + "     i.institution_id as i_id, i.institution_name as i_name, " 
+        + "     i.institution_id as i_id, i.institution_name as i_name, "
         + "     i.it_director_name as i_it_director_name, i.it_director_email as i_it_director_email, "
         + "     i.create_date as i_create_date, i.update_date as i_update_date, "
         + "     ur.user_role_id, ur.user_id, ur.role_id, ur.dac_id, r.name  "
@@ -41,18 +40,6 @@ public interface UserDAO extends Transactional<UserDAO> {
         + " LEFT JOIN institution i ON u.institution_id = i.institution_id"
         + " WHERE u.dacuserid = :dacUserId")
     User findUserById(@Bind("dacUserId") Integer dacUserId);
-
-    @RegisterBeanMapper(value = User.class)
-    @RegisterBeanMapper(value = UserProperty.class)
-    @UseRowReducer(UserWithPropertiesReducer.class)
-    @SqlQuery("SELECT "
-        + "     u.dacuserid, u.email, u.displayname, u.createdate, u.additional_email, "
-        + "     u.email_preference, u.status, u.rationale, u.institution_id, "
-        + "     p.propertykey, p.propertyvalue"
-        + " FROM dacuser u "
-        + " LEFT JOIN user_property p ON p.userid = u.dacuserid "
-        + " WHERE u.dacuserid = :dacUserId")
-    User findUserWithPropertiesById(@Bind("dacUserId") Integer dacUserId);
 
     @RegisterBeanMapper(value = User.class)
     @UseRowReducer(UserWithRolesReducer.class)
@@ -111,10 +98,6 @@ public interface UserDAO extends Transactional<UserDAO> {
                        @Bind("id") Integer id,
                        @Bind("additionalEmail") String additionalEmail,
                        @Bind("institutionId") Integer institutionId);
-
-    @Deprecated // Use deleteUserById instead
-    @SqlUpdate("DELETE FROM dacuser WHERE LOWER(email) = LOWER(:email)")
-    void deleteUserByEmail(@Bind("email") String email);
 
     @SqlUpdate("delete from dacuser where dacuserid = :id")
     void deleteUserById(@Bind("id") Integer id);
