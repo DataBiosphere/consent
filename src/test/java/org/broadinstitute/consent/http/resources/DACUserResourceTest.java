a4c6268654eeaea4773cddc2ce734e3e81789773
Greg Rushton <rushtong@users.noreply.github.com>
2021-07-20 09:15:58 -0400

    [DUOS-1350][risk=no] Omit the user's create date on update (#1176)
    * filter unnecessary parsing on create/update

--- src/test/java/org/broadinstitute/consent/http/resources/DACUserResourceTest.java
+++ src/test/java/org/broadinstitute/consent/http/resources/DACUserResourceTest.java
@@ -159,35 +159,18 @@ public class DACUserResourceTest {
     }
 
     @Test
-    public void testConvertJsonToDACUserNumericDateCase() {
-        String jsonRole = "[{\"roleId\": 1, \"name\":\"name\", \"what\": \"Huh?\", \"rationale\": \"rationale\", \"status\": \"pending\"}]";
-        String json = "{\"dacUserId\": 1, \"email\":\"email\", \"what\": \"Huh?\", \"createDate\": 1302828677828, \"additionalEmail\": \"additionalEmail\", \"emailPreference\": false, \"roles\": " + jsonRole + "}";
-        User user = new User(json);
-        Assert.assertNotNull(user);
-        Assert.assertNotNull(user.getCreateDate());
-        Assert.assertEquals(user.getDacUserId().intValue(), 1);
-        Assert.assertEquals(user.getEmail(), "email");
-        Assert.assertEquals(user.getAdditionalEmail(), "additionalEmail");
-        Assert.assertEquals(user.getEmailPreference(), false);
-        Assert.assertFalse(user.getRoles().isEmpty());
-        Assert.assertEquals(user.getRoles().get(0).getRoleId().intValue(), 1);
-        System.out.println(user.toString());
-    }
-
-    @Test
-    public void testConvertJsonToDACUserStringDateCase() {
+    public void testConvertJsonToDACUserDateIgnoredCase() {
         String jsonRole = "[{\"roleId\": 1, \"name\":\"name\", \"what\": \"Huh?\", \"rationale\": \"rationale\", \"status\": \"pending\"}]";
         String json = "{\"dacUserId\": 1, \"email\":\"email\", \"what\": \"Huh?\", \"createDate\": \"Oct 28, 2020\", \"additionalEmail\": \"additionalEmail\", \"emailPreference\": false, \"roles\": " + jsonRole + "}";
         User user = new User(json);
         Assert.assertNotNull(user);
-        Assert.assertNotNull(user.getCreateDate());
+        Assert.assertNull(user.getCreateDate());
         Assert.assertEquals(user.getDacUserId().intValue(), 1);
         Assert.assertEquals(user.getEmail(), "email");
         Assert.assertEquals(user.getAdditionalEmail(), "additionalEmail");
         Assert.assertEquals(user.getEmailPreference(), false);
         Assert.assertFalse(user.getRoles().isEmpty());
         Assert.assertEquals(user.getRoles().get(0).getRoleId().intValue(), 1);
-        System.out.println(user.toString());
     }
 
     @Test
@@ -203,7 +186,6 @@ public class DACUserResourceTest {
         Assert.assertEquals(user.getEmailPreference(), false);
         Assert.assertFalse(user.getRoles().isEmpty());
         Assert.assertEquals(user.getRoles().get(0).getRoleId().intValue(), 1);
-        System.out.println(user.toString());
     }
 
     private User createDacUser(UserRoles roles) {
