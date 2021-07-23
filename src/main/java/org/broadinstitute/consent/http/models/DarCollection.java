91e0c1aec8ce7a5cbb3425b306b24e7a28f53de0
raejohanek <rjohanek@broadinstitute.org>
2021-07-23 16:28:50 -0400

    creat dar collections model

---
+++ src/main/java/org/broadinstitute/consent/http/models/DarCollection.java
@@ -0,0 +1,79 @@
+package org.broadinstitute.consent.http.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.sql.Timestamp;
+
+//represents a multi-dataset access request
+public class DarCollection {
+
+  @JsonProperty
+  Integer darCollectionId;
+
+  @JsonProperty
+  String darCode;
+
+  @JsonProperty
+  Timestamp createDate;
+
+  //userId or User?
+  @JsonProperty
+  User createUser;
+
+  @JsonProperty
+  Timestamp updateDate;
+
+  @JsonProperty
+  User updateUser;
+
+  //empty constructor sets all null values except create Date
+  public DarCollection() {this.createDate = new Timestamp(System.currentTimeMillis()); }
+
+  public Integer getDarCollectionId() {
+    return darCollectionId;
+  }
+
+  public void setDarCollectionId(Integer darCollectionId) {
+    this.darCollectionId = darCollectionId;
+  }
+
+  public String getDarCode() {
+    return darCode;
+  }
+
+  public void setDarCode(String darCode) {
+    this.darCode = darCode;
+  }
+
+  public Timestamp getCreateDate() {
+    return createDate;
+  }
+
+  public void setCreateDate(Timestamp createDate) {
+    this.createDate = createDate;
+  }
+
+  public User getCreateUser() {
+    return createUser;
+  }
+
+  public void setCreateUser(User createUser) {
+    this.createUser = createUser;
+  }
+
+  public Timestamp getUpdateDate() {
+    return updateDate;
+  }
+
+  public void setUpdateDate(Timestamp updateDate) {
+    this.updateDate = updateDate;
+  }
+
+  public User getUpdateUser() {
+    return updateUser;
+  }
+
+  public void setUpdateUser(User updateUser) {
+    this.updateUser = updateUser;
+  }
+
+}
