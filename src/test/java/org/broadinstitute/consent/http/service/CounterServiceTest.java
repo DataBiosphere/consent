812c41b259498692e028d1c2e421c2d89b86b6e8
Greg Rushton <grushton@broadinstitute.org>
2021-07-26 11:39:48 -0400

    fix test

--- src/test/java/org/broadinstitute/consent/http/service/CounterServiceTest.java
+++ src/test/java/org/broadinstitute/consent/http/service/CounterServiceTest.java
@@ -1,15 +1,14 @@
 package org.broadinstitute.consent.http.service;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
-
 import org.broadinstitute.consent.http.db.CounterDAO;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 public class CounterServiceTest {
 
@@ -20,7 +19,7 @@ public class CounterServiceTest {
 
     @Before
     public void setUp() {
-        MockitoAnnotations.initMocks(this);
+        MockitoAnnotations.openMocks(this);
     }
 
     private void initService() {
@@ -30,8 +29,7 @@ public class CounterServiceTest {
     @Test
     public void testGetNextDarSequence() {
         int count = 10;
-        doNothing().when(counterDAO).incrementCountByName(any());
-        when(counterDAO.getMaxCountByName(any())).thenReturn(count);
+        when(counterDAO.incrementCountByName(any())).thenReturn(count);
         initService();
 
         Integer seq = service.getNextDarSequence();
