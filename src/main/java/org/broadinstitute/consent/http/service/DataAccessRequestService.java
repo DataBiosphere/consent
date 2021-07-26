04a9340a9eb1e4635746fcaddab5b55d8a0de1fe
Greg Rushton <grushton@broadinstitute.org>
2021-07-26 13:38:04 -0400

    handle empty user id list correctly

--- src/main/java/org/broadinstitute/consent/http/service/DataAccessRequestService.java
+++ src/main/java/org/broadinstitute/consent/http/service/DataAccessRequestService.java
@@ -298,7 +298,7 @@ public class DataAccessRequestService {
         Set<Dac> dacs = datasetIds.isEmpty() ? Collections.emptySet() : dacDAO.findDacsForDatasetIds(datasetIds);
         // Batch call 4
         List<Integer> userIds = dataAccessRequests.stream().map(DataAccessRequest::getUserId).collect(toList());
-        Collection<User> researchers = userDAO.findUsers(userIds);
+        Collection<User> researchers = userIds.isEmpty() ? Collections.emptyList() : userDAO.findUsers(userIds);
         Map<Integer, User> researcherMap = researchers.stream()
                 .collect(Collectors.toMap(User::getDacUserId, Function.identity()));
 
