f2bc7560f9685d627402aab43bf4696cbf4f81dd
Greg Rushton <grushton@broadinstitute.org>
2021-08-16 14:10:18 -0400

    PR feedback

--- src/main/java/org/broadinstitute/consent/http/authentication/OAuthAuthenticator.java
+++ src/main/java/org/broadinstitute/consent/http/authentication/OAuthAuthenticator.java
@@ -33,11 +33,7 @@ public class OAuthAuthenticator implements Authenticator<String, AuthUser> {
         try {
             validateAudience(bearer);
             GoogleUser googleUser = getUserInfo(bearer);
-            AuthUser user = new AuthUser()
-                    .setGoogleUser(googleUser)
-                    .setAuthToken(bearer)
-                    .setEmail(googleUser.getEmail())
-                    .setName(googleUser.getName());
+            AuthUser user = new AuthUser(googleUser).setAuthToken(bearer);
             return Optional.of(user);
         } catch (Exception e) {
             logger.error("Error authenticating credentials: " + e.getMessage());
