a0c6fa8ed617797c5341b33a22ae423511405460
Greg Rushton <grushton@broadinstitute.org>
2021-09-10 08:41:06 -0400

    formatting

--- src/main/java/org/broadinstitute/consent/http/db/UserDAO.java
+++ src/main/java/org/broadinstitute/consent/http/db/UserDAO.java
@@ -192,13 +192,13 @@ public interface UserDAO extends Transactional<UserDAO> {
             " AND lc.institution_id = :institutionId")
     List<User> getCardsForUnregisteredUsers(@Bind("institutionId") Integer institutionId);
 
-  @RegisterBeanMapper(value = User.class)
-  @RegisterBeanMapper(value = UserRole.class)
-  @RegisterBeanMapper(value = LibraryCard.class, prefix = "lc")
-  @RegisterBeanMapper(value = Institution.class, prefix = "lci")
-  @UseRowReducer(UserWithRolesReducer.class)
-  @SqlQuery(
-      " SELECT du.*, r.name, ur.role_id, ur.user_role_id, ur.dac_id, ur.user_id, "
+    @RegisterBeanMapper(value = User.class)
+    @RegisterBeanMapper(value = UserRole.class)
+    @RegisterBeanMapper(value = LibraryCard.class, prefix = "lc")
+    @RegisterBeanMapper(value = Institution.class, prefix = "lci")
+    @UseRowReducer(UserWithRolesReducer.class)
+    @SqlQuery(
+            " SELECT du.*, r.name, ur.role_id, ur.user_role_id, ur.dac_id, ur.user_id, "
           + " lc.id AS lc_id , lc.user_id AS lc_user_id, lc.institution_id AS lc_institution_id, "
           + " lc.era_commons_id AS lc_era_commons_id, lc.user_name AS lc_user_name, lc.user_email AS lc_user_email, "
           + " lc.create_user_id AS lc_create_user_id, lc.create_date AS lc_create_date, "
@@ -212,7 +212,7 @@ public interface UserDAO extends Transactional<UserDAO> {
           + " INNER JOIN library_card lc ON lc.user_id = du.dacuserid "
           + " LEFT JOIN institution i ON lc.institution_id = i.institution_id"
           + " WHERE (du.institution_id != :institutionId OR du.institution_id IS NULL) AND lc.institution_id = :institutionId")
-  List<User> getUsersOutsideInstitutionWithCards(@Bind("institutionId") Integer institutionId);
+    List<User> getUsersOutsideInstitutionWithCards(@Bind("institutionId") Integer institutionId);
 
     @RegisterBeanMapper(value = User.class)
     @RegisterBeanMapper(value = UserRole.class)
