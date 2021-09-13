e69b40bfdc9d03a34bb95720f4a5b02a2601867a
JVThomas <jthomas0209@gmail.com>
2021-09-13 15:03:44 -0400

    DUOS-586 remove unused import

--- src/main/java/org/broadinstitute/consent/http/models/DataUse.java
+++ src/main/java/org/broadinstitute/consent/http/models/DataUse.java
@@ -9,7 +9,6 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 @SuppressWarnings({"unused", "SameParameterValue", "WeakerAccess"})
