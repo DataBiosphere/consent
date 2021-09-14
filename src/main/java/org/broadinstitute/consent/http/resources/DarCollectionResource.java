97c4877ed8edd0da0ca7a27cdd6269b2f0dd04c9
Greg Rushton <grushton@broadinstitute.org>
2021-09-14 13:56:37 -0400

    new api methods

--- src/main/java/org/broadinstitute/consent/http/resources/DarCollectionResource.java
+++ src/main/java/org/broadinstitute/consent/http/resources/DarCollectionResource.java
@@ -2,12 +2,13 @@ package org.broadinstitute.consent.http.resources;
 
 import com.google.inject.Inject;
 import io.dropwizard.auth.Auth;
-
+import java.util.Collections;
 import java.util.List;
-
+import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 import org.broadinstitute.consent.http.models.AuthUser;
@@ -22,10 +23,7 @@ public class DarCollectionResource extends Resource {
   private final DarCollectionService darCollectionService;
 
   @Inject
-  public DarCollectionResource(
-    UserService userService,
-    DarCollectionService darCollectionService
-  ) {
+  public DarCollectionResource(UserService userService, DarCollectionService darCollectionService) {
     this.userService = userService;
     this.darCollectionService = darCollectionService;
   }
@@ -34,12 +32,48 @@ public class DarCollectionResource extends Resource {
   @Produces("application/json")
   @RolesAllowed(RESEARCHER)
   public Response getCollectionsForResearcher(@Auth AuthUser authUser) {
-    try{
+    try {
       User user = userService.findUserByEmail(authUser.getEmail());
       List<DarCollection> collections = darCollectionService.getCollectionsForUser(user);
       darCollectionService.addDatasetsToCollections(collections);
       return Response.ok().entity(collections).build();
-    } catch(Exception e) {
+    } catch (Exception e) {
+      return createExceptionResponse(e);
+    }
+  }
+
+  @GET
+  @Path("{collectionId}")
+  @Produces("application/json")
+  @PermitAll
+  public Response getCollectionById(
+      @Auth AuthUser authUser,
+      @PathParam("collectionId") Integer collectionId) {
+    try {
+      User user = userService.findUserByEmail(authUser.getEmail());
+      DarCollection collection = darCollectionService.getByCollectionId(collectionId);
+      // Users can only see their own collection, regardless of user's roles
+      validateAuthedRoleUser(Collections.emptyList(), user, collection.getCreateUserId());
+      return Response.ok().entity(collection).build();
+    } catch (Exception e) {
+      return createExceptionResponse(e);
+    }
+  }
+
+  @GET
+  @Path("dar/{referenceId}")
+  @Produces("application/json")
+  @PermitAll
+  public Response getCollectionByReferenceId(
+      @Auth AuthUser authUser,
+      @PathParam("referenceId") String referenceId) {
+    try {
+      User user = userService.findUserByEmail(authUser.getEmail());
+      DarCollection collection = darCollectionService.getByReferenceId(referenceId);
+      // Users can only see their own collection, regardless of user's roles
+      validateAuthedRoleUser(Collections.emptyList(), user, collection.getCreateUserId());
+      return Response.ok().entity(collection).build();
+    } catch (Exception e) {
       return createExceptionResponse(e);
     }
   }
