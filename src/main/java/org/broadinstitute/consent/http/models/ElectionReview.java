e3814692dd644c32b61c0cc68e012f60bb2bca66
Greg Rushton <grushton@broadinstitute.org>
2021-10-14 09:45:59 -0400

    imports

--- src/main/java/org/broadinstitute/consent/http/models/ElectionReview.java
+++ src/main/java/org/broadinstitute/consent/http/models/ElectionReview.java
@@ -1,7 +1,6 @@
 package org.broadinstitute.consent.http.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
