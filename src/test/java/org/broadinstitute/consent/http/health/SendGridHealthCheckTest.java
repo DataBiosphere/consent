647543a5b7b7123415b169d5b6c3f1cd114d3350
MiriamAlter <mralter@hotmail.com>
2021-11-03 10:21:04 -0400

    SendGrid health check test *actually* fixed

--- src/test/java/org/broadinstitute/consent/http/health/SendGridHealthCheckTest.java
+++ src/test/java/org/broadinstitute/consent/http/health/SendGridHealthCheckTest.java
@@ -12,7 +12,9 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
@@ -39,7 +41,9 @@ public class SendGridHealthCheckTest {
 
     private void initHealthCheck() {
         try {
-            when(response.getEntity()).thenReturn(new StringEntity("{}"));
+            when(response.getEntity()).thenReturn(
+                    new StringEntity("{\"page\":{\"id\":\"3tgl2vf85cht\",\"name\":\"SendGrid\",\"url\":\"https://status.sendgrid.com\",\"time_zone\":\"America/Los_Angeles\",\"updated_at\":\"2021-11-03T04:01:21.355-07:00\"},\"status\":{\"indicator\":\"none\",\"description\":\"All Systems Operational\"}}")
+            );
             when(clientUtil.getHttpResponse(any())).thenReturn(response);
             when(mailConfiguration.getSendGridStatusUrl()).thenReturn("http://localhost:8000");
             healthCheck = new SendGridHealthCheck(clientUtil, mailConfiguration);
