b0316eed8fbdc9dc34a4b968d9da0828ea7ead1f
MiriamAlter <mralter@hotmail.com>
2021-11-12 14:35:03 -0500

    ES health check test first draft finished

--- src/test/java/org/broadinstitute/consent/http/health/ElasticSearchHealthCheckTest.java
+++ src/test/java/org/broadinstitute/consent/http/health/ElasticSearchHealthCheckTest.java
@@ -2,35 +2,24 @@ package org.broadinstitute.consent.http.health;
 
 import com.codahale.metrics.health.HealthCheck;
 import com.google.api.client.http.HttpStatusCodes;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import org.apache.commons.io.IOUtils;
-import org.apache.http.StatusLine;
 import org.broadinstitute.consent.http.WithMockServer;
 import org.broadinstitute.consent.http.configurations.ElasticSearchConfiguration;
 import org.broadinstitute.consent.http.service.ontology.ElasticSearchSupport;
-import org.elasticsearch.client.Response;
 import org.elasticsearch.client.RestClient;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockserver.client.MockServerClient;
 import org.testcontainers.containers.MockServerContainer;
 
-import java.nio.charset.Charset;
-import java.util.ArrayList;
+import javax.ws.rs.InternalServerErrorException;
 import java.util.Collections;
-import java.util.List;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.when;
 import static org.mockserver.model.HttpRequest.request;
 import static org.mockserver.model.HttpResponse.response;
 
@@ -100,17 +89,11 @@ public class ElasticSearchHealthCheckTest implements WithMockServer {
     @Test
     public void testCheckServerFailure() throws Exception {
         initHealthCheck("green", HttpStatusCodes.STATUS_CODE_SERVER_ERROR);
-
-        HealthCheck.Result result = healthCheck.check();
-        assertFalse(result.isHealthy());
-    }
-
-    @Test
-    public void testCheckException() throws Exception {
-//        doThrow(new RuntimeException()).when(esResponse).getStatusLine();
-        initHealthCheck("green", HttpStatusCodes.STATUS_CODE_OK);
-
-        HealthCheck.Result result = healthCheck.check();
-        assertFalse(result.isHealthy());
+        try {
+            healthCheck.check();
+            fail("Expected InternalServerErrorException was not thrown.");
+        } catch (InternalServerErrorException e) {
+            return;
+        }
     }
 }
