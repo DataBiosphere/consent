47793e4a9495b3d2ea793f1334fd78d63e4e5296
MiriamAlter <mralter@hotmail.com>
2021-11-12 11:54:38 -0500

    ES health check test - troubleshooting 2

--- src/test/java/org/broadinstitute/consent/http/health/ElasticSearchHealthCheckTest.java
+++ src/test/java/org/broadinstitute/consent/http/health/ElasticSearchHealthCheckTest.java
@@ -6,17 +6,23 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.StatusLine;
+import org.broadinstitute.consent.http.WithMockServer;
 import org.broadinstitute.consent.http.configurations.ElasticSearchConfiguration;
 import org.broadinstitute.consent.http.service.ontology.ElasticSearchSupport;
 import org.elasticsearch.client.Response;
 import org.elasticsearch.client.RestClient;
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockserver.client.MockServerClient;
+import org.testcontainers.containers.MockServerContainer;
 
 import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.assertFalse;
@@ -25,14 +31,10 @@ import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
 
-public class ElasticSearchHealthCheckTest {
-    @Mock
-    private RestClient client;
-
-    @Mock
-    private ElasticSearchConfiguration config;
-
+public class ElasticSearchHealthCheckTest implements WithMockServer {
     @Mock
     private StatusLine statusLine;
 
@@ -40,19 +42,33 @@ public class ElasticSearchHealthCheckTest {
     private Response esResponse;
 
     private ElasticSearchHealthCheck healthCheck;
+    private ElasticSearchConfiguration config;
+    private RestClient client;
+    private MockServerClient mockServerClient;
+
+    @Rule
+    public MockServerContainer container = new MockServerContainer(IMAGE);
 
     @Before
     public void setUp() {
         MockitoAnnotations.openMocks(this);
 
-        List<String> servers = new ArrayList<>();
-        servers.add("localhost");
-        when(config.getServers()).thenReturn(servers);
-        when(ElasticSearchSupport.createRestClient(config)).thenReturn(client);
+        config = new ElasticSearchConfiguration();
+        config.setServers(Collections.singletonList("localhost"));
+        config.setPort(container.getServerPort());
+        client = ElasticSearchSupport.createRestClient(config);
+
+        mockServerClient = new MockServerClient(container.getHost(), container.getServerPort());
+    }
+
+    @After
+    public void tearDown() {
+        stop(container);
     }
 
     private void initHealthCheck(String status) {
         try {
+            mockServerClient.when(request()).respond(response().withStatusCode(200).withBody(String.valueOf(esResponse)));
             String stringResponse = IOUtils.toString(esResponse.getEntity().getContent(), Charset.defaultCharset());
             JsonObject jsonResponse = JsonParser.parseString(stringResponse).getAsJsonObject();
             when(jsonResponse.get("status").getAsString()).thenReturn(status);
