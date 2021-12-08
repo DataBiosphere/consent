a2e2ece6b374addc2097dbc213327822e6b3b392
Greg Rushton <grushton@broadinstitute.org>
2021-12-08 13:45:46 -0500

    add a second translated ToS endpoint

--- src/main/java/org/broadinstitute/consent/http/resources/TosResource.java
+++ src/main/java/org/broadinstitute/consent/http/resources/TosResource.java
@@ -1,8 +1,6 @@
 package org.broadinstitute.consent.http.resources;
 
 import com.google.inject.Inject;
-import io.dropwizard.auth.Auth;
-import org.broadinstitute.consent.http.models.AuthUser;
 import org.broadinstitute.consent.http.service.sam.SamService;
 
 import javax.ws.rs.GET;
@@ -13,22 +11,35 @@ import javax.ws.rs.core.Response;
 @Path("tos")
 public class TosResource extends Resource {
 
-    private final SamService samService;
+  private final SamService samService;
 
-    @Inject
-    public TosResource(SamService samService) {
-        this.samService = samService;
-    }
+  @Inject
+  public TosResource(SamService samService) {
+    this.samService = samService;
+  }
 
-    @Path("text")
-    @GET
-    @Produces("text/plain")
-    public Response getToSText() {
-        try {
-            String text = samService.getToSText();
-            return Response.ok().entity(text).build();
-        } catch (Exception e) {
-            return createExceptionResponse(e);
-        }
+  @Path("text")
+  @GET
+  @Produces("text/plain")
+  public Response getToSText() {
+    try {
+      String text = samService.getToSText();
+      return Response.ok().entity(text).build();
+    } catch (Exception e) {
+      return createExceptionResponse(e);
     }
+  }
+
+  @Path("text/duos")
+  @GET
+  @Produces("text/plain")
+  public Response getDUOSToSText() {
+    try {
+      String text = samService.getToSText();
+      String duos = text.replaceAll("Terra", "Terra/DUOS");
+      return Response.ok().entity(duos).build();
+    } catch (Exception e) {
+      return createExceptionResponse(e);
+    }
+  }
 }
--- src/test/java/org/broadinstitute/consent/http/resources/TosResourceTest.java
+++ src/test/java/org/broadinstitute/consent/http/resources/TosResourceTest.java
@@ -1,7 +1,6 @@
 package org.broadinstitute.consent.http.resources;
 
 import com.google.api.client.http.HttpStatusCodes;
-import org.broadinstitute.consent.http.models.AuthUser;
 import org.broadinstitute.consent.http.service.sam.SamService;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,6 +9,7 @@ import org.mockito.Mock;
 import javax.ws.rs.core.Response;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.openMocks;
 
@@ -37,5 +37,16 @@ public class TosResourceTest {
     Response response = resource.getToSText();
     assertEquals(HttpStatusCodes.STATUS_CODE_OK, response.getStatus());
     assertEquals(mockText, response.getEntity().toString());
+  }
+
+  @Test
+  public void testGetDuosToSText() throws Exception {
+    String mockText = "Sample Terra ToS";
+    when(service.getToSText()).thenReturn(mockText);
+    initResource();
+
+    Response response = resource.getDUOSToSText();
+    assertEquals(HttpStatusCodes.STATUS_CODE_OK, response.getStatus());
+    assertTrue(response.getEntity().toString().contains("DUOS"));
   }
 }
