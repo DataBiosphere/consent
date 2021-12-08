51b6c479e4346328da1811a37d211368ad10d902
Greg Rushton <grushton@broadinstitute.org>
2021-12-08 13:25:26 -0500

    format

--- src/test/java/org/broadinstitute/consent/http/resources/TosResourceTest.java
+++ src/test/java/org/broadinstitute/consent/http/resources/TosResourceTest.java
@@ -10,38 +10,32 @@ import org.mockito.Mock;
 import javax.ws.rs.core.Response;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.openMocks;
 
 public class TosResourceTest {
 
-    @Mock
-    private AuthUser authUser;
+  @Mock private SamService service;
 
-    @Mock
-    private SamService service;
+  private TosResource resource;
 
-    private TosResource resource;
+  @Before
+  public void setUp() {
+    openMocks(this);
+  }
 
-    @Before
-    public void setUp() {
-        openMocks(this);
-    }
+  private void initResource() {
+    resource = new TosResource(service);
+  }
 
-    private void initResource() {
-        resource = new TosResource(service);
-    }
+  @Test
+  public void testGetToSText() throws Exception {
+    String mockText = "Plain Text";
+    when(service.getToSText()).thenReturn(mockText);
+    initResource();
 
-    @Test
-    public void testGetToSText() throws Exception {
-        String mockText = "Plain Text";
-        when(service.getToSText()).thenReturn(mockText);
-        initResource();
-
-        Response response = resource.getToSText();
-        assertEquals(HttpStatusCodes.STATUS_CODE_OK, response.getStatus());
-        assertEquals(mockText, response.getEntity().toString());
-    }
-
+    Response response = resource.getToSText();
+    assertEquals(HttpStatusCodes.STATUS_CODE_OK, response.getStatus());
+    assertEquals(mockText, response.getEntity().toString());
+  }
 }
