6353ae078c985150e8b2f5cf13aa127fcdaea132
Greg Rushton <grushton@broadinstitute.org>
2021-12-17 08:57:02 -0500

    imports

--- src/test/java/org/broadinstitute/consent/http/service/VoteServiceTest.java
+++ src/test/java/org/broadinstitute/consent/http/service/VoteServiceTest.java
@@ -35,7 +35,6 @@ import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.openMocks;
 
