b15da91ea8c2a42d28c8a009a3f695df2e39f643
Greg Rushton <grushton@broadinstitute.org>
2022-01-12 15:22:49 -0500

    tests

--- src/test/java/org/broadinstitute/consent/http/service/dao/DarCollectionServiceDAOTest.java
+++ src/test/java/org/broadinstitute/consent/http/service/dao/DarCollectionServiceDAOTest.java
@@ -1,6 +1,7 @@
 package org.broadinstitute.consent.http.service.dao;
 
 import org.broadinstitute.consent.http.db.DAOTestHelper;
+import org.broadinstitute.consent.http.enumeration.ElectionStatus;
 import org.broadinstitute.consent.http.enumeration.ElectionType;
 import org.broadinstitute.consent.http.enumeration.UserRoles;
 import org.broadinstitute.consent.http.enumeration.VoteType;
@@ -11,13 +12,13 @@ import org.broadinstitute.consent.http.models.DataAccessRequest;
 import org.broadinstitute.consent.http.models.DataSet;
 import org.broadinstitute.consent.http.models.Election;
 import org.broadinstitute.consent.http.models.Vote;
-import org.junit.After;
 import org.junit.Test;
 
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
@@ -95,6 +96,34 @@ public class DarCollectionServiceDAOTest extends DAOTestHelper {
     assertTrue(createdVotes.stream().anyMatch(v -> v.getType().equals(VoteType.FINAL.getValue())));
     assertTrue(createdVotes.stream().anyMatch(v -> v.getType().equals(VoteType.DAC.getValue())));
     assertTrue(createdVotes.stream().noneMatch(v -> v.getType().equals(VoteType.AGREEMENT.getValue())));
+  }
+
+  @Test
+  public void testCreateElectionsForDarCollectionAfterCancelingEarlierElections() throws Exception {
+    initService();
+
+    DarCollection collection = setUpDarCollectionWithDacDataset();
+    DataAccessRequest dar = collection.getDars().values().stream().findFirst().orElse(null);
+    assertNotNull(dar);
+
+    // create elections & votes:
+    serviceDAO.createElectionsForDarCollection(collection);
+
+    // cancel those elections:
+    electionDAO.findLastElectionsByReferenceIds(List.of(dar.getReferenceId())).forEach(e ->
+        electionDAO.updateElectionById(e.getElectionId(), ElectionStatus.CANCELED.getValue(), new Date()));
+
+    // re-create elections & new votes:
+    serviceDAO.createElectionsForDarCollection(collection);
+
+    List<Election> createdElections =
+        electionDAO.findLastElectionsByReferenceIds(List.of(dar.getReferenceId()));
+
+    // Ensure that we have the right number of access and rp elections, i.e. 1 each
+    assertFalse(createdElections.isEmpty());
+    assertEquals(2, createdElections.size());
+    assertEquals(1, createdElections.stream().filter(e -> e.getElectionType().equals(ElectionType.DATA_ACCESS.getValue())).count());
+    assertEquals(1, createdElections.stream().filter(e -> e.getElectionType().equals(ElectionType.RP.getValue())).count());
   }
 
   private DarCollection setUpDarCollectionWithDacDataset() {
