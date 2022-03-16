b8b1a504c3d69927084d367c57202078abab7ec0
Shae Marks <smarks.dev1@gmail.com>
2022-03-16 12:38:02 -0400

    Remove original updateVotes method

--- src/main/java/org/broadinstitute/consent/http/resources/VoteResource.java
+++ src/main/java/org/broadinstitute/consent/http/resources/VoteResource.java
@@ -18,10 +18,7 @@ import javax.ws.rs.NotFoundException;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -37,64 +34,6 @@ public class VoteResource extends Resource {
         this.voteService = voteService;
     }
 
-    /**
-     * This API will take a boolean vote value as a query param and apply it to the list of vote ids
-     * passed in as a list of integer vote ids.
-     *
-     * Error cases are:
-     * 1. JSON Body is not a list of integers
-     * 2. Auth user is not the owner of all votes being updated
-     * 3. No votes match the list of ids provided
-     *
-     * @param authUser The AuthUser
-     * @param vote The boolean value to update votes for
-     * @param rationale The string value for all rationales
-     * @param json The list of vote ids, in json format
-     * @return Response with results of the update.
-     */
-    @PUT
-    @Consumes("application/json")
-    @Produces("application/json")
-    @RolesAllowed({CHAIRPERSON, MEMBER})
-    public Response updateVotes(
-            @Auth AuthUser authUser,
-            @QueryParam("vote") Boolean vote,
-            @QueryParam("rationale") String rationale,
-            String json) {
-        // Validate input json - it needs to be an array of integers
-        Type intListType = new TypeToken<ArrayList<Integer>>(){}.getType();
-        List<Integer> voteIds;
-        try {
-            voteIds = new ArrayList<>(gson.fromJson(json, intListType));
-        } catch (Exception e) {
-            return createExceptionResponse(
-                    new BadRequestException("Unable to parse required vote ids: " + json)
-            );
-        }
-
-        if (voteIds.isEmpty()) {
-            return createExceptionResponse(new NotFoundException());
-        }
-
-        try {
-            List<Vote> votes = voteService.findVotesByIds(voteIds);
-            if (votes.isEmpty()) {
-                return createExceptionResponse(new NotFoundException());
-            }
-
-            // Validate that the user is only updating their own votes:
-            User user = userService.findUserByEmail(authUser.getEmail());
-            boolean authed = votes.stream().map(Vote::getDacUserId).allMatch(id -> id.equals(user.getDacUserId()));
-            if (!authed) {
-                return createExceptionResponse(new NotFoundException());
-            }
-
-            List<Vote> updatedVotes = voteService.updateVotesWithValue(votes, vote, rationale);
-            return Response.ok().entity(updatedVotes).build();
-        } catch (Exception e) {
-            return createExceptionResponse(e);
-        }
-    }
 
     /**
      * This API will take a boolean vote value as a query param and apply it to the list of vote ids
@@ -128,8 +67,8 @@ public class VoteResource extends Resource {
         if (Objects.isNull(voteUpdateInfo.getVote())) {
             return createExceptionResponse(new BadRequestException("Vote value is required"));
         }
-        if (voteUpdateInfo.getVoteIds().isEmpty()) {
-            return createExceptionResponse(new NotFoundException());
+        if (Objects.isNull(voteUpdateInfo.getVoteIds()) || voteUpdateInfo.getVoteIds().isEmpty()) {
+            return createExceptionResponse(new BadRequestException("Vote ids are required"));
         }
 
         try {
