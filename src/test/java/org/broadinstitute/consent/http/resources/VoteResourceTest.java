b734849a55437e722eff2104a9b37c9f3e65851a
Shae Marks <smarks.dev1@gmail.com>
2022-03-16 12:25:59 -0400

    Recreate existing updateVotes test using new version of method

--- src/test/java/org/broadinstitute/consent/http/resources/VoteResourceTest.java
+++ src/test/java/org/broadinstitute/consent/http/resources/VoteResourceTest.java
@@ -1,9 +1,11 @@
 package org.broadinstitute.consent.http.resources;
 
 import com.google.api.client.http.HttpStatusCodes;
+import com.google.gson.Gson;
 import org.broadinstitute.consent.http.models.AuthUser;
 import org.broadinstitute.consent.http.models.User;
 import org.broadinstitute.consent.http.models.Vote;
+import org.broadinstitute.consent.http.models.VoteUpdateInfo;
 import org.broadinstitute.consent.http.service.UserService;
 import org.broadinstitute.consent.http.service.VoteService;
 import org.junit.Before;
@@ -11,6 +13,7 @@ import org.junit.Test;
 import org.mockito.Mock;
 
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -28,11 +31,13 @@ public class VoteResourceTest {
 
   @Mock private AuthUser authUser;
 
-  private User user = new User();
+  private final User user = new User();
 
-  private Vote vote = new Vote();
+  private final Vote vote = new Vote();
 
   private VoteResource resource;
+
+  private final Gson gson = new Gson();
 
   @Before
   public void setUp() {
@@ -101,4 +106,79 @@ public class VoteResourceTest {
     Response response = resource.updateVotes(authUser, true, "rationale", "[1, 2, 3]");
     assertEquals(HttpStatusCodes.STATUS_CODE_OK, response.getStatus());
   }
+
+
+
+
+
+
+  @Test
+  public void testUpdateVotes_invalidJson2() {
+    initResource();
+    Response response = resource.updateVotes2(authUser, "{\"vote\": true, \"ID\":12345}");
+    assertEquals(HttpStatusCodes.STATUS_CODE_BAD_REQUEST, response.getStatus());
+  }
+
+  @Test
+  public void testUpdateVotes_noIds2() {
+    initResource();
+    VoteUpdateInfo voteUpdateInfo = new VoteUpdateInfo(true, "example", new ArrayList<>());
+    Response response = resource.updateVotes2(authUser, gson.toJson(voteUpdateInfo, VoteUpdateInfo.class));
+    assertEquals(HttpStatusCodes.STATUS_CODE_BAD_REQUEST, response.getStatus());
+  }
+
+
+  @Test
+  public void testUpdateVotes_noVotesForIds2() {
+    when(userService.findUserByEmail(any())).thenReturn(user);
+    when(voteService.findVotesByIds(any())).thenReturn(Collections.emptyList());
+    initResource();
+
+    VoteUpdateInfo voteUpdateInfo = new VoteUpdateInfo(true, "example", List.of(1));
+    Response response = resource.updateVotes2(authUser, gson.toJson(voteUpdateInfo, VoteUpdateInfo.class));
+    assertEquals(HttpStatusCodes.STATUS_CODE_NOT_FOUND, response.getStatus());
+  }
+
+  @Test
+  public void testUpdateVotes_invalidUser2() {
+    user.setDacUserId(1);
+    vote.setDacUserId(2);
+    when(userService.findUserByEmail(any())).thenReturn(user);
+    when(voteService.findVotesByIds(any())).thenReturn(List.of(vote));
+    initResource();
+
+    VoteUpdateInfo voteUpdateInfo = new VoteUpdateInfo(true, "example", List.of(1, 2, 3));
+    Response response = resource.updateVotes2(authUser, gson.toJson(voteUpdateInfo, VoteUpdateInfo.class));
+    assertEquals(HttpStatusCodes.STATUS_CODE_NOT_FOUND, response.getStatus());
+  }
+
+  @Test
+  public void testUpdateVotes_closedElection2() {
+    user.setDacUserId(1);
+    vote.setDacUserId(2);
+    when(userService.findUserByEmail(any())).thenReturn(user);
+    doThrow(new IllegalArgumentException()).when(voteService).findVotesByIds(any());
+    initResource();
+
+    VoteUpdateInfo voteUpdateInfo = new VoteUpdateInfo(true, "example", List.of(1, 2, 3));
+    Response response = resource.updateVotes2(authUser, gson.toJson(voteUpdateInfo, VoteUpdateInfo.class));
+    assertEquals(HttpStatusCodes.STATUS_CODE_BAD_REQUEST, response.getStatus());
+  }
+
+
+  @Test
+  public void testUpdateVotes2() {
+    user.setDacUserId(1);
+    vote.setDacUserId(1);
+    when(userService.findUserByEmail(any())).thenReturn(user);
+    when(voteService.findVotesByIds(any())).thenReturn(List.of(vote));
+    initResource();
+
+    VoteUpdateInfo voteUpdateInfo = new VoteUpdateInfo(true, "example", List.of(1, 2, 3));
+    Response response = resource.updateVotes2(authUser, gson.toJson(voteUpdateInfo, VoteUpdateInfo.class));
+    assertEquals(HttpStatusCodes.STATUS_CODE_OK, response.getStatus());
+  }
+
+
+
 }
