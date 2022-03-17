9d6f22bb7dc700293bf5e0c7da03cb341673e1e7
Shae Marks <smarks.dev1@gmail.com>
2022-03-17 11:12:27 -0400

    Remove VoteUpdateInfo class from VoteResource

--- src/main/java/org/broadinstitute/consent/http/resources/VoteResource.java
+++ src/main/java/org/broadinstitute/consent/http/resources/VoteResource.java
@@ -89,44 +89,4 @@ public class VoteResource extends Resource {
             return createExceptionResponse(e);
         }
     }
-
-    static class VoteUpdateInfo {
-        private Boolean vote;
-        private String rationale;
-        private List<Integer> voteIds;
-
-        public VoteUpdateInfo() {
-
-        }
-
-        public VoteUpdateInfo(Boolean vote, String rationale, List<Integer> voteIds) {
-            this.vote = vote;
-            this.rationale = rationale;
-            this.voteIds = voteIds;
-        }
-
-        public Boolean getVote() {
-            return vote;
-        }
-
-        public void setVote(Boolean vote) {
-            this.vote = vote;
-        }
-
-        public String getRationale() {
-            return rationale;
-        }
-
-        public void setRationale(String rationale) {
-            this.rationale = rationale;
-        }
-
-        public List<Integer> getVoteIds() {
-            return voteIds;
-        }
-
-        public void setVoteIds(List<Integer> voteIds) {
-            this.voteIds = voteIds;
-        }
-    }
 }
