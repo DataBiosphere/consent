04295b418b96c589b7dcb51f0b2e609f948b8776
Justin Variath Thomas <JVThomas@users.noreply.github.com>
2022-03-17 13:55:41 -0400

    DUOS-1679 Add elections to DarCollectionsDAO.findAllDarCollections (#1491)

--- src/main/java/org/broadinstitute/consent/http/db/DarCollectionDAO.java
+++ src/main/java/org/broadinstitute/consent/http/db/DarCollectionDAO.java
@@ -141,12 +141,14 @@ public interface DarCollectionDAO {
   @RegisterBeanMapper(value = DarCollection.class)
   @RegisterBeanMapper(value = DataAccessRequest.class, prefix = "dar")
   @RegisterBeanMapper(value = UserProperty.class, prefix = "up")
+  @RegisterBeanMapper(value = Election.class, prefix = "e")
   @UseRowReducer(DarCollectionReducer.class)
   @SqlQuery(
     " SELECT c.*, " +
         User.QUERY_FIELDS_WITH_U_PREFIX + QUERY_FIELD_SEPARATOR +
         Institution.QUERY_FIELDS_WITH_I_PREFIX + QUERY_FIELD_SEPARATOR +
         UserProperty.QUERY_FIELDS_WITH_UP_PREFIX + QUERY_FIELD_SEPARATOR +
+        Election.QUERY_FIELDS_WITH_E_PREFIX + QUERY_FIELD_SEPARATOR +
         "dar.id AS dar_id, dar.reference_id AS dar_reference_id, dar.collection_id AS dar_collection_id, " +
         "dar.draft AS dar_draft, dar.user_id AS dar_userId, dar.create_date AS dar_create_date, " +
         "dar.sort_date AS dar_sort_date, dar.submission_date AS dar_submission_date, " +
@@ -155,7 +157,9 @@ public interface DarCollectionDAO {
         "INNER JOIN dacuser u ON c.create_user_id = u.dacuserid " +
         "LEFT JOIN user_property up ON u.dacuserid = up.userid " +
         "INNER JOIN data_access_request dar on c.collection_id = dar.collection_id " +
-        "LEFT JOIN institution i ON i.institution_id = u.institution_id "
+        "LEFT JOIN institution i ON i.institution_id = u.institution_id " +
+        " LEFT JOIN (SELECT election.*, MAX(election.electionid) OVER (PARTITION BY election.referenceid, election.electiontype) AS latest FROM election) AS e " +
+        "   ON dar.reference_id = e.referenceid AND (e.latest = e.electionid OR e.latest IS NULL) "
   )
   List<DarCollection> findAllDARCollections();
 
