3de83ea9481f7986cfaa655186313177f80ca8ae
Gregory Rushton <rushtong@users.noreply.github.com>
2022-06-10 09:27:54 -0400

    [DUOS-1820][risk=no] Bulk Upload Institutions (#1595)
    * add 409 case to POST endpoint
    * javascript upload utility

---
+++ migration/insitution/consentAPI.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+const log = require('./logging');
+
+module.exports = {
+
+    postInstitution: async (host, token, name) => {
+        const url = host + '/api/institutions'
+        const config = {
+            headers: {
+                Authorization: `Bearer ${token}`,
+                Accept: 'application/json'
+            }
+        }
+        const data = {name: name}
+        return axios.post(url, data, config).then(response => {
+            log.info(response.status + ': ' + name)
+            return response.status;
+        }).catch(err => {
+            log.error(err.message + ': ' + name)
+            return err.response.status;
+        });
+    }
+};
