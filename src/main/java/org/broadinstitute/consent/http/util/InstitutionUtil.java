9667bba2fd490f64b3d7b0193e17f5b6920d0692
Shae Marks <smarks.dev1@gmail.com>
2022-06-30 16:45:27 -0400

    Include signing officials for non-admins calling the endpoint

--- src/main/java/org/broadinstitute/consent/http/util/InstitutionUtil.java
+++ src/main/java/org/broadinstitute/consent/http/util/InstitutionUtil.java
@@ -38,11 +38,7 @@ public class InstitutionUtil {
       @Override
       public boolean shouldSkipField(FieldAttributes field) {
         String fieldName = field.getName();
-        if (!isAdmin && (fieldName != "id" && fieldName != "name")) {
-          return true;
-        } else {
-          return false;
-        }
+        return !isAdmin && (!fieldName.equals("id") && !fieldName.equals("name") && !fieldName.equals("signingOfficials"));
       }
 
       // NOTE: shouldSkipClass is mandatory when creating an ExclusionStrategy
