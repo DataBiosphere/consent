7ffb9e439dbe1830512b88d8c1060c7a09b52f19
Shae Marks <smarks.dev1@gmail.com>
2022-07-11 09:50:22 -0400

    Update CustomRequestField equals and hashcode

--- src/main/java/org/broadinstitute/consent/http/models/supportticket/CustomRequestField.java
+++ src/main/java/org/broadinstitute/consent/http/models/supportticket/CustomRequestField.java
@@ -1,5 +1,7 @@
 package org.broadinstitute.consent.http.models.supportticket;
 
+import java.util.Objects;
+
 public class CustomRequestField {
 
     private String id;
@@ -24,5 +26,18 @@ public class CustomRequestField {
 
     public void setValue(String value) {
         this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CustomRequestField that = (CustomRequestField) o;
+        return Objects.equals(id, that.id) && Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, value);
     }
 }
