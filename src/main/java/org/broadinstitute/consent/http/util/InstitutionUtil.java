6d77316fbb62b69ec6618abef5a700061f7993d1
Connor Barker <connorlbark@gmail.com>
2022-07-14 15:43:17 -0400

    fix bug where non-admins could not see signing officials

--- src/main/java/org/broadinstitute/consent/http/util/InstitutionUtil.java
+++ src/main/java/org/broadinstitute/consent/http/util/InstitutionUtil.java
@@ -38,7 +38,12 @@ public class InstitutionUtil {
       @Override
       public boolean shouldSkipField(FieldAttributes field) {
         String fieldName = field.getName();
-        return !isAdmin && !(fieldName.equals("id") || fieldName.equals("name") || fieldName.equals("signingOfficials"));
+        
+        return !isAdmin && !(fieldName.equals("id")
+                || fieldName.equals("name")
+                || fieldName.equals("signingOfficials")
+                || fieldName.equals("displayName")
+                || fieldName.equals("userId"));
       }
 
       // NOTE: shouldSkipClass is mandatory when creating an ExclusionStrategy
