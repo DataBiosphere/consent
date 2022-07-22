d41a0f84154a4ba5d92a74634e7a32829a347a85
Shae Marks <smarks.dev1@gmail.com>
2022-07-22 17:38:04 -0400

    Add queries for dar collection summary by collection id

--- src/main/java/org/broadinstitute/consent/http/db/DarCollectionSummaryDAO.java
+++ src/main/java/org/broadinstitute/consent/http/db/DarCollectionSummaryDAO.java
@@ -23,7 +23,7 @@ public interface DarCollectionSummaryDAO extends Transactional<DarCollectionSumm
   @SqlQuery
   (
     "SELECT c.collection_id as dar_collection_id, c.dar_code, dar.submission_date, u.display_name as researcher_name, " +
-      "i.institution_name, e.electionid, e.status, e.datasetid, e.referenceid, v.voteid as v_vote_id, dd.dataset_id as dd_datasetid, " +      
+      "i.institution_name, e.electionid, e.status, e.datasetid, e.referenceid, v.voteid as v_vote_id, dd.dataset_id as dd_datasetid, " +
       "v.dacuserid as v_dac_user_id, v.vote as v_vote, v.electionid as v_election_id, v.createdate as v_create_date, v.updatedate as v_update_date, v.type as v_type, " +
       "(dar.data #>> '{}')::jsonb ->> 'projectTitle' AS name " +
     "FROM dar_collection c " +
@@ -51,6 +51,8 @@ public interface DarCollectionSummaryDAO extends Transactional<DarCollectionSumm
       @Bind("currentUserId") Integer currentUserId,
       @BindList("datasetIds") List<Integer> datasetIds);
 
+
+
   @RegisterBeanMapper(value = DarCollectionSummary.class)
   @RegisterBeanMapper(value = DarCollection.class)
   @RegisterBeanMapper(value = Election.class)
@@ -58,7 +60,7 @@ public interface DarCollectionSummaryDAO extends Transactional<DarCollectionSumm
   @SqlQuery
   (
     "SELECT c.collection_id as dar_collection_id, c.dar_code, dar.submission_date, u.display_name as researcher_name, " +
-      "i.institution_name, e.electionid, e.status, e.datasetid, e.referenceid, dd.dataset_id as dd_datasetid, " +      
+      "i.institution_name, e.electionid, e.status, e.datasetid, e.referenceid, dd.dataset_id as dd_datasetid, " +
       "(dar.data #>> '{}')::jsonb ->> 'projectTitle' AS name " +
     "FROM dar_collection c " +
     "INNER JOIN users u " +
@@ -88,7 +90,7 @@ public interface DarCollectionSummaryDAO extends Transactional<DarCollectionSumm
   @SqlQuery
   (
     "SELECT c.collection_id as dar_collection_id, c.dar_code, dar.submission_date, u.display_name as researcher_name, " +
-      "i.institution_name, e.electionid, e.status, e.datasetid, e.referenceid, dd.dataset_id as dd_datasetid, " +      
+      "i.institution_name, e.electionid, e.status, e.datasetid, e.referenceid, dd.dataset_id as dd_datasetid, " +
       "(dar.data #>> '{}')::jsonb ->> 'projectTitle' AS name " +
     "FROM dar_collection c " +
     "INNER JOIN users u " +
@@ -116,7 +118,7 @@ public interface DarCollectionSummaryDAO extends Transactional<DarCollectionSumm
   @SqlQuery
   (
     "SELECT c.collection_id as dar_collection_id, c.dar_code, dar.submission_date, dar.reference_id as dar_reference_id, u.display_name as researcher_name, " +
-      "i.institution_name, e.electionid, e.status, e.datasetid, e.referenceid, dd.dataset_id as dd_datasetid, " +      
+      "i.institution_name, e.electionid, e.status, e.datasetid, e.referenceid, dd.dataset_id as dd_datasetid, " +
       "(dar.data #>> '{}')::jsonb ->> 'projectTitle' AS name, " +
       "(dar.data #>> '{}')::jsonb ->> 'status' AS dar_status " +
     "FROM dar_collection c " +
@@ -139,4 +141,76 @@ public interface DarCollectionSummaryDAO extends Transactional<DarCollectionSumm
   )
   List<DarCollectionSummary> getDarCollectionSummariesForResearcher(
       @Bind("userId") Integer userId);
+
+
+  @RegisterBeanMapper(value = DarCollectionSummary.class)
+  @RegisterBeanMapper(value = DarCollection.class)
+  @RegisterBeanMapper(value = Vote.class, prefix = "v")
+  @RegisterBeanMapper(value = Election.class)
+  @UseRowReducer(DarCollectionSummaryReducer.class)
+  @SqlQuery
+          (
+                  "SELECT c.collection_id as dar_collection_id, c.dar_code, dar.submission_date, u.display_name as researcher_name, " +
+                          "i.institution_name, e.electionid, e.status, e.datasetid, e.referenceid, v.voteid as v_vote_id, dd.dataset_id as dd_datasetid, " +
+                          "v.dacuserid as v_dac_user_id, v.vote as v_vote, v.electionid as v_election_id, v.createdate as v_create_date, v.updatedate as v_update_date, v.type as v_type, " +
+                          "(dar.data #>> '{}')::jsonb ->> 'projectTitle' AS name " +
+                          "FROM dar_collection c " +
+                          "INNER JOIN users u " +
+                          "ON u.user_id = c.create_user_id " +
+                          "INNER JOIN institution i " +
+                          "ON i.institution_id = u.institution_id " +
+                          "INNER JOIN data_access_request dar " +
+                          "ON dar.collection_id = c.collection_id " +
+                          "LEFT JOIN ( " +
+                          "SELECT election.*, MAX(election.electionid) OVER(PARTITION BY election.referenceid, election.datasetid) AS latest " +
+                          "FROM election " +
+                          "WHERE LOWER(election.electiontype) = 'dataaccess' AND election.datasetid IN (<datasetIds>)" +
+                          ") AS e " +
+                          "ON e.referenceid = dar.reference_id " +
+                          "LEFT JOIN vote v " +
+                          "ON e.electionid = v.electionid " +
+                          "INNER JOIN dar_dataset dd " +
+                          "ON dar.reference_id = dd.reference_id " +
+                          "WHERE c.collection_id= :collectionId " +
+                          "AND dd.dataset_id IN (<datasetIds>) " +
+                          "AND (e.latest = e.electionid OR e.electionid IS NULL) " +
+                          "AND (LOWER(v.type) = 'final' OR (v.dacuserid = :currentUserId OR v.voteid IS NULL))"
+          )
+  List<DarCollectionSummary> getDarCollectionSummaryForDACByCollectionId(
+          @Bind("currentUserId") Integer currentUserId,
+          @BindList("datasetIds") List<Integer> datasetIds,
+          @Bind("collectionId") Integer collectionId);
+
+
+
+  @RegisterBeanMapper(value = DarCollectionSummary.class)
+  @RegisterBeanMapper(value = DarCollection.class)
+  @RegisterBeanMapper(value = Election.class)
+  @UseRowReducer(DarCollectionSummaryReducer.class)
+  @SqlQuery
+          (
+                  "SELECT c.collection_id as dar_collection_id, c.dar_code, dar.submission_date, dar.reference_id as dar_reference_id, u.display_name as researcher_name, " +
+                          "i.institution_name, e.electionid, e.status, e.datasetid, e.referenceid, dd.dataset_id as dd_datasetid, " +
+                          "(dar.data #>> '{}')::jsonb ->> 'projectTitle' AS name, " +
+                          "(dar.data #>> '{}')::jsonb ->> 'status' AS dar_status " +
+                          "FROM dar_collection c " +
+                          "INNER JOIN users u " +
+                          "ON u.user_id = c.create_user_id " +
+                          "INNER JOIN institution i " +
+                          "ON i.institution_id = u.institution_id " +
+                          "INNER JOIN data_access_request dar " +
+                          "ON dar.collection_id = c.collection_id " +
+                          "LEFT JOIN ( " +
+                          "SELECT election.*, MAX(election.electionid) OVER(PARTITION BY election.referenceid, election.datasetid) AS latest " +
+                          "FROM election " +
+                          "WHERE LOWER(election.electiontype) = 'dataaccess'"+
+                          ") AS e " +
+                          "ON e.referenceid = dar.reference_id " +
+                          "INNER JOIN dar_dataset dd " +
+                          "ON dar.reference_id = dd.reference_id " +
+                          "WHERE c.collection_id = :collectionId " +
+                          "AND (e.latest = e.electionid OR e.electionid IS NULL)"
+          )
+  List<DarCollectionSummary> getDarCollectionSummaryByCollectionId(
+          @Bind("collectionId") Integer collectionId);
 }
