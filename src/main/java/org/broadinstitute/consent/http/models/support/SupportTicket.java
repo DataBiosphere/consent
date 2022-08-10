fa53a74e5275d5418e385b0d4a8bd4c96e20f4c7
Shae Marks <smarks.dev1@gmail.com>
2022-08-10 16:40:03 -0400

    Add SupportTicket constructor to replace SupportRequestService.createSupportTicket

--- src/main/java/org/broadinstitute/consent/http/models/support/SupportTicket.java
+++ src/main/java/org/broadinstitute/consent/http/models/support/SupportTicket.java
@@ -1,6 +1,10 @@
 package org.broadinstitute.consent.http.models.support;
 
+import org.broadinstitute.consent.http.enumeration.SupportRequestType;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Represents ticket to request support required by the Zendesk API https://broadinstitute.zendesk.com/api/v2/requests.json
@@ -12,6 +16,28 @@ public class SupportTicket {
     public SupportTicket(SupportRequester requester, String subject, List<CustomRequestField> customFields, SupportRequestComment comment) {
         this.request = new SupportRequest(requester, subject, customFields, comment);
     }
+
+    public SupportTicket(String name, SupportRequestType type, String email, String subject, String description, String url) {
+        if (Objects.isNull(name) || Objects.isNull(email)) {
+            throw new IllegalArgumentException("Name and email of user requesting support is required");
+        }
+        if (Objects.isNull(subject)) {
+            throw new IllegalArgumentException("Support ticket subject is required");
+        }
+        if (Objects.isNull(description)) {
+            throw new IllegalArgumentException("Support ticket description is required");
+        }
+        if (Objects.isNull(type)) {
+            throw new IllegalArgumentException("Support ticket type is required");
+        }
+        if (Objects.isNull(url)) {
+            throw new IllegalArgumentException("Support ticket url is required");
+        }
+
+        this.request = new SupportRequest(name, type, email, subject, description, url);
+    }
+
+
 
     public SupportRequest getRequest() {
         return request;
@@ -36,6 +62,13 @@ public class SupportTicket {
             this.subject = subject;
             this.customFields = customFields;
             this.comment = comment;
+        }
+
+        public SupportRequest(String name, SupportRequestType type, String email, String subject, String description, String url) {
+            this.requester = new SupportRequester(name, email);
+            this.subject = subject;
+            this.customFields = createCustomFields(name, type, email, description);
+            this.comment = new SupportRequestComment(description + "\n\n------------------\nSubmitted from: " + url);
         }
 
         public SupportRequester getRequester() {
@@ -72,6 +105,16 @@ public class SupportTicket {
 
         public long getTicketFormId() {
             return ticketFormId;
+        }
+
+        private List<CustomRequestField> createCustomFields(String name, SupportRequestType type, String email, String description) {
+            List<CustomRequestField> customFields = new ArrayList<>();
+            customFields.add(new CustomRequestField(360012744452L, type.getValue()));
+            customFields.add(new CustomRequestField(360007369412L, description));
+            customFields.add(new CustomRequestField(360012744292L, name));
+            customFields.add(new CustomRequestField(360012782111L, email));
+            customFields.add(new CustomRequestField(360018545031L, email));
+            return customFields;
         }
     }
 }
