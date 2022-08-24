ff83c54bb1bf65f201efc7f15a0e3ad7132cbbb9
shaemarks <81024249+shaemarks@users.noreply.github.com>
2022-08-24 12:14:34 -0400

    [DUOS-1988][risk=no] fix zendesk support request and expand functionality to selected institutions/SOs (#1695)
    * Make method to build post request without authentication headers
    
    * Remove AuthUser from support request methods
    
    * Update expected status code
    
    * Update status code in tests
    
    * Test other http client
    
    * Use ByteArrayContent instead of JsonHttpContent
    
    * Create method to generate ticket for both new and existing signing officials / institutions
    
    * Update consent module provides
    
    * Add tests
    
    * Update call in UserResource method
    
    * Delete old versions of support resource methods
    
    * Update swagger description
    
    * Add SupportTicket constructor to replace SupportRequestService.createSupportTicket
    
    * Copy createInstitutionSOSupportTicket into SupportTicket file
    
    * Create SupportTicketFactory
    
    * Update SupportRequestService to create InstitutionSOSupportTickets through SupportTicketFactory
    
    * Move CreateInstitutionSOSupportTicket tests from SupportRequestServiceTest into SupportTicketFactoryTest
    
    * Remove createSupportTicket from SupportRequestService in favor of constructor in SupportTicket
    
    * Only keep SupportTicket constructors that enforce correct structure
    
    * Rename to SupportTicketCreator
    
    * Remove unused import
    
    * Update test to use java 17 text block
    
    * Update test to use java 17 text block for json string
    
    * Add configuration field to SupportTicketCreator
    
    Co-authored-by: Shae Marks <smarks.dev1@gmail.com>

--- src/main/java/org/broadinstitute/consent/http/ConsentModule.java
+++ src/main/java/org/broadinstitute/consent/http/ConsentModule.java
@@ -541,6 +541,6 @@ public class ConsentModule extends AbstractModule {
 
     @Provides
     SupportRequestService providesSupportRequestService() {
-        return new SupportRequestService(config.getServicesConfiguration());
+        return new SupportRequestService(config.getServicesConfiguration(), providesInstitutionDAO(), providesUserDAO());
     }
 }
