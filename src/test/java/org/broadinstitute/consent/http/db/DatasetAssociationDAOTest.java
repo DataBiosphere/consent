5c55abcf03ff7aa9a14103c378a1f3897a8ce989
quazi-broad <qhoque@broadinstitute.org>
2022-08-31 08:02:13 -0400

    Import a java util for testing

--- src/test/java/org/broadinstitute/consent/http/db/DatasetAssociationDAOTest.java
+++ src/test/java/org/broadinstitute/consent/http/db/DatasetAssociationDAOTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.broadinstitute.consent.http.enumeration.UserRoles;
