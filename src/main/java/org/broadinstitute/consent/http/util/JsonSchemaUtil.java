cc61f19e68505fea1cc49d6264798349b796a176
Greg Rushton <grushton@broadinstitute.org>
2022-10-03 09:31:46 -0400

    resource, api docs, schema objects

--- src/main/java/org/broadinstitute/consent/http/util/JsonSchemaUtil.java
+++ src/main/java/org/broadinstitute/consent/http/util/JsonSchemaUtil.java
@@ -1,5 +1,8 @@
 package org.broadinstitute.consent.http.util;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import org.apache.commons.io.IOUtils;
 import org.everit.json.schema.Schema;
 import org.everit.json.schema.ValidationException;
@@ -8,13 +11,33 @@ import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.concurrent.ExecutionException;
 
 public class JsonSchemaUtil {
 
   private final Logger logger = LoggerFactory.getLogger(this.getClass());
+  private final LoadingCache<String, String> cache;
+  private final String datasetRegistrationSchemaV1 = "/dataset-registration-schema_v1.json";
 
+  public JsonSchemaUtil() {
+    CacheLoader<String, String> loader = new CacheLoader<>() {
+      @Override
+      public String load(String key) throws Exception {
+        return IOUtils.resourceToString(key, Charset.defaultCharset());
+      }
+    };
+    this.cache = CacheBuilder.newBuilder().build(loader);
+  }
+
+  public String getDatasetRegistrationSchemaV1() {
+    try {
+      return cache.get(datasetRegistrationSchemaV1);
+    } catch (ExecutionException ee) {
+      logger.error("Unable to load the data submitter schema: " + ee.getMessage());
+      return null;
+    }
+  }
   /**
    * Compares an instance of a dataset registration object to the dataset registration schema
    *
@@ -23,15 +46,14 @@ public class JsonSchemaUtil {
    */
   public boolean isValidSchema_v1(String datasetRegistrationInstance) {
     try {
-      String schemaString =
-          IOUtils.resourceToString("/dataset-registration-schema_v1.json", Charset.defaultCharset());
+      String schemaString = cache.get(datasetRegistrationSchemaV1);
       JSONObject jsonSchema = new JSONObject(schemaString);
       JSONObject jsonSubject = new JSONObject(datasetRegistrationInstance);
       Schema schema = SchemaLoader.load(jsonSchema);
       schema.validate(jsonSubject);
       return true;
-    } catch (IOException ioe) {
-      logger.error("Unable to load the data submitter schema: " + ioe.getMessage());
+    } catch (ExecutionException ee) {
+      logger.error("Unable to load the data submitter schema: " + ee.getMessage());
       return false;
     } catch (ValidationException ve) {
       if (logger.isDebugEnabled()) {
@@ -45,4 +67,5 @@ public class JsonSchemaUtil {
       return false;
     }
   }
+
 }
