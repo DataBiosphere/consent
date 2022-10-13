15b83d3ff93172292e963ce8c328f71b4c8bd125
Greg Rushton <grushton@broadinstitute.org>
2022-10-13 16:36:02 -0400

    mots

--- src/main/java/org/broadinstitute/consent/http/service/dao/DatasetServiceDAO.java
+++ src/main/java/org/broadinstitute/consent/http/service/dao/DatasetServiceDAO.java
@@ -27,7 +27,6 @@ public class DatasetServiceDAO {
     }
 
     public List<DatasetProperty> synchronizeDatasetProperties(Integer datasetId, List<DatasetProperty> properties) throws SQLException {
-        Timestamp now = new Timestamp(new Date().getTime());
         jdbi.useHandle(
                 handle -> {
                     // By default, new connections are set to auto-commit which breaks our rollback strategy.
@@ -41,7 +40,7 @@ public class DatasetServiceDAO {
                     List<Update> updates = new ArrayList<>(generateDictionaryInsertsForProperties(handle, properties));
                     // We need to know existing properties for all property operations
                     Set<DatasetProperty> existingProps = datasetDAO.findDatasetPropertiesByDatasetId(datasetId);
-                    updates.addAll(generatePropertyInserts(handle, datasetId, properties, existingProps, now));
+                    updates.addAll(generatePropertyInserts(handle, datasetId, properties, existingProps));
                     updates.addAll(generatePropertyUpdates(handle, datasetId, properties, existingProps));
                     updates.addAll(generatePropertyDeletes(handle, properties, existingProps));
                     updates.forEach(Update::execute);
@@ -70,7 +69,8 @@ public class DatasetServiceDAO {
         return insert;
     }
 
-    private List<Update> generatePropertyInserts(Handle handle, Integer datasetId, List<DatasetProperty> properties, Set<DatasetProperty> existingProps, Timestamp now) {
+    private List<Update> generatePropertyInserts(Handle handle, Integer datasetId, List<DatasetProperty> properties, Set<DatasetProperty> existingProps) {
+        Timestamp now = new Timestamp(new Date().getTime());
         List<Update> updates = new ArrayList<>();
         List<String> existingPropNames = existingProps.stream().map(DatasetProperty::getPropertyName).toList();
         // Generate new inserts for props we don't know about yet
@@ -110,7 +110,7 @@ public class DatasetServiceDAO {
     }
 
     private Update createPropertyInsert(Handle handle, DatasetProperty property, Timestamp now) {
-        // TODO - the key will need to be a subquery
+        // TODO - the key will need to be a subquery since it could be a previous insert into dictionary
         final String sql = """
             INSERT INTO dataset_property (dataset_id, property_key, schema_property, property_value, property_type, create_date ) 
             VALUES (:datasetId, :propertyKey, :schemaProperty, :propertyStringValue, :propertyTypeValue, :createDate)
