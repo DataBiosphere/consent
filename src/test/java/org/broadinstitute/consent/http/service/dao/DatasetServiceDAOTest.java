1c47239066d676e068cf0a620a224e5c065e261e
Greg Rushton <grushton@broadinstitute.org>
2022-10-14 12:05:10 -0400

    cleanup after tests

--- src/test/java/org/broadinstitute/consent/http/service/dao/DatasetServiceDAOTest.java
+++ src/test/java/org/broadinstitute/consent/http/service/dao/DatasetServiceDAOTest.java
@@ -19,7 +19,6 @@ import java.util.List;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 public class DatasetServiceDAOTest extends DAOTestHelper {
