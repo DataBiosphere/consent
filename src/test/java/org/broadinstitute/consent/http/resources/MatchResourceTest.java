c9bb384b6338ba534abd9f398333c924899bc2c0
Greg Rushton <grushton@broadinstitute.org>
2022-11-08 16:40:50 -0500

    rm unused match endpoints

--- src/test/java/org/broadinstitute/consent/http/resources/MatchResourceTest.java
+++ src/test/java/org/broadinstitute/consent/http/resources/MatchResourceTest.java
@@ -9,8 +9,6 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import javax.ws.rs.NotFoundException;
-import javax.ws.rs.ServerErrorException;
 import javax.ws.rs.core.Response;
 import java.util.Collections;
 import java.util.List;
@@ -20,8 +18,8 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.openMocks;
 
 public class MatchResourceTest {
 
@@ -33,56 +31,11 @@ public class MatchResourceTest {
 
   @Before
   public void setUp() {
-    MockitoAnnotations.openMocks(this);
+    openMocks(this);
   }
 
   private void initResource() {
     resource = new MatchResource(service);
-  }
-
-  @Test
-  public void testGetMatchByConsentAndPurpose() {
-    when(service.findMatchByConsentIdAndPurposeId(any(), any())).thenReturn(new Match());
-    initResource();
-
-    Response response = resource.getMatchByConsentAndPurpose(authUser,
-            UUID.randomUUID().toString(),
-            UUID.randomUUID().toString());
-    assertEquals(HttpStatusCodes.STATUS_CODE_OK, response.getStatus());
-  }
-
-  @Test
-  public void testGetMatchByConsentAndPurposeNotFound() {
-    doThrow(new NotFoundException()).when(service).findMatchByConsentIdAndPurposeId(any(), any());
-    initResource();
-
-    Response response = resource.getMatchByConsentAndPurpose(authUser,
-            UUID.randomUUID().toString(),
-            UUID.randomUUID().toString());
-    assertEquals(HttpStatusCodes.STATUS_CODE_NOT_FOUND, response.getStatus());
-  }
-
-  @Test
-  public void testGetMatchByConsentAndPurposeServerException() {
-    doThrow(new ServerErrorException(500))
-        .when(service)
-        .findMatchByConsentIdAndPurposeId(any(), any());
-    initResource();
-
-    Response response = resource.getMatchByConsentAndPurpose(authUser,
-            UUID.randomUUID().toString(),
-            UUID.randomUUID().toString());
-    assertEquals(HttpStatusCodes.STATUS_CODE_SERVER_ERROR, response.getStatus());
-  }
-
-  @Test
-  public void testGetMatchesForConsent() {
-    when(service.findMatchesByPurposeId(any())).thenReturn(Collections.singletonList(new Match()));
-    initResource();
-
-    Response response = resource.getMatchesForConsent(authUser,
-            UUID.randomUUID().toString());
-    assertEquals(HttpStatusCodes.STATUS_CODE_OK, response.getStatus());
   }
 
   @Test
