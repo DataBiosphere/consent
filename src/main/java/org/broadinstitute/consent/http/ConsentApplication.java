8ef701891f97e3924e4f31d5d58521e02e287052
Greg Rushton <grushton@broadinstitute.org>
2022-11-08 15:30:11 -0500

    rm comment

--- src/main/java/org/broadinstitute/consent/http/ConsentApplication.java
+++ src/main/java/org/broadinstitute/consent/http/ConsentApplication.java
@@ -240,7 +240,6 @@ public class ConsentApplication extends Application<ConsentConfiguration> {
         env.jersey().register(new DacResource(dacService, userService, datasetService));
         env.jersey().register(new DACUserResource(userService));
         env.jersey().register(new DarCollectionResource(dataAccessRequestService, darCollectionService, userService));
-//        env.jersey().register(new DataRequestElectionResource(dataAccessRequestService, emailService, voteService, electionService));
         env.jersey().register(new DataRequestVoteResource(darCollectionService, datasetAssociationService, emailService, voteService, datasetService, electionService, userService));
         env.jersey().register(new DataRequestCasesResource(summaryService));
         env.jersey().register(new DataRequestReportsResource(dataAccessRequestService));
