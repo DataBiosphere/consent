a84ba2b0a4aeb8fe9e95fd66e7ed62776a85c6b8
Gregory Rushton <rushtong@users.noreply.github.com>
2023-01-19 14:15:09 -0500

    [DUOS-2197][risk=low] New Admin-only Update DataUse API (#1895)
    * update data use api

--- src/main/java/org/broadinstitute/consent/http/resources/DatasetResource.java
+++ src/main/java/org/broadinstitute/consent/http/resources/DatasetResource.java
@@ -1,29 +1,21 @@
 package org.broadinstitute.consent.http.resources;
 
-import com.google.cloud.storage.BlobId;
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 import com.google.inject.Inject;
 import io.dropwizard.auth.Auth;
-import org.apache.commons.collections.CollectionUtils;
-import org.broadinstitute.consent.http.enumeration.UserRoles;
-import org.broadinstitute.consent.http.models.AuthUser;
-import org.broadinstitute.consent.http.models.Dataset;
-import org.broadinstitute.consent.http.models.Dictionary;
-import org.broadinstitute.consent.http.models.User;
-import org.broadinstitute.consent.http.models.UserRole;
-import org.broadinstitute.consent.http.models.dataset_registration_v1.DatasetRegistrationSchemaV1;
-import org.broadinstitute.consent.http.models.dto.DatasetDTO;
-import org.broadinstitute.consent.http.models.dto.DatasetPropertyDTO;
-import org.broadinstitute.consent.http.service.DataAccessRequestService;
-import org.broadinstitute.consent.http.service.DatasetService;
-import org.broadinstitute.consent.http.service.UserService;
-import org.broadinstitute.consent.http.util.JsonSchemaUtil;
-import org.glassfish.jersey.media.multipart.BodyPart;
-import org.glassfish.jersey.media.multipart.FormDataBodyPart;
-import org.glassfish.jersey.media.multipart.FormDataMultiPart;
-import org.glassfish.jersey.media.multipart.FormDataParam;
-import org.json.JSONObject;
-
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.BadRequestException;
@@ -45,19 +37,25 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import org.apache.commons.collections.CollectionUtils;
+import org.broadinstitute.consent.http.enumeration.UserRoles;
+import org.broadinstitute.consent.http.models.AuthUser;
+import org.broadinstitute.consent.http.models.DataUse;
+import org.broadinstitute.consent.http.models.Dataset;
+import org.broadinstitute.consent.http.models.Dictionary;
+import org.broadinstitute.consent.http.models.User;
+import org.broadinstitute.consent.http.models.UserRole;
+import org.broadinstitute.consent.http.models.dataset_registration_v1.DatasetRegistrationSchemaV1;
+import org.broadinstitute.consent.http.models.dto.DatasetDTO;
+import org.broadinstitute.consent.http.models.dto.DatasetPropertyDTO;
+import org.broadinstitute.consent.http.service.DataAccessRequestService;
+import org.broadinstitute.consent.http.service.DatasetService;
+import org.broadinstitute.consent.http.service.UserService;
+import org.broadinstitute.consent.http.util.JsonSchemaUtil;
+import org.glassfish.jersey.media.multipart.FormDataBodyPart;
+import org.glassfish.jersey.media.multipart.FormDataMultiPart;
+import org.glassfish.jersey.media.multipart.FormDataParam;
+import org.json.JSONObject;
 
 @Path("api/dataset")
 public class DatasetResource extends Resource {
@@ -179,7 +177,7 @@ public class DatasetResource extends Resource {
                     files);
 
             URI uri = UriBuilder.fromPath("/api/dataset/v2").build();
-            return Response.created(uri).entity(unmarshal(datasets)).build();
+            return Response.created(uri).entity(datasets).build();
         } catch (Exception e) {
             return createExceptionResponse(e);
         }
@@ -276,7 +274,7 @@ public class DatasetResource extends Resource {
         try {
             User user = userService.findUserByEmail(authUser.getEmail());
             List<Dataset> datasets = datasetService.findAllDatasetsByUser(user);
-            return Response.ok(unmarshal(datasets)).build();
+            return Response.ok(datasets).build();
         } catch (Exception e) {
             return createExceptionResponse(e);
         }
@@ -306,7 +304,7 @@ public class DatasetResource extends Resource {
             if (Objects.isNull(dataset)) {
                 throw new NotFoundException("Could not find the dataset with id: " + datasetId.toString());
             }
-            return Response.ok(unmarshal(dataset)).build();
+            return Response.ok(dataset).build();
         } catch (Exception e){
             return createExceptionResponse(e);
         }
@@ -330,7 +328,7 @@ public class DatasetResource extends Resource {
                                 + String.join(",", differences.stream().map((i) -> i.toString()).collect(Collectors.toSet())));
 
             }
-            return Response.ok(unmarshal(datasets)).build();
+            return Response.ok(datasets).build();
         } catch (Exception e){
             return createExceptionResponse(e);
         }
@@ -475,7 +473,7 @@ public class DatasetResource extends Resource {
             User dacUser = userService.findUserByEmail(authUser.getEmail());
             Integer dacUserId = dacUser.getUserId();
             List<Map<String, String>> datasets = datasetService.autoCompleteDatasets(partial, dacUserId);
-            return Response.ok(unmarshal(datasets), MediaType.APPLICATION_JSON).build();
+            return Response.ok(datasets, MediaType.APPLICATION_JSON).build();
         } catch (Exception e) {
             return createExceptionResponse(e);
         }
@@ -503,6 +501,31 @@ public class DatasetResource extends Resource {
             Dataset dataset = datasetService.updateNeedsReviewDatasets(dataSetId, needsApproval);
             return Response.ok().entity(unmarshal(dataset)).build();
         }catch (Exception e){
+            return createExceptionResponse(e);
+        }
+    }
+
+    @PUT
+    @Produces("application/json")
+    @RolesAllowed(ADMIN)
+    @Path("/{id}/datause")
+    public Response updateDatasetDataUse(@Auth AuthUser authUser, @PathParam("id") Integer id, String dataUseJson) {
+        try {
+            User user = userService.findUserByEmail(authUser.getEmail());
+            Gson gson = new Gson();
+            DataUse dataUse = gson.fromJson(dataUseJson, DataUse.class);
+            Dataset originalDataset = datasetService.findDatasetById(id);
+            if (Objects.isNull(originalDataset)) {
+                throw new NotFoundException("Dataset not found: " + id);
+            }
+            if (Objects.equals(dataUse, originalDataset.getDataUse())) {
+                return Response.notModified().entity(originalDataset).build();
+            }
+            Dataset dataset = datasetService.updateDatasetDataUse(user, id, dataUse);
+            return Response.ok().entity(dataset).build();
+        } catch (JsonSyntaxException jse) {
+            return createExceptionResponse(new BadRequestException("Invalid JSON Syntax: " + dataUseJson));
+        } catch (Exception e) {
             return createExceptionResponse(e);
         }
     }
