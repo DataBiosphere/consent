c6245df1f979db663b6dd320cd178f163be0852f
Greg Rushton <grushton@broadinstitute.org>
2023-02-17 08:59:19 -0500

    broken test, but uses the correct implementation model

--- src/test/java/org/broadinstitute/consent/pact/sam/SamPactTests.java
+++ src/test/java/org/broadinstitute/consent/pact/sam/SamPactTests.java
@@ -1,22 +1,19 @@
 package org.broadinstitute.consent.pact.sam;
 
-import au.com.dius.pact.consumer.MockServer;
+import static org.junit.Assert.assertEquals;
+
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
-import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
-import au.com.dius.pact.consumer.junit5.PactTestFor;
+import au.com.dius.pact.consumer.junit.PactProviderRule;
+import au.com.dius.pact.consumer.junit.PactVerification;
 import au.com.dius.pact.core.model.RequestResponsePact;
 import au.com.dius.pact.core.model.annotations.Pact;
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
-import org.apache.hc.client5.http.fluent.Request;
-import org.apache.hc.core5.http.ClassicHttpResponse;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.Rule;
+import org.junit.Test;
 
 /**
+ * Modeled on https://github.com/pact-foundation/pact-jvm/blob/master/consumer/junit/src/test/java/au/com/dius/pact/consumer/junit/examples/ExampleJavaConsumerPactRuleTest.java
+ *
  * Pact Consumer Contract and test for interactions with Sam
  *
  * TODO:
@@ -24,51 +21,47 @@ import org.junit.jupiter.api.extension.ExtendWith;
  * GitHub action for pushing to pact broker
  * Expand test suite
  */
-@ExtendWith(PactConsumerTestExt.class)
-@PactTestFor(providerName="SamProvider")
 public class SamPactTests {
 
   private static final String SELF_INFO_URL = "/register/user/v2/self/info";
+  private static final String providerName = "SamProvider";
+  private static final String consumerName = "SamConsumer";
 
-  @BeforeEach
-  public void setUp(MockServer mockServer) {
-    Assertions.assertNotNull(mockServer);
-  }
+  @Rule
+  public PactProviderRule mockProvider = new PactProviderRule(providerName, this);
 
   /**
-   * This defines an expectation that Consent has of Sam. This expectation will
-   * be published to a Pact Broker so that if our expectation differs from what
-   * is published, we'll be notified via GitHub Action runs.
+   * This defines an expectation that Consent has of Sam. This expectation will be published to a
+   * Pact Broker so that if our expectation differs from what is published, we'll be notified via
+   * GitHub Action runs.
    *
    * @param builder PactDslWithProvider
-   * @return RequestResponsePact
+   * @return RequestResponsePactmockProvider
    */
-  @Pact(provider="SamProvider", consumer = "test_consumer")
-  public RequestResponsePact selfInfoPact(PactDslWithProvider builder) {
-    Map<String, String> headers = new HashMap<>();
-    headers.put("Content-Type", "application/json");
+  @Pact(provider=providerName, consumer=consumerName)
+  public RequestResponsePact createPact(PactDslWithProvider builder) {
+    Map<String, String> headers = Map.of("Content-Type", "application/json");
     return builder
         .given("test GET")
-          .uponReceiving("GET REQUEST")
-          .path(SELF_INFO_URL)
-          .method("GET")
+        .uponReceiving("GET REQUEST")
+        .path(SELF_INFO_URL)
+        .method("GET")
         .willRespondWith()
-          .status(200)
-          .headers(headers)
-          .body("""
-              {
-                 "adminEnabled": true,
-                 "enabled": true,
-                 "userEmail": "gregory.rushton@gmail.com",
-                 "userSubjectId": "104394682457782759726"
-               }""")
-          .toPact();
+        .status(200)
+        .headers(headers)
+        .body("""
+            {
+               "adminEnabled": true,
+               "enabled": true,
+               "userEmail": "test.user@gmail.com",
+               "userSubjectId": "1234567890"
+             }""")
+        .toPact();
   }
 
   @Test
-  @PactTestFor(pactMethod = "selfInfoPact")
-  void test(MockServer mockServer) throws IOException {
-    ClassicHttpResponse httpResponse = (ClassicHttpResponse)  Request.get(mockServer.getUrl() + SELF_INFO_URL).execute().returnResponse();
-    Assertions.assertEquals(200, httpResponse.getCode());
+  @PactVerification(providerName)
+  public void runTest() throws Exception {
+        assertEquals(200, new ConsumerClient(mockProvider.getUrl()).options(SELF_INFO_URL));
   }
 }
