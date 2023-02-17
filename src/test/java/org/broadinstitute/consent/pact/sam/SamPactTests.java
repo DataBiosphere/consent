8a6d0527c8f9d5c397d56f6c6c2b4b363ed0c617
Greg Rushton <grushton@broadinstitute.org>
2023-02-17 09:23:50 -0500

    fix versions and test

--- src/test/java/org/broadinstitute/consent/pact/sam/SamPactTests.java
+++ src/test/java/org/broadinstitute/consent/pact/sam/SamPactTests.java
@@ -1,6 +1,7 @@
 package org.broadinstitute.consent.pact.sam;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.consumer.junit.PactProviderRule;
@@ -17,7 +18,6 @@ import org.junit.Test;
  * Pact Consumer Contract and test for interactions with Sam
  *
  * TODO:
- * Figure out this error: java.lang.IncompatibleClassChangeError: Expected static method 'java.lang.Object au.com.dius.pact.core.support.expressions.ExpressionParser.parseExpression(java.lang.String, au.com.dius.pact.core.support.expressions.DataType)'
  * GitHub action for pushing to pact broker
  * Expand test suite
  */
@@ -36,32 +36,41 @@ public class SamPactTests {
    * GitHub Action runs.
    *
    * @param builder PactDslWithProvider
-   * @return RequestResponsePactmockProvider
+   * @return RequestResponsePact
    */
   @Pact(provider=providerName, consumer=consumerName)
   public RequestResponsePact createPact(PactDslWithProvider builder) {
     Map<String, String> headers = Map.of("Content-Type", "application/json");
     return builder
         .given("test GET")
-        .uponReceiving("GET REQUEST")
-        .path(SELF_INFO_URL)
-        .method("GET")
+        .uponReceiving("GET Request")
+          .path(SELF_INFO_URL)
+          .method("GET")
         .willRespondWith()
-        .status(200)
-        .headers(headers)
-        .body("""
-            {
-               "adminEnabled": true,
-               "enabled": true,
-               "userEmail": "test.user@gmail.com",
-               "userSubjectId": "1234567890"
-             }""")
+          .status(200)
+          .headers(headers)
+          .body("""
+              {
+                 "adminEnabled": true,
+                 "enabled": true,
+                 "userEmail": "test.user@gmail.com",
+                 "userSubjectId": "1234567890"
+               }""")
+        .given("test OPTIONS")
+        .uponReceiving("OPTIONS Request")
+          .path(SELF_INFO_URL)
+          .method("OPTIONS")
+        .willRespondWith()
+          .status(200)
         .toPact();
   }
 
   @Test
   @PactVerification(providerName)
-  public void runTest() throws Exception {
-        assertEquals(200, new ConsumerClient(mockProvider.getUrl()).options(SELF_INFO_URL));
+  public void testGet() throws Exception {
+    ConsumerClient client = new ConsumerClient(mockProvider.getUrl());
+    Map responseMap = client.get(SELF_INFO_URL);
+    assertFalse(responseMap.isEmpty());
+    assertEquals(200, client.options(SELF_INFO_URL));
   }
 }
