869401f5f6ddb1085d62c7eca74fcf3f789f4997
Greg Rushton <grushton@broadinstitute.org>
2023-03-01 08:01:51 -0500

    update Sam logging

--- src/main/java/org/broadinstitute/consent/http/db/SamDAO.java
+++ src/main/java/org/broadinstitute/consent/http/db/SamDAO.java
@@ -4,7 +4,6 @@ import com.google.api.client.http.EmptyContent;
 import com.google.api.client.http.GenericUrl;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpResponse;
-import com.google.api.client.http.HttpStatusCodes;
 import com.google.api.client.http.json.JsonHttpContent;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.common.util.concurrent.FutureCallback;
@@ -14,6 +13,13 @@ import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.common.util.concurrent.MoreExecutors;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import javax.ws.rs.ServerErrorException;
+import javax.ws.rs.core.MediaType;
 import org.broadinstitute.consent.http.configurations.ServicesConfiguration;
 import org.broadinstitute.consent.http.models.AuthUser;
 import org.broadinstitute.consent.http.models.sam.ResourceType;
@@ -21,26 +27,16 @@ import org.broadinstitute.consent.http.models.sam.TosResponse;
 import org.broadinstitute.consent.http.models.sam.UserStatus;
 import org.broadinstitute.consent.http.models.sam.UserStatusDiagnostics;
 import org.broadinstitute.consent.http.models.sam.UserStatusInfo;
+import org.broadinstitute.consent.http.util.ConsentLogger;
 import org.broadinstitute.consent.http.util.HttpClientUtil;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import javax.ws.rs.core.MediaType;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-public class SamDAO {
+public class SamDAO implements ConsentLogger {
 
   private final ExecutorService executorService;
   private final HttpClientUtil clientUtil;
   private final ServicesConfiguration configuration;
-  private final Logger logger = LoggerFactory.getLogger(this.getClass());
-
   public SamDAO(ServicesConfiguration configuration) {
     this.executorService = Executors.newCachedThreadPool();
     this.clientUtil = new HttpClientUtil(configuration);
@@ -51,8 +47,8 @@ public class SamDAO {
     GenericUrl genericUrl = new GenericUrl(configuration.getV1ResourceTypesUrl());
     HttpRequest request = clientUtil.buildGetRequest(genericUrl, authUser);
     HttpResponse response = clientUtil.handleHttpRequest(request);
-    if (response.getStatusCode() != HttpStatusCodes.STATUS_CODE_OK) {
-      logger.error("Error getting resource types from Sam: " + response.getStatusMessage());
+    if (!response.isSuccessStatusCode()) {
+      logException("Error getting resource types from Sam: " + response.getStatusMessage(), new ServerErrorException(response.getStatusMessage(), response.getStatusCode()));
     }
     String body = response.parseAsString();
     Type resourceTypesListType = new TypeToken<ArrayList<ResourceType>>() {}.getType();
@@ -63,8 +59,8 @@ public class SamDAO {
     GenericUrl genericUrl = new GenericUrl(configuration.getRegisterUserV2SelfInfoUrl());
     HttpRequest request = clientUtil.buildGetRequest(genericUrl, authUser);
     HttpResponse response = clientUtil.handleHttpRequest(request);
-    if (response.getStatusCode() != HttpStatusCodes.STATUS_CODE_OK) {
-      logger.error("Error getting user registration information from Sam: " + response.getStatusMessage());
+    if (!response.isSuccessStatusCode()) {
+      logException("Error getting user registration information from Sam: " + response.getStatusMessage(), new ServerErrorException(response.getStatusMessage(), response.getStatusCode()));
     }
     String body = response.parseAsString();
     return new Gson().fromJson(body, UserStatusInfo.class);
@@ -74,8 +70,8 @@ public class SamDAO {
     GenericUrl genericUrl = new GenericUrl(configuration.getV2SelfDiagnosticsUrl());
     HttpRequest request = clientUtil.buildGetRequest(genericUrl, authUser);
     HttpResponse response = clientUtil.handleHttpRequest(request);
-    if (response.getStatusCode() != HttpStatusCodes.STATUS_CODE_OK) {
-      logger.error("Error getting enabled statuses of user from Sam: " + response.getStatusMessage());
+    if (!response.isSuccessStatusCode()) {
+      logException("Error getting enabled statuses of user from Sam: " + response.getStatusMessage(), new ServerErrorException(response.getStatusMessage(), response.getStatusCode()));
     }
     String body = response.parseAsString();
     return new Gson().fromJson(body, UserStatusDiagnostics.class);
@@ -85,8 +81,8 @@ public class SamDAO {
     GenericUrl genericUrl = new GenericUrl(configuration.postRegisterUserV2SelfUrl());
     HttpRequest request = clientUtil.buildPostRequest(genericUrl, new EmptyContent(), authUser);
     HttpResponse response = clientUtil.handleHttpRequest(request);
-    if (response.getStatusCode() != HttpStatusCodes.STATUS_CODE_OK) {
-      logger.error("Error posting user registration information to Sam: " + response.getStatusMessage());
+    if (!response.isSuccessStatusCode()) {
+      logException("Error posting user registration information to Sam: " + response.getStatusMessage(), new ServerErrorException(response.getStatusMessage(), response.getStatusCode()));
     }
     String body = response.parseAsString();
     return new Gson().fromJson(body, UserStatus.class);
@@ -101,12 +97,11 @@ public class SamDAO {
         new FutureCallback<>() {
           @Override
           public void onSuccess(@Nullable UserStatus userStatus) {
-            logger.info("Successfully registered user in Sam: " + authUser.getEmail());
+            logInfo("Successfully registered user in Sam: " + authUser.getEmail());
           }
-
           @Override
           public void onFailure(@NonNull Throwable throwable) {
-            logger.error(throwable.getMessage());
+            logException(throwable.getMessage(), new ServerErrorException(throwable.getMessage(), 500));
           }
         },
         listeningExecutorService);
@@ -117,8 +112,8 @@ public class SamDAO {
     HttpRequest request = clientUtil.buildUnAuthedGetRequest(genericUrl);
     request.getHeaders().setAccept(MediaType.TEXT_PLAIN);
     HttpResponse response = clientUtil.handleHttpRequest(request);
-    if (response.getStatusCode() != HttpStatusCodes.STATUS_CODE_OK) {
-      logger.error("Error getting Terms of Service text from Sam: " + response.getStatusMessage());
+    if (!response.isSuccessStatusCode()) {
+      logException("Error getting Terms of Service text from Sam: " + response.getStatusMessage(), new ServerErrorException(response.getStatusMessage(), response.getStatusCode()));
     }
     return response.parseAsString();
   }
@@ -128,8 +123,8 @@ public class SamDAO {
     JsonHttpContent content = new JsonHttpContent(new GsonFactory(), "app.terra.bio/#terms-of-service");
     HttpRequest request = clientUtil.buildPostRequest(genericUrl, content, authUser);
     HttpResponse response = clientUtil.handleHttpRequest(request);
-    if (response.getStatusCode() != HttpStatusCodes.STATUS_CODE_OK) {
-      logger.error("Error accepting Terms of Service through Sam: " + response.getStatusMessage());
+    if (!response.isSuccessStatusCode()) {
+      logException("Error accepting Terms of Service through Sam: " + response.getStatusMessage(), new ServerErrorException(response.getStatusMessage(), response.getStatusCode()));
     }
     String body = response.parseAsString();
     return new Gson().fromJson(body, TosResponse.class);
@@ -139,8 +134,8 @@ public class SamDAO {
     GenericUrl genericUrl = new GenericUrl(configuration.tosRegistrationUrl());
     HttpRequest request = clientUtil.buildDeleteRequest(genericUrl, authUser);
     HttpResponse response = clientUtil.handleHttpRequest(request);
-    if (response.getStatusCode() != HttpStatusCodes.STATUS_CODE_OK) {
-      logger.error("Error removing Terms of Service acceptance through Sam: " + response.getStatusMessage());
+    if (!response.isSuccessStatusCode()) {
+      logException("Error removing Terms of Service acceptance through Sam: " + response.getStatusMessage(), new ServerErrorException(response.getStatusMessage(), response.getStatusCode()));
     }
     String body = response.parseAsString();
     return new Gson().fromJson(body, TosResponse.class);
