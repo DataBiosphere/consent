cc5eeb8baaa26b6a5dd0ca8da287466db50be193
Greg Rushton <grushton@broadinstitute.org>
2023-03-09 13:40:16 -0500

    more tests

--- src/test/java/org/broadinstitute/consent/pact/sam/SamPactTests.java
+++ src/test/java/org/broadinstitute/consent/pact/sam/SamPactTests.java
@@ -1,5 +1,6 @@
 package org.broadinstitute.consent.pact.sam;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
 import au.com.dius.pact.consumer.MockServer;
@@ -8,10 +9,12 @@ import au.com.dius.pact.consumer.junit.PactProviderRule;
 import au.com.dius.pact.consumer.junit.PactVerification;
 import au.com.dius.pact.core.model.RequestResponsePact;
 import au.com.dius.pact.core.model.annotations.Pact;
+import java.util.List;
 import java.util.Map;
 import org.broadinstitute.consent.http.configurations.ServicesConfiguration;
 import org.broadinstitute.consent.http.db.SamDAO;
 import org.broadinstitute.consent.http.models.AuthUser;
+import org.broadinstitute.consent.http.models.sam.ResourceType;
 import org.broadinstitute.consent.http.models.sam.UserStatusDiagnostics;
 import org.broadinstitute.consent.http.models.sam.UserStatusInfo;
 import org.broadinstitute.consent.pact.PactTests;
@@ -21,35 +24,32 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 /**
- * Modeled on <a href="https://github.com/pact-foundation/pact-jvm/blob/master/consumer/junit/src/test/java/au/com/dius/pact/consumer/junit/examples/ExampleJavaConsumerPactRuleTest.java">ExampleJavaConsumerPactRuleTest.java</a>
- *
- * Pact Consumer Contract and test for interactions with Sam
- *
- * TODO:
- *    Expand test suite
- *    Can we split tests up more granularly???
+ * Pact Consumer Contract and test for interactions between Consent and Sam
  */
 @Category(PactTests.class)
 public class SamPactTests {
 
-  private static final String SELF_INFO_URL = "/register/user/v2/self/info";
-  private static final String SELF_DIAGNOSTICS_URL = "/register/user/v2/self/diagnostics";
   private static final String PROVIDER_NAME = "sam-provider";
   private static final String CONSUMER_NAME = "consent-consumer";
+
+  private static final List<ResourceType> RESOURCE_TYPES = List.of(
+      new ResourceType()
+          .setName("resource-name")
+  );
   private static final UserStatusInfo USER_STATUS_INFO =
-    new UserStatusInfo()
-      .setAdminEnabled(true)
-      .setEnabled(true)
-      .setUserEmail("test.user@gmail.com")
-      .setUserSubjectId("1234567890");
+      new UserStatusInfo()
+          .setAdminEnabled(true)
+          .setEnabled(true)
+          .setUserEmail("test.user@gmail.com")
+          .setUserSubjectId("1234567890");
 
   private static final UserStatusDiagnostics USER_STATUS_DIAGNOSTICS =
-    new UserStatusDiagnostics()
-      .setAdminEnabled(true)
-      .setEnabled(true)
-      .setInAllUsersGroup(true)
-      .setInGoogleProxyGroup(true)
-      .setTosAccepted(true);
+      new UserStatusDiagnostics()
+          .setAdminEnabled(true)
+          .setEnabled(true)
+          .setInAllUsersGroup(true)
+          .setInGoogleProxyGroup(true)
+          .setTosAccepted(true);
 
   private SamDAO samDAO;
 
@@ -76,28 +76,39 @@ public class SamPactTests {
    * @param builder PactDslWithProvider
    * @return RequestResponsePact
    */
-  @Pact(provider=PROVIDER_NAME, consumer=CONSUMER_NAME)
+  @Pact(provider = PROVIDER_NAME, consumer = CONSUMER_NAME)
   public RequestResponsePact createPact(PactDslWithProvider builder) {
-    Map<String, String> headers = Map.of("Content-Type", "application/json");
+    Map<String, String> headers = Map.of(
+        "Content-Type", "application/json",
+        "Authentication", "Bearer: auth-token");
     return builder
+        // Resource Types:
+        .given(" GET Sam Resource Types")
+        .uponReceiving(" GET Request: " + ServicesConfiguration.RESOURCE_TYPES_PATH)
+        .path("/" + ServicesConfiguration.RESOURCE_TYPES_PATH)
+        .method("GET")
+        .willRespondWith()
+        .status(200)
+        .headers(headers)
+        .body(RESOURCE_TYPES.toString())
         // Self Info:
         .given(" GET Sam Self Info")
-        .uponReceiving(" GET Request: " + SELF_INFO_URL)
-          .path(SELF_INFO_URL)
-          .method("GET")
+        .uponReceiving(" GET Request: " + ServicesConfiguration.REGISTER_SELF_INFO_PATH)
+        .path("/" + ServicesConfiguration.REGISTER_SELF_INFO_PATH)
+        .method("GET")
         .willRespondWith()
-          .status(200)
-          .headers(headers)
-          .body(USER_STATUS_INFO.toString())
+        .status(200)
+        .headers(headers)
+        .body(USER_STATUS_INFO.toString())
         // Self Diagnostics:
         .given(" GET Sam Self Diagnostics")
-        .uponReceiving(" GET Request: " + SELF_DIAGNOSTICS_URL)
-          .path(SELF_DIAGNOSTICS_URL)
-          .method("GET")
+        .uponReceiving(" GET Request: " + ServicesConfiguration.REGISTER_SELF_DIAGNOSTICS_PATH)
+        .path("/" + ServicesConfiguration.REGISTER_SELF_DIAGNOSTICS_PATH)
+        .method("GET")
         .willRespondWith()
-          .status(200)
-          .headers(headers)
-          .body(USER_STATUS_DIAGNOSTICS.toString())
+        .status(200)
+        .headers(headers)
+        .body(USER_STATUS_DIAGNOSTICS.toString())
         .toPact();
   }
 
@@ -106,6 +117,11 @@ public class SamPactTests {
   public void testSamContracts() throws Exception {
     initSamDAO(mockProvider.getMockServer());
     AuthUser authUser = new AuthUser();
+    authUser.setAuthToken("auth-token");
+
+    List<ResourceType> types = samDAO.getResourceTypes(authUser);
+    assertNotNull(types);
+    assertFalse(types.isEmpty());
 
     UserStatusInfo info = samDAO.getRegistrationInfo(authUser);
     assertNotNull(info);
