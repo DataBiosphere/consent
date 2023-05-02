7f996103ef287ebfa0a3a82a58f078c13121bb78
Greg Rushton <grushton@broadinstitute.org>
2023-05-02 12:21:38 -0400

    citation fixes

--- src/main/java/org/broadinstitute/consent/http/models/Presentation.java
+++ src/main/java/org/broadinstitute/consent/http/models/Presentation.java
@@ -1,4 +1,4 @@
 package org.broadinstitute.consent.http.models;
 
-public record Presentation(String title, String link, String date, String authors, String citation) {
+public record Presentation(String title, String link, String date, String authors, String datasetCitation, Boolean citation) {
 }
