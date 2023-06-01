439454172a89a219d5ee8a6397eac2006b93c5a9
Connor Barker <connorlbark@gmail.com>
2023-06-01 07:19:19 -0400

    cleanup

--- src/main/java/org/broadinstitute/consent/http/service/ontology/ElasticSearchSupport.java
+++ src/main/java/org/broadinstitute/consent/http/service/ontology/ElasticSearchSupport.java
@@ -1,9 +1,5 @@
 package org.broadinstitute.consent.http.service.ontology;
 
-import co.elastic.clients.elasticsearch.ElasticsearchClient;
-import co.elastic.clients.json.jackson.JacksonJsonpMapper;
-import co.elastic.clients.transport.ElasticsearchTransport;
-import co.elastic.clients.transport.rest_client.RestClientTransport;
 import org.apache.http.Header;
 import org.apache.http.HttpHost;
 import org.apache.http.message.BasicHeader;
@@ -21,20 +17,6 @@ public class ElasticSearchSupport {
         toList().
         toArray(new HttpHost[configuration.getServers().size()]);
     return RestClient.builder(hosts).build();
-  }
-
-  public static ElasticsearchClient createClient(ElasticSearchConfiguration configuration) {
-    RestClient restClient = createRestClient(configuration);
-
-    // Create the transport with a Jackson mapper
-    ElasticsearchTransport transport = new RestClientTransport(
-        restClient, new JacksonJsonpMapper());
-
-    // And create the API client
-    ElasticsearchClient client = new ElasticsearchClient(transport);
-
-    return client;
-
   }
 
   public static String getClusterHealthPath() {
