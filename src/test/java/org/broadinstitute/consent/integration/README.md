a1c58105b2f6063a46d22d0cf3d4705dc6bb2367
Greg Rushton <grushton@broadinstitute.org>
2023-09-28 14:17:47 -0400

    cleanup; try actual upload

--- src/test/java/org/broadinstitute/consent/integration/README.md
+++ src/test/java/org/broadinstitute/consent/integration/README.md
@@ -1,16 +1,14 @@
 # Smoke Testing
 
-Provides a mechanism for running simple smoke tests. 
-The intention here is to keep this layer as slim as 
-possible to provide a minimum sense of confidence in 
-application stability. 
+Provides a mechanism for running simple smoke tests. The intention here is to keep this
+layer as slim as possible to provide a minimum sense of confidence in application stability. 
 
 ## Local development/testing process
+
+To run against the default environment (dev), run with no additional arguments:
 
 ```shell
 mvn clean test -P integration-tests
 ```
 
-1. Spin up a local instance in a terminal window using a proper docker compose file
-2. Run integration tests against that environment: `mvn gatling:test`
-   1. You can also run the above command with any custom destination: `mvn gatling:test -DbaseUrl=https://consent.dsde-dev.broadinstitute.org/`
+To run against a custom environment, pass a `-DbaseUrl=` with a valid base url.
--- src/test/java/org/broadinstitute/consent/integration/status/StatusTests.java
+++ src/test/java/org/broadinstitute/consent/integration/status/StatusTests.java
@@ -3,34 +3,38 @@ package org.broadinstitute.consent.integration.status;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.google.api.client.http.HttpStatusCodes;
-import java.nio.charset.Charset;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-import org.apache.commons.io.IOUtils;
-import org.apache.hc.client5.http.classic.HttpClient;
-import org.apache.hc.client5.http.classic.methods.HttpGet;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.broadinstitute.consent.http.util.HttpClientUtil.SimpleResponse;
 import org.broadinstitute.consent.integration.IntegrationTestHelper;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.api.Test;
 
+/**
+ * These tests are not parameterized because that displays poorly in the results xml, i.e. compare:
+ *   Parameterized:
+ *     <testcase name="testStatus(String 1)" classname="org.broadinstitute.consent.integration.status.StatusTests" time="1.004"/>
+ *     <testcase name="testStatus(String 2)" classname="org.broadinstitute.consent.integration.status.StatusTests" time="0.184"/>
+ *     <testcase name="testStatus(String 3)" classname="org.broadinstitute.consent.integration.status.StatusTests" time="0.144"/>
+ *   Non-parameterized:
+ *     <testcase name="testVersion" classname="org.broadinstitute.consent.integration.status.StatusTests" time="1.004"/>
+ *     <testcase name="testStatus" classname="org.broadinstitute.consent.integration.status.StatusTests" time="0.184"/>
+ *     <testcase name="testLiveness" classname="org.broadinstitute.consent.integration.status.StatusTests" time="0.144"/>
+ */
 class StatusTests implements IntegrationTestHelper {
 
-  @Tag("IntegrationTest")
-  @ParameterizedTest
-  @ValueSource(strings = {"liveness", "status", "version"})
-  void testStatus(String path) throws Exception {
-    HttpClient client = HttpClients.createDefault();
-    HttpGet request = new HttpGet(getBaseUrl() + path);
-    final ScheduledExecutorService executor = Executors.newScheduledThreadPool(5);
-    executor.schedule(request::cancel, 30, TimeUnit.SECONDS);
-    SimpleResponse response = client.execute(request, httpResponse ->
-        new SimpleResponse(
-            httpResponse.getCode(),
-            IOUtils.toString(httpResponse.getEntity().getContent(), Charset.defaultCharset())));
+  @Test
+  void testStatus() throws Exception {
+    SimpleResponse response = fetchGetResponse("status");
+    assertEquals(HttpStatusCodes.STATUS_CODE_OK, response.code());
+  }
+
+  @Test
+  void testLiveness() throws Exception {
+    SimpleResponse response = fetchGetResponse("liveness");
+    assertEquals(HttpStatusCodes.STATUS_CODE_OK, response.code());
+  }
+
+  @Test
+  void testVersion() throws Exception {
+    SimpleResponse response = fetchGetResponse("version");
     assertEquals(HttpStatusCodes.STATUS_CODE_OK, response.code());
   }
 
