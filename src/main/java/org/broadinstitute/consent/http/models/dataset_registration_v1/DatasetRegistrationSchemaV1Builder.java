1bcbca67b64781a54673996801d0c6e32791e038
Greg Rushton <grushton@broadinstitute.org>
2023-10-06 12:43:50 -0400

    flesh out study prop parsers and more testing

--- src/main/java/org/broadinstitute/consent/http/models/dataset_registration_v1/DatasetRegistrationSchemaV1Builder.java
+++ src/main/java/org/broadinstitute/consent/http/models/dataset_registration_v1/DatasetRegistrationSchemaV1Builder.java
@@ -1,5 +1,7 @@
 package org.broadinstitute.consent.http.models.dataset_registration_v1;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -15,61 +17,90 @@ import org.broadinstitute.consent.http.util.gson.GsonUtil;
 
 public class DatasetRegistrationSchemaV1Builder {
 
+  public static final String studyType = "studyType";
+  public static final String phenotypeIndication = "phenotypeIndication";
+  public static final String dataCustodianEmail = "dataCustodianEmail";
+  public static final String species = "species";
+  public static final String nihAnvilUse = "nihAnvilUse";
+  public static final String submittingToAnvil = "submittingToAnvil";
+  public static final String dbGaPPhsID = "dbGaPPhsID";
+  public static final String dbGaPStudyRegistrationName = "dbGaPStudyRegistrationName";
+  public static final String embargoReleaseDate = "embargoReleaseDate";
+  public static final String sequencingCenter = "sequencingCenter";
+  public static final String piInstitution = "piInstitution";
+  public static final String nihGrantContractNumber = "nihGrantContractNumber";
+  public static final String nihICsSupportingStudy = "nihICsSupportingStudy";
+  public static final String nihProgramOfficerName = "nihProgramOfficerName";
+  public static final String nihInstitutionCenterSubmission = "nihInstitutionCenterSubmission";
+  public static final String nihGenomicProgramAdministratorName = "nihGenomicProgramAdministratorName";
+  public static final String multiCenterStudy = "multiCenterStudy";
+  public static final String collaboratingSites = "collaboratingSites";
+  public static final String controlledAccessRequiredForGenomicSummaryResultsGSR = "controlledAccessRequiredForGenomicSummaryResultsGSR";
+  public static final String controlledAccessRequiredForGenomicSummaryResultsGSRRequiredExplanation = "controlledAccessRequiredForGenomicSummaryResultsGSRRequiredExplanation";
+  public static final String alternativeDataSharingPlanReasons = "alternativeDataSharingPlanReasons";
+  public static final String alternativeDataSharingPlanExplanation = "alternativeDataSharingPlanExplanation";
+  public static final String alternativeDataSharingPlanFileName = "alternativeDataSharingPlanFileName";
+  public static final String alternativeDataSharingPlanDataSubmitted = "alternativeDataSharingPlanDataSubmitted";
+  public static final String alternativeDataSharingPlanDataReleased = "alternativeDataSharingPlanDataReleased";
+  public static final String alternativeDataSharingPlanTargetDeliveryDate = "alternativeDataSharingPlanTargetDeliveryDate";
+  public static final String alternativeDataSharingPlanTargetPublicReleaseDate = "alternativeDataSharingPlanTargetPublicReleaseDate";
+  public static final String alternativeDataSharingPlanControlledOpenAccess = "alternativeDataSharingPlanControlledOpenAccess";
+
   public DatasetRegistrationSchemaV1 build(Study study, List<Dataset> datasets) {
     DatasetRegistrationSchemaV1 schemaV1 = new DatasetRegistrationSchemaV1();
     if (Objects.nonNull(study)) {
       schemaV1.setStudyId(study.getStudyId());
       schemaV1.setStudyName(study.getName());
-      String studyType = findStringPropValue(study.getProperties(), "studyType");
-      if (Objects.nonNull(studyType)) {
-        schemaV1.setStudyType(DatasetRegistrationSchemaV1.StudyType.fromValue(studyType));
+      String studyTypeVal = findStringPropValue(study.getProperties(), studyType);
+      if (Objects.nonNull(studyTypeVal)) {
+        schemaV1.setStudyType(DatasetRegistrationSchemaV1.StudyType.fromValue(studyTypeVal));
       }
       schemaV1.setStudyDescription(study.getDescription());
       schemaV1.setDataTypes(study.getDataTypes());
-      schemaV1.setPhenotypeIndication(findStringPropValue(study.getProperties(), "phenotypeIndication"));
-      schemaV1.setSpecies(findStringPropValue(study.getProperties(), "species"));
+      schemaV1.setPhenotypeIndication(findStringPropValue(study.getProperties(), phenotypeIndication));
+      schemaV1.setSpecies(findStringPropValue(study.getProperties(), species));
       schemaV1.setPiName(study.getPiName());
       schemaV1.setDataSubmitterUserId(study.getCreateUserId());
-      schemaV1.setDataCustodianEmail(findListStringPropValue(study.getProperties(), "dataCustodianEmail"));
+      schemaV1.setDataCustodianEmail(findListStringPropValue(study.getProperties(), dataCustodianEmail));
       schemaV1.setPublicVisibility(study.getPublicVisibility());
-      String nihAnvilUse = findStringPropValue(study.getProperties(), "nihAnvilUse");
-      if (Objects.nonNull(nihAnvilUse)) {
-        schemaV1.setNihAnvilUse(NihAnvilUse.valueOf(nihAnvilUse));
+      String nihAnvilUseVal = findStringPropValue(study.getProperties(), nihAnvilUse);
+      if (Objects.nonNull(nihAnvilUseVal)) {
+        schemaV1.setNihAnvilUse(NihAnvilUse.fromValue(nihAnvilUseVal));
       }
-      schemaV1.setSubmittingToAnvil(findBooleanPropValue(study.getProperties(), "submittingToAnvil"));
-      schemaV1.setDbGaPPhsID(findStringPropValue(study.getProperties(), "dbGaPPhsID"));
-      schemaV1.setDbGaPStudyRegistrationName(findStringPropValue(study.getProperties(), "dbGaPStudyRegistrationName"));
-      schemaV1.setEmbargoReleaseDate(findStringPropValue(study.getProperties(), "embargoReleaseDate"));
-      schemaV1.setSequencingCenter(findStringPropValue(study.getProperties(), "sequencingCenter"));
-      schemaV1.setPiInstitution(findIntegerPropValue(study.getProperties(), "piInstitution"));
-      schemaV1.setNihGrantContractNumber(findStringPropValue(study.getProperties(), "nihGrantContractNumber"));
+      schemaV1.setSubmittingToAnvil(findBooleanPropValue(study.getProperties(), submittingToAnvil));
+      schemaV1.setDbGaPPhsID(findStringPropValue(study.getProperties(), dbGaPPhsID));
+      schemaV1.setDbGaPStudyRegistrationName(findStringPropValue(study.getProperties(), dbGaPStudyRegistrationName));
+      schemaV1.setEmbargoReleaseDate(findStringPropValue(study.getProperties(), embargoReleaseDate));
+      schemaV1.setSequencingCenter(findStringPropValue(study.getProperties(), sequencingCenter));
+      schemaV1.setPiInstitution(findIntegerPropValue(study.getProperties(), piInstitution));
+      schemaV1.setNihGrantContractNumber(findStringPropValue(study.getProperties(), nihGrantContractNumber));
       schemaV1.setNihICsSupportingStudy(findListNICSSPropValue(study.getProperties()));
-      schemaV1.setNihProgramOfficerName(findStringPropValue(study.getProperties(), "nihProgramOfficerName"));
-      String nihInstitutionCenterSubmission = findStringPropValue(study.getProperties(), "nihInstitutionCenterSubmission");
-      if (Objects.nonNull(nihInstitutionCenterSubmission)) {
-        schemaV1.setNihInstitutionCenterSubmission(NihInstitutionCenterSubmission.valueOf(nihInstitutionCenterSubmission));
+      schemaV1.setNihProgramOfficerName(findStringPropValue(study.getProperties(), nihProgramOfficerName));
+      String nihInstitutionCenterSubmissionVal = findStringPropValue(study.getProperties(), nihInstitutionCenterSubmission);
+      if (Objects.nonNull(nihInstitutionCenterSubmissionVal)) {
+        schemaV1.setNihInstitutionCenterSubmission(NihInstitutionCenterSubmission.fromValue(nihInstitutionCenterSubmissionVal));
       }
-      schemaV1.setNihGenomicProgramAdministratorName(findStringPropValue(study.getProperties(), "nihGenomicProgramAdministratorName"));
-      schemaV1.setMultiCenterStudy(findBooleanPropValue(study.getProperties(), "multiCenterStudy"));
-      schemaV1.setCollaboratingSites(findListStringPropValue(study.getProperties(), "collaboratingSites"));
-      schemaV1.setControlledAccessRequiredForGenomicSummaryResultsGSR(findBooleanPropValue(study.getProperties(), "controlledAccessRequiredForGenomicSummaryResultsGSR"));
-      schemaV1.setControlledAccessRequiredForGenomicSummaryResultsGSRRequiredExplanation(findStringPropValue(study.getProperties(), "controlledAccessRequiredForGenomicSummaryResultsGSRRequiredExplanation"));
+      schemaV1.setNihGenomicProgramAdministratorName(findStringPropValue(study.getProperties(), nihGenomicProgramAdministratorName));
+      schemaV1.setMultiCenterStudy(findBooleanPropValue(study.getProperties(), multiCenterStudy));
+      schemaV1.setCollaboratingSites(findListStringPropValue(study.getProperties(), collaboratingSites));
+      schemaV1.setControlledAccessRequiredForGenomicSummaryResultsGSR(findBooleanPropValue(study.getProperties(), controlledAccessRequiredForGenomicSummaryResultsGSR));
+      schemaV1.setControlledAccessRequiredForGenomicSummaryResultsGSRRequiredExplanation(findStringPropValue(study.getProperties(), controlledAccessRequiredForGenomicSummaryResultsGSRRequiredExplanation));
       if (Objects.nonNull(study.getAlternativeDataSharingPlan())) {
         schemaV1.setAlternativeDataSharingPlan(Boolean.TRUE);
       }
       schemaV1.setAlternativeDataSharingPlanReasons(findListADSPRPropValue(study.getProperties()));
-      schemaV1.setAlternativeDataSharingPlanExplanation(findStringPropValue(study.getProperties(), "alternativeDataSharingPlanExplanation"));
-      schemaV1.setAlternativeDataSharingPlanFileName(findStringPropValue(study.getProperties(), "alternativeDataSharingPlanFileName"));
-      String alternativeDataSharingPlanDataSubmitted = findStringPropValue(study.getProperties(), "alternativeDataSharingPlanDataSubmitted");
-      if (Objects.nonNull(alternativeDataSharingPlanDataSubmitted)) {
-        schemaV1.setAlternativeDataSharingPlanDataSubmitted(AlternativeDataSharingPlanDataSubmitted.fromValue(alternativeDataSharingPlanDataSubmitted));
+      schemaV1.setAlternativeDataSharingPlanExplanation(findStringPropValue(study.getProperties(), alternativeDataSharingPlanExplanation));
+      schemaV1.setAlternativeDataSharingPlanFileName(findStringPropValue(study.getProperties(), alternativeDataSharingPlanFileName));
+      String alternativeDataSharingPlanDataSubmittedVal = findStringPropValue(study.getProperties(), alternativeDataSharingPlanDataSubmitted);
+      if (Objects.nonNull(alternativeDataSharingPlanDataSubmittedVal)) {
+        schemaV1.setAlternativeDataSharingPlanDataSubmitted(AlternativeDataSharingPlanDataSubmitted.fromValue(alternativeDataSharingPlanDataSubmittedVal));
       }
-      schemaV1.setAlternativeDataSharingPlanDataReleased(findBooleanPropValue(study.getProperties(), "alternativeDataSharingPlanDataReleased"));
-      schemaV1.setAlternativeDataSharingPlanTargetDeliveryDate(findStringPropValue(study.getProperties(), "alternativeDataSharingPlanTargetDeliveryDate"));
-      schemaV1.setAlternativeDataSharingPlanTargetPublicReleaseDate(findStringPropValue(study.getProperties(), "alternativeDataSharingPlanTargetPublicReleaseDate"));
-      String alternativeDataSharingPlanControlledOpenAccess = findStringPropValue(study.getProperties(), "alternativeDataSharingPlanControlledOpenAccess");
-      if (Objects.nonNull(alternativeDataSharingPlanControlledOpenAccess)) {
-        schemaV1.setAlternativeDataSharingPlanControlledOpenAccess(AlternativeDataSharingPlanControlledOpenAccess.fromValue(alternativeDataSharingPlanControlledOpenAccess));
+      schemaV1.setAlternativeDataSharingPlanDataReleased(findBooleanPropValue(study.getProperties(), alternativeDataSharingPlanDataReleased));
+      schemaV1.setAlternativeDataSharingPlanTargetDeliveryDate(findStringPropValue(study.getProperties(), alternativeDataSharingPlanTargetDeliveryDate));
+      schemaV1.setAlternativeDataSharingPlanTargetPublicReleaseDate(findStringPropValue(study.getProperties(), alternativeDataSharingPlanTargetPublicReleaseDate));
+      String alternativeDataSharingPlanControlledOpenAccessVal = findStringPropValue(study.getProperties(), alternativeDataSharingPlanControlledOpenAccess);
+      if (Objects.nonNull(alternativeDataSharingPlanControlledOpenAccessVal)) {
+        schemaV1.setAlternativeDataSharingPlanControlledOpenAccess(AlternativeDataSharingPlanControlledOpenAccess.fromValue(alternativeDataSharingPlanControlledOpenAccessVal));
       }
     }
     if (!datasets.isEmpty()) {
@@ -90,7 +121,11 @@ public class DatasetRegistrationSchemaV1Builder {
           .stream()
           .filter(p -> p.getKey().equalsIgnoreCase(key))
           .map(StudyProperty::getValue)
-          .map(p -> GsonUtil.getInstance().toJson(p, List.class))
+          .map(p -> GsonUtil.getInstance().fromJson(p.toString(), JsonElement.class))
+          .map(JsonElement::getAsJsonArray)
+          .map(JsonArray::asList)
+          .flatMap(List::stream)
+          .map(JsonElement::getAsString)
           .toList();
     }
     return List.of();
@@ -100,10 +135,14 @@ public class DatasetRegistrationSchemaV1Builder {
     if (Objects.nonNull(props) && !props.isEmpty()) {
       return props
           .stream()
-          .filter(p -> p.getKey().equalsIgnoreCase("alternativeDataSharingPlanReasons"))
+          .filter(p -> p.getKey().equalsIgnoreCase(alternativeDataSharingPlanReasons))
           .map(StudyProperty::getValue)
-          .map(p -> GsonUtil.getInstance().toJson(p, List.class))
-          .map(AlternativeDataSharingPlanReason::valueOf)
+          .map(p -> GsonUtil.getInstance().fromJson(p.toString(), JsonElement.class))
+          .map(JsonElement::getAsJsonArray)
+          .map(JsonArray::asList)
+          .flatMap(List::stream)
+          .map(JsonElement::getAsString)
+          .map(AlternativeDataSharingPlanReason::fromValue)
           .toList();
     }
     return List.of();
@@ -113,10 +152,14 @@ public class DatasetRegistrationSchemaV1Builder {
     if (Objects.nonNull(props) && !props.isEmpty()) {
       return props
           .stream()
-          .filter(p -> p.getKey().equalsIgnoreCase("nihICsSupportingStudy"))
+          .filter(p -> p.getKey().equalsIgnoreCase(nihICsSupportingStudy))
           .map(StudyProperty::getValue)
-          .map(p -> GsonUtil.getInstance().toJson(p, List.class))
-          .map(NihICsSupportingStudy::valueOf)
+          .map(p -> GsonUtil.getInstance().fromJson(p.toString(), JsonElement.class))
+          .map(JsonElement::getAsJsonArray)
+          .map(JsonArray::asList)
+          .flatMap(List::stream)
+          .map(JsonElement::getAsString)
+          .map(NihICsSupportingStudy::fromValue)
           .toList();
     }
     return List.of();
--- src/test/java/org/broadinstitute/consent/http/models/DatasetRegistrationSchemaV1BuilderTest.java
+++ src/test/java/org/broadinstitute/consent/http/models/DatasetRegistrationSchemaV1BuilderTest.java
@@ -2,9 +2,24 @@ package org.broadinstitute.consent.http.models;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
+import org.broadinstitute.consent.http.enumeration.PropertyType;
+import org.broadinstitute.consent.http.models.dataset_registration_v1.AlternativeDataSharingPlanReason;
 import org.broadinstitute.consent.http.models.dataset_registration_v1.DatasetRegistrationSchemaV1;
+import org.broadinstitute.consent.http.models.dataset_registration_v1.DatasetRegistrationSchemaV1.AlternativeDataSharingPlanControlledOpenAccess;
+import org.broadinstitute.consent.http.models.dataset_registration_v1.DatasetRegistrationSchemaV1.AlternativeDataSharingPlanDataSubmitted;
+import org.broadinstitute.consent.http.models.dataset_registration_v1.DatasetRegistrationSchemaV1.NihAnvilUse;
+import org.broadinstitute.consent.http.models.dataset_registration_v1.DatasetRegistrationSchemaV1.NihInstitutionCenterSubmission;
+import org.broadinstitute.consent.http.models.dataset_registration_v1.DatasetRegistrationSchemaV1.StudyType;
 import org.broadinstitute.consent.http.models.dataset_registration_v1.DatasetRegistrationSchemaV1Builder;
+import org.broadinstitute.consent.http.models.dataset_registration_v1.NihICsSupportingStudy;
+import org.broadinstitute.consent.http.util.gson.GsonUtil;
 import org.junit.jupiter.api.Test;
 
 class DatasetRegistrationSchemaV1BuilderTest {
@@ -17,4 +32,119 @@ class DatasetRegistrationSchemaV1BuilderTest {
     assertNotNull(schemaV1);
   }
 
+  @Test
+  void testBuildSchemaWithStudyProps() {
+    DatasetRegistrationSchemaV1Builder builder = new DatasetRegistrationSchemaV1Builder();
+    Study study = createMockStudy();
+    addAllStudyProperties(study);
+
+    DatasetRegistrationSchemaV1 schemaV1 = builder.build(study, List.of());
+    assertNotNull(schemaV1);
+    assertNotNull(schemaV1.getStudyId());
+    assertNotNull(schemaV1.getStudyName());
+    assertNotNull(schemaV1.getStudyType());
+    assertNotNull(schemaV1.getStudyDescription());
+    assertNotNull(schemaV1.getDataTypes());
+    assertNotNull(schemaV1.getPhenotypeIndication());
+    assertNotNull(schemaV1.getSpecies());
+    assertNotNull(schemaV1.getPiName());
+    assertNotNull(schemaV1.getDataSubmitterUserId());
+    assertNotNull(schemaV1.getDataCustodianEmail());
+    assertNotNull(schemaV1.getPublicVisibility());
+    assertNotNull(schemaV1.getNihAnvilUse());
+    assertNotNull(schemaV1.getSubmittingToAnvil());
+    assertNotNull(schemaV1.getDbGaPPhsID());
+    assertNotNull(schemaV1.getDbGaPStudyRegistrationName());
+    assertNotNull(schemaV1.getEmbargoReleaseDate());
+    assertNotNull(schemaV1.getSequencingCenter());
+    assertNotNull(schemaV1.getPiInstitution());
+    assertNotNull(schemaV1.getNihGrantContractNumber());
+    assertNotNull(schemaV1.getNihICsSupportingStudy());
+    assertNotNull(schemaV1.getNihProgramOfficerName());
+    assertNotNull(schemaV1.getNihInstitutionCenterSubmission());
+    assertNotNull(schemaV1.getNihGenomicProgramAdministratorName());
+    assertNotNull(schemaV1.getMultiCenterStudy());
+    assertNotNull(schemaV1.getCollaboratingSites());
+    assertNotNull(schemaV1.getControlledAccessRequiredForGenomicSummaryResultsGSR());
+    assertNotNull(schemaV1.getControlledAccessRequiredForGenomicSummaryResultsGSRRequiredExplanation());
+    assertNotNull(schemaV1.getAlternativeDataSharingPlanReasons());
+    assertNotNull(schemaV1.getAlternativeDataSharingPlanExplanation());
+    assertNotNull(schemaV1.getAlternativeDataSharingPlanFileName());
+    assertNotNull(schemaV1.getAlternativeDataSharingPlanDataSubmitted());
+    assertNotNull(schemaV1.getAlternativeDataSharingPlanDataReleased());
+    assertNotNull(schemaV1.getAlternativeDataSharingPlanTargetDeliveryDate());
+    assertNotNull(schemaV1.getAlternativeDataSharingPlanTargetPublicReleaseDate());
+    assertNotNull(schemaV1.getAlternativeDataSharingPlanControlledOpenAccess());
+
+    // TODO: Test code ... remove when complete.
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    System.out.println(gson.toJson(schemaV1));
+  }
+
+  private Study createMockStudy() {
+    Date now = new Date();
+    Study study = new Study();
+    study.setStudyId(randomInt());
+    study.setName(randomString());
+    study.setDescription(randomString());
+    study.setPublicVisibility(Boolean.TRUE);
+    study.setPiName(randomString());
+    study.setDataTypes(List.of(randomString(), randomString(), randomString()));
+    study.setAlternativeDataSharingPlan(new FileStorageObject());
+    study.setCreateDate(now);
+    study.setCreateUserEmail(randomString());
+    study.setCreateUserId(randomInt());
+    study.setUpdateDate(now);
+    study.setUpdateUserId(randomInt());
+    study.setUuid(UUID.randomUUID());
+    return study;
+  }
+
+  private void addAllStudyProperties(Study study) {
+    study.addProperty(createStudyProperty(DatasetRegistrationSchemaV1Builder.studyType, study.getStudyId(), StudyType.OBSERVATIONAL.value(), PropertyType.String));
+    study.addProperty(createStudyProperty(DatasetRegistrationSchemaV1Builder.phenotypeIndication, study.getStudyId(), randomString(), PropertyType.String));
+    study.addProperty(createStudyProperty(DatasetRegistrationSchemaV1Builder.species, study.getStudyId(), randomString(), PropertyType.String));
+    study.addProperty(createStudyProperty(DatasetRegistrationSchemaV1Builder.dataCustodianEmail, study.getStudyId(), GsonUtil.getInstance().toJson(List.of(randomString())), PropertyType.Json));
+    study.addProperty(createStudyProperty(DatasetRegistrationSchemaV1Builder.nihAnvilUse, study.getStudyId(), NihAnvilUse.I_AM_NHGRI_FUNDED_AND_I_HAVE_A_DB_GA_P_PHS_ID_ALREADY.value(), PropertyType.String));
+    study.addProperty(createStudyProperty(DatasetRegistrationSchemaV1Builder.submittingToAnvil, study.getStudyId(), Boolean.TRUE, PropertyType.Boolean));
+    study.addProperty(createStudyProperty(DatasetRegistrationSchemaV1Builder.dbGaPPhsID, study.getStudyId(), randomString(), PropertyType.String));
+    study.addProperty(createStudyProperty(DatasetRegistrationSchemaV1Builder.dbGaPStudyRegistrationName, study.getStudyId(), randomString(), PropertyType.String));
+    study.addProperty(createStudyProperty(DatasetRegistrationSchemaV1Builder.embargoReleaseDate, study.getStudyId(), randomString(), PropertyType.String));
+    study.addProperty(createStudyProperty(DatasetRegistrationSchemaV1Builder.sequencingCenter, study.getStudyId(), randomString(), PropertyType.String));
+    study.addProperty(createStudyProperty(DatasetRegistrationSchemaV1Builder.piInstitution, study.getStudyId(), randomInt(), PropertyType.Number));
+    study.addProperty(createStudyProperty(DatasetRegistrationSchemaV1Builder.nihGrantContractNumber, study.getStudyId(), randomString(), PropertyType.String));
+    study.addProperty(createStudyProperty(DatasetRegistrationSchemaV1Builder.nihICsSupportingStudy, study.getStudyId(), GsonUtil.getInstance().toJson(List.of(NihICsSupportingStudy.CC.value())), PropertyType.Json));
+    study.addProperty(createStudyProperty(DatasetRegistrationSchemaV1Builder.nihProgramOfficerName, study.getStudyId(), randomString(), PropertyType.String));
+    study.addProperty(createStudyProperty(DatasetRegistrationSchemaV1Builder.nihInstitutionCenterSubmission, study.getStudyId(), NihInstitutionCenterSubmission.CC.value(), PropertyType.String));
+    study.addProperty(createStudyProperty(DatasetRegistrationSchemaV1Builder.nihGenomicProgramAdministratorName, study.getStudyId(), randomString(), PropertyType.String));
+    study.addProperty(createStudyProperty(DatasetRegistrationSchemaV1Builder.multiCenterStudy, study.getStudyId(), Boolean.TRUE, PropertyType.Boolean));
+    study.addProperty(createStudyProperty(DatasetRegistrationSchemaV1Builder.collaboratingSites, study.getStudyId(), GsonUtil.getInstance().toJson(List.of(randomString())), PropertyType.Json));
+    study.addProperty(createStudyProperty(DatasetRegistrationSchemaV1Builder.controlledAccessRequiredForGenomicSummaryResultsGSR, study.getStudyId(), Boolean.TRUE, PropertyType.Boolean));
+    study.addProperty(createStudyProperty(DatasetRegistrationSchemaV1Builder.controlledAccessRequiredForGenomicSummaryResultsGSRRequiredExplanation, study.getStudyId(), randomString(), PropertyType.String));
+    study.addProperty(createStudyProperty(DatasetRegistrationSchemaV1Builder.alternativeDataSharingPlanReasons, study.getStudyId(), GsonUtil.getInstance().toJson(List.of(AlternativeDataSharingPlanReason.OTHER.value())), PropertyType.Json));
+    study.addProperty(createStudyProperty(DatasetRegistrationSchemaV1Builder.alternativeDataSharingPlanExplanation, study.getStudyId(), randomString(), PropertyType.String));
+    study.addProperty(createStudyProperty(DatasetRegistrationSchemaV1Builder.alternativeDataSharingPlanFileName, study.getStudyId(), randomString(), PropertyType.String));
+    study.addProperty(createStudyProperty(DatasetRegistrationSchemaV1Builder.alternativeDataSharingPlanDataSubmitted, study.getStudyId(), AlternativeDataSharingPlanDataSubmitted.BY_BATCHES_OVER_STUDY_TIMELINE_E_G_BASED_ON_CLINICAL_TRIAL_ENROLLMENT_BENCHMARKS.value(), PropertyType.String));
+    study.addProperty(createStudyProperty(DatasetRegistrationSchemaV1Builder.alternativeDataSharingPlanDataReleased, study.getStudyId(), Boolean.TRUE, PropertyType.Boolean));
+    study.addProperty(createStudyProperty(DatasetRegistrationSchemaV1Builder.alternativeDataSharingPlanTargetDeliveryDate, study.getStudyId(), randomString(), PropertyType.String));
+    study.addProperty(createStudyProperty(DatasetRegistrationSchemaV1Builder.alternativeDataSharingPlanTargetPublicReleaseDate, study.getStudyId(), randomString(), PropertyType.String));
+    study.addProperty(createStudyProperty(DatasetRegistrationSchemaV1Builder.alternativeDataSharingPlanControlledOpenAccess, study.getStudyId(), AlternativeDataSharingPlanControlledOpenAccess.OPEN_ACCESS.value(), PropertyType.String));
+  }
+
+  private String randomString() {
+    return RandomStringUtils.randomAlphabetic(5);
+  }
+
+  private Integer randomInt() {
+    return RandomUtils.nextInt(1, 100);
+  }
+
+  private StudyProperty createStudyProperty(String key, Integer studyId, Object value, PropertyType type) {
+    StudyProperty property = new StudyProperty();
+    property.setKey(key);
+    property.setStudyId(studyId);
+    property.setValue(value);
+    property.setType(type);
+    return property;
+  }
 }
