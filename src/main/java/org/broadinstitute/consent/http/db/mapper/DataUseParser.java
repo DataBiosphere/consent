75457722b81d23283c435b38b5559a078638ec06
Greg Rushton <grushton@broadinstitute.org>
2024-01-05 12:41:52 -0500

    use concurrent map

--- src/main/java/org/broadinstitute/consent/http/db/mapper/DataUseParser.java
+++ src/main/java/org/broadinstitute/consent/http/db/mapper/DataUseParser.java
@@ -1,8 +1,8 @@
 package org.broadinstitute.consent.http.db.mapper;
 
 import com.google.gson.Gson;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import org.broadinstitute.consent.http.models.DataUse;
 import org.broadinstitute.consent.http.util.ConsentLogger;
 import org.broadinstitute.consent.http.util.gson.GsonUtil;
@@ -10,7 +10,7 @@ import org.broadinstitute.consent.http.util.gson.GsonUtil;
 public class DataUseParser implements ConsentLogger {
 
   private final Gson gson = GsonUtil.gsonBuilderWithAdapters().create();
-  private final Map<String, DataUse> dataUseCache = new HashMap<>();
+  private final ConcurrentMap<String, DataUse> dataUseCache = new ConcurrentHashMap<>();
 
   public DataUse parseDataUse(String dataUseString) {
     return dataUseCache.computeIfAbsent(dataUseString, s -> {
