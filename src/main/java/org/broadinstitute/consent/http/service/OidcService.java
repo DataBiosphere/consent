ac510e20c5383f14f696a2775818a973a01ed372
Douglas Voet <dvoet@users.noreply.github.com>
2024-01-10 13:11:17 -0500

    nosemgrep

--- src/main/java/org/broadinstitute/consent/http/service/OidcService.java
+++ src/main/java/org/broadinstitute/consent/http/service/OidcService.java
@@ -15,6 +15,8 @@ import org.broadinstitute.consent.http.models.OAuth2Configuration;
 
 public class OidcService {
   private static final String SCOPE_PARAM = "scope";
+
+  // nosemgrep
   private static final String CLIENT_SECRET_PARAM = "client_secret";
 
   private final OidcAuthorityDAO oidcAuthorityDAO;
