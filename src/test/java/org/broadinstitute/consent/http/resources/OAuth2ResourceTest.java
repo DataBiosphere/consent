61c66a6f9e57a2287da0dc874218bcc000a42383
Douglas Voet <dvoet@users.noreply.github.com>
2024-01-11 14:41:33 -0500

    resource tests

---
+++ src/test/java/org/broadinstitute/consent/http/resources/OAuth2ResourceTest.java
@@ -0,0 +1,59 @@
+package org.broadinstitute.consent.http.resources;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import jakarta.ws.rs.core.MultivaluedHashMap;
+import jakarta.ws.rs.core.UriInfo;
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.broadinstitute.consent.http.models.OAuth2Configuration;
+import org.broadinstitute.consent.http.service.OidcService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class OAuth2ResourceTest {
+  @Mock private OidcService mockOidcService;
+  @Mock private UriInfo mockUriInfo;
+
+  @BeforeEach
+  public void setUp() throws Exception {
+    MockitoAnnotations.openMocks(this);
+  }
+
+  @Test
+  public void testGetOAuth2Redirect() throws URISyntaxException {
+    var queryParameters = new MultivaluedHashMap<String, String>();
+    when(mockUriInfo.getQueryParameters()).thenReturn(queryParameters);
+    when(mockOidcService.getAuthorizationURI(queryParameters)).thenReturn(new URI("http://example.com"));
+    OAuth2Resource resource = new OAuth2Resource(mockOidcService);
+    var response = resource.getAuthorizationEndpoint(mockUriInfo);
+    assertEquals(302, response.getStatus());
+    assertEquals("http://example.com", response.getLocation().toString());
+  }
+
+  @Test
+  public void testGetToken() {
+    var queryParameters = new MultivaluedHashMap<String, String>();
+    var bodyParams = new MultivaluedHashMap<String, String>();
+    when(mockUriInfo.getQueryParameters()).thenReturn(queryParameters);
+    when(mockOidcService.tokenExchange(bodyParams, queryParameters)).thenReturn("token");
+    OAuth2Resource resource = new OAuth2Resource(mockOidcService);
+    var response = resource.getToken(mockUriInfo, bodyParams);
+    assertEquals(200, response.getStatus());
+    assertEquals("token", response.getEntity());
+    assertEquals("application/json", response.getMediaType().toString());
+  }
+
+  @Test
+  public void testGetOAuth2Configuration() {
+    var expectedConfig = new OAuth2Configuration("http://example.com", "test-client-id");
+    when(mockOidcService.getOAuth2Configuration()).thenReturn(expectedConfig);
+    OAuth2Resource resource = new OAuth2Resource(mockOidcService);
+    var response = resource.getOAuth2Configuration();
+    assertEquals(200, response.getStatus());
+    assertEquals(expectedConfig, response.getEntity());
+  }
+}
