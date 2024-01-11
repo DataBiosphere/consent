08eb5b67f102dba6d7215331585d9b1c6ebaf245
rushtong <grushton@broadinstitute.org>
2024-01-11 13:58:56 -0500

    handle null dataset updates

--- src/main/java/org/broadinstitute/consent/http/service/DatasetRegistrationService.java
+++ src/main/java/org/broadinstitute/consent/http/service/DatasetRegistrationService.java
@@ -736,8 +736,9 @@ public class DatasetRegistrationService implements ConsentLogger {
    */
   public List<Dataset> createdDatasetsFromUpdatedStudy(Study updatedStudy,
       List<DatasetServiceDAO.DatasetUpdate> datasetUpdates) {
-    List<Integer> datasetUpdateIds = datasetUpdates.stream()
-        .map(DatasetServiceDAO.DatasetUpdate::datasetId).toList();
+    List<Integer> datasetUpdateIds = (datasetUpdates == null) ?
+        List.of() :
+        datasetUpdates.stream().map(DatasetServiceDAO.DatasetUpdate::datasetId).toList();
     if (updatedStudy.getDatasets() == null) {
       return List.of();
     }
