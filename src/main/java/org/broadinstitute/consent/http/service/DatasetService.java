6e6b62cca997c85c59ef7b0ae1496c2c2a6e388d
rushtong <grushton@broadinstitute.org>
2024-04-03 09:16:50 -0400

    feat: remove population and usage of the old dbGap dataset property

--- src/main/java/org/broadinstitute/consent/http/service/DatasetService.java
+++ src/main/java/org/broadinstitute/consent/http/service/DatasetService.java
@@ -506,9 +506,6 @@ public class DatasetService implements ConsentLogger {
       // Handle "URL"
       newPropConversion(dictionaries, dataset, "URL", "url", PropertyType.String,
           studyConversion.getUrl());
-      // Handle "dbGAP"
-      legacyPropConversion(dictionaries, dataset, "dbGAP", "url", PropertyType.String,
-          studyConversion.getUrl());
     }
 
     // Handle "Data Submitter User ID"
