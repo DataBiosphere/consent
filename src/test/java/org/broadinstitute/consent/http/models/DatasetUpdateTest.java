cd57502c2f0cd11190f35e046f6db2a1c8df745e
rushtong <grushton@broadinstitute.org>
2024-07-15 09:30:13 -0400

    test: clarify and flesh out tests

--- src/test/java/org/broadinstitute/consent/http/models/DatasetUpdateTest.java
+++ src/test/java/org/broadinstitute/consent/http/models/DatasetUpdateTest.java
@@ -2,6 +2,7 @@ package org.broadinstitute.consent.http.models;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -15,42 +16,40 @@ public class DatasetUpdateTest {
 
   @Test
   void testDatasetUpdate() {
-    // Ensure each property type is correctly handled. See dataset-registration-schema_v1.json
-    // for full list of properties datasets can have.
-    String location = "TDR Location";
-    Long participants = 12L;
-    Boolean nmds = true;
     String json = """
         {
           "name": "Test Dataset Update",
           "dacId": 1,
           "properties": [
             {
-              "propertyName": "Data Location",
-              "propertyValue": "%s"
+              "propertyName": "string value",
+              "propertyValue": "TDR Location"
             },
             {
-              "propertyName": "# of participants",
-              "propertyValue": %d
+              "propertyName": "long value",
+              "propertyValue": 12
             },
             {
-              "propertyName": "nmds",
-              "propertyValue": %s
+              "propertyName": "boolean value",
+              "propertyValue": true
+            },
+            {
+              "propertyName": "double value",
+              "propertyValue": 10.001
             },
             {
               "propertyName": "File Types",
               "propertyValue": [{"fileType":"ARRAYS","functionalEquivalence":"testing"}]
             }
           ]
-        }""".formatted(location, participants, nmds);
+        }""";
     DatasetUpdate update = new DatasetUpdate(json);
+
     List<DatasetProperty> props = update.getDatasetProperties();
-
-    assertEquals(location, getPropByName("Data Location", props).getPropertyValue());
-
-    assertEquals(participants, getPropByName("# of participants", props).getPropertyValue());
-
-    assertEquals(nmds, getPropByName("nmds", props).getPropertyValue());
+    assertEquals("TDR Location", getPropByName("string value", props).getPropertyValue());
+    assertEquals(12L, getPropByName("long value", props).getPropertyValue());
+    assertEquals(true, getPropByName("boolean value", props).getPropertyValue());
+    assertEquals(10.001D, getPropByName("double value", props).getPropertyValue());
 
     // Parsing the object value of this prop is a little complicated due to JSON serialization
     DatasetProperty fileTypeProp = getPropByName("File Types", props);
@@ -60,11 +59,29 @@ public class DatasetUpdateTest {
     assertFalse(fileTypes.isEmpty());
     FileTypeObject type = fileTypes.get(0);
     assertEquals(FileType.ARRAYS, type.getFileType());
-
   }
 
   private DatasetProperty getPropByName(String name, List<DatasetProperty> props ) {
     return props.stream().filter(p -> p.getPropertyName().equals(name)).findFirst().orElse(null);
+  }
+
+  @Test
+  void testDatasetUpdateNullValues() {
+    String json = """
+        {
+          "not_a_name": "Test Dataset Update",
+          "not_a_dac_id": 1,
+          "no_properties": [
+            {
+              "propertyName": "string value",
+              "propertyValue": "TDR Location"
+            }
+          ]
+        }""";
+    DatasetUpdate update = new DatasetUpdate(json);
+    assertNull(update.getName());
+    assertNull(update.getDacId());
+    assertNull(update.getDatasetProperties());
   }
 
 }
