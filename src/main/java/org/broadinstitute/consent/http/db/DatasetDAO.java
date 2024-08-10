26a4333fbf4cad6905b45bbfb1d7b194b303b5db
rushtong <grushton@broadinstitute.org>
2024-08-10 09:54:07 -0400

    feat: rm unnecessary sort direction

--- src/main/java/org/broadinstitute/consent/http/db/DatasetDAO.java
+++ src/main/java/org/broadinstitute/consent/http/db/DatasetDAO.java
@@ -187,7 +187,7 @@ public interface DatasetDAO extends Transactional<DatasetDAO> {
           LEFT JOIN dataset s_dataset ON s_dataset.study_id = s.study_id
           LEFT JOIN file_storage_object fso ON (fso.entity_id = d.dataset_id::text OR fso.entity_id = s.uuid::text) AND fso.deleted = false
           WHERE d.dataset_id in (<datasetIds>)
-          ORDER BY d.dataset_id ASC
+          ORDER BY d.dataset_id
       """)
   List<Dataset> findDatasetsByIdList(@BindList("datasetIds") List<Integer> datasetIds);
 
