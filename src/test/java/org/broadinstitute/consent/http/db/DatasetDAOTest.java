614a7005c5ec07e52763f4aa366d8ce03e9c9e68
rushtong <grushton@broadinstitute.org>
2024-11-06 15:22:34 -0500

    feat: revert unnecessary import

--- src/test/java/org/broadinstitute/consent/http/db/DatasetDAOTest.java
+++ src/test/java/org/broadinstitute/consent/http/db/DatasetDAOTest.java
@@ -8,7 +8,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.google.gson.JsonObject;
