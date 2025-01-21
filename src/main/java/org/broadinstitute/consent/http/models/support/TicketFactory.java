685652b1531c0ce0fa617d25c28adbb0d85aa7d8
rushtong <grushton@broadinstitute.org>
2025-01-21 09:25:15 -0500

    feat: PR feedback

--- src/main/java/org/broadinstitute/consent/http/models/support/TicketFactory.java
+++ src/main/java/org/broadinstitute/consent/http/models/support/TicketFactory.java
@@ -2,11 +2,7 @@ package org.broadinstitute.consent.http.models.support;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
-import java.util.List;
-import org.broadinstitute.consent.http.enumeration.SupportRequestType;
 import org.broadinstitute.consent.http.util.gson.GsonUtil;
-import org.zendesk.client.v2.model.Comment;
-import org.zendesk.client.v2.model.CustomFieldValue;
 import org.zendesk.client.v2.model.Request;
 import org.zendesk.client.v2.model.Ticket;
 
@@ -28,46 +24,12 @@ public class TicketFactory {
   /**
    * Constructs a DuosTicket with the proper structure to request support via Zendesk
    *
-   * @param ticketFields  TicketFields
+   * @param ticketFields TicketFields
    */
   public static DuosTicket createTicket(TicketFields ticketFields) {
     ticketFields.validate();
-    Ticket ticket = new Ticket(
-        new Ticket.Requester(ticketFields.name(), ticketFields.email()),
-        ticketFields.subject(),
-        createComment(ticketFields.description(), ticketFields.url(), ticketFields.uploads()));
-    ticket.setCustomFields(createCustomFields(ticketFields.name(), ticketFields.type(), ticketFields.email(), ticketFields.description()));
-    // This value specifies tickets as belonging to the DUOS group defined in Zendesk
-    ticket.setTicketFormId(360000669472L);
+    Ticket ticket = ticketFields.toTicket();
     return new DuosTicket(ticket);
-  }
-
-  static private Comment createComment(String description, String url, List<String> uploads) {
-    Comment comment = new Comment();
-    comment.setBody(description + "\n\n------------------\nSubmitted from: " + url);
-    if (uploads != null && !uploads.isEmpty()) {
-      comment.setUploads(uploads);
-    }
-    return comment;
-  }
-
-  /**
-   * Custom fields represent a Zendesk ID that corresponds to a component of the created ticket.
-   * These fields have IDs that are defined in the Zendesk administrative interface.
-   *
-   * @param name Name of user
-   * @param type SupportRequestType Type of request, i.e. Question, Incident, etc.
-   * @param email User's email address
-   * @param description The contents of the support request
-   * @return List of Custom Fields that are required by Zendesk
-   */
-  static private List<CustomFieldValue> createCustomFields(String name, SupportRequestType type,
-      String email, String description) {
-    return List.of(new CustomFieldValue(360012744452L, new String[]{type.name()}),
-        new CustomFieldValue(360007369412L, new String[]{description}),
-        new CustomFieldValue(360012744292L, new String[]{name}),
-        new CustomFieldValue(360012782111L, new String[]{email}),
-        new CustomFieldValue(360018545031L, new String[]{email}));
   }
 
 }
--- src/main/java/org/broadinstitute/consent/http/models/support/TicketFields.java
+++ src/main/java/org/broadinstitute/consent/http/models/support/TicketFields.java
@@ -2,26 +2,55 @@ package org.broadinstitute.consent.http.models.support;
 
 import java.util.List;
 import org.broadinstitute.consent.http.enumeration.SupportRequestType;
+import org.zendesk.client.v2.model.Comment;
+import org.zendesk.client.v2.model.CustomFieldValue;
+import org.zendesk.client.v2.model.Ticket;
 
 public record TicketFields(String name, SupportRequestType type, String email, String subject,
                            String description, String url, List<String> uploads) {
 
+  private static void validate(boolean condition, String prefix) {
+    if (condition) {
+      throw new IllegalArgumentException(prefix + " is required");
+    }
+  }
+
   public void validate() {
-    if (name() == null || email() == null) {
-      throw new IllegalArgumentException("Name and email of user requesting support is required");
+    validate(name == null || email == null, "Name and email of user requesting support");
+    validate(subject == null, "Subject");
+    validate(description == null, "Description");
+    validate(type == null, "Type");
+    validate(url == null, "URL");
+  }
+
+  Ticket toTicket() {
+    Ticket ticket = new Ticket(new Ticket.Requester(name, email), subject, createComment());
+    ticket.setCustomFields(createCustomFields());
+    // This value specifies tickets as belonging to the DUOS group defined in Zendesk
+    ticket.setTicketFormId(360000669472L);
+    return ticket;
+  }
+
+  private Comment createComment() {
+    Comment comment = new Comment();
+    comment.setBody(description + "\n\n------------------\nSubmitted from: " + url);
+    if (uploads != null && !uploads.isEmpty()) {
+      comment.setUploads(uploads);
     }
-    if (subject() == null) {
-      throw new IllegalArgumentException("Subject is required");
-    }
-    if (description() == null) {
-      throw new IllegalArgumentException("Description is required");
-    }
-    if (type() == null) {
-      throw new IllegalArgumentException("Type is required");
-    }
-    if (url() == null) {
-      throw new IllegalArgumentException("URL is required");
-    }
+    return comment;
+  }
+
+  /**
+   * Custom fields consist of predefined long values corresponding to field types in the Zendesk UI
+   *
+   * @return List<CustomFieldValue> List of custom fields
+   */
+  private List<CustomFieldValue> createCustomFields() {
+    return List.of(new CustomFieldValue(360012744452L, new String[]{type.name()}),
+        new CustomFieldValue(360007369412L, new String[]{description}),
+        new CustomFieldValue(360012744292L, new String[]{name}),
+        new CustomFieldValue(360012782111L, new String[]{email}),
+        new CustomFieldValue(360018545031L, new String[]{email}));
   }
 
 }
