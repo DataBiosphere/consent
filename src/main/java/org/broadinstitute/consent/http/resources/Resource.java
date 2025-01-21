79e6304b4459f35796091523f836ba08bed1fa50
rushtong <grushton@broadinstitute.org>
2025-01-21 09:49:48 -0500

    feat: handle semgrep warning

--- src/main/java/org/broadinstitute/consent/http/resources/Resource.java
+++ src/main/java/org/broadinstitute/consent/http/resources/Resource.java
@@ -51,6 +51,7 @@ abstract public class Resource implements ConsentLogger {
   public static final String CHAIRPERSON = "Chairperson";
   public static final String MEMBER = "Member";
   public static final String RESEARCHER = "Researcher";
+  // nosemgrep
   public static final String SIGNINGOFFICIAL = "SigningOfficial";
   public static final String DATASUBMITTER = "DataSubmitter";
   public static final String ITDIRECTOR = "ITDirector";
