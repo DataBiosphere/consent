e47d24949ed4469ca4ec0df6c21b0d0e5f05993e
Elliot Otchet <otchet@broadinstitute.org>
2025-02-03 14:32:22 -0500

    Add required import to code suggestion.

--- src/test/java/org/broadinstitute/consent/http/resources/DraftResourceTest.java
+++ src/test/java/org/broadinstitute/consent/http/resources/DraftResourceTest.java
@@ -23,6 +23,7 @@ import java.sql.SQLException;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 import org.broadinstitute.consent.http.enumeration.DraftType;
