b1857e58012015f0db272bb9edb56a5b20c19139
Elliot Otchet <otchet@broadinstitute.org>
2025-04-16 11:56:29 -0400

    Update approved error message text.

--- src/main/java/org/broadinstitute/consent/http/exceptions/LibraryCardRequiredException.java
+++ src/main/java/org/broadinstitute/consent/http/exceptions/LibraryCardRequiredException.java
@@ -1,7 +1,7 @@
 package org.broadinstitute.consent.http.exceptions;
 
 public class LibraryCardRequiredException extends UnprocessableEntityException{
-  public static final String MESSAGE = "Please register in DUOS with your institution's email and obtain approval from your Signing Official in order to submit a data access request.";
+  public static final String MESSAGE = "Please register in DUOS with your institution's email and obtain a library card from your Signing Official in order to submit a data access request.";
   public LibraryCardRequiredException() {
     super(MESSAGE);
   }
