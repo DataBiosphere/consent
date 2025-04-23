25e145b7a62c92044db4a829abc581de5014cecb
rushtong <grushton@broadinstitute.org>
2025-04-23 13:45:41 -0400

    feat: log source and destination IP

--- src/main/java/org/broadinstitute/consent/http/util/ComplianceLogger.java
+++ src/main/java/org/broadinstitute/consent/http/util/ComplianceLogger.java
@@ -25,35 +25,35 @@ public class ComplianceLogger implements ConsentLogger {
   }
 
   private static final String MESSAGE = """
-      _time: %s;
-      src_ip: -;
-      dest_ip: -;
-      dest_port: 443;
-      user_name: %s;
-      user_id: %s;
-      user_id_provider: %s;
-      session_id: -;
-      url: %s;
-      app: DUOS;
-      http_user_agent: %s;
-      status: %s;
-      http_content_type: %s;
-      bytes: -;
-      duration: -;
-      nih_ico: -;
-      cadr_name: DUOS;
-      user_country_name: -;
-      user_org: %s;
-      user_email: %s;
-      associated_study: %s;
-      eRA_commons_id: %s;
-      user_permission_group: -;
-      event_type: %s;
+      _time: %s; \
+      src_ip: %s; \
+      dest_ip: %s; \
+      dest_port: 443; \
+      user_name: %s; \
+      user_id: %s; \
+      user_id_provider: %s; \
+      session_id: -; \
+      url: %s; \
+      app: DUOS; \
+      http_user_agent: %s; \
+      status: %s; \
+      http_content_type: %s; \
+      nih_ico: -; \
+      cadr_name: DUOS; \
+      user_org: %s; \
+      user_email: %s; \
+      associated_study: %s; \
+      eRA_commons_id: %s; \
+      event_type: %s; \
       """;
 
   private void logEvent(User user, List<Dataset> datasets, ContainerRequest request,
       int responseStatusCode, ComplianceEvent event) {
     Instant now = Instant.now();
+    String sourceIp = request.getHeaderString("X-Forwarded-For") == null ? "-"
+        : request.getHeaderString("X-Forwarded-For");
+    String destinationIp = request.getHeaderString("X-Forwarded-Server") == null ? "-"
+        : request.getHeaderString("X-Forwarded-Server");
     String userId = request.getHeaderString("oidc_claim_user_id") == null ? "-"
         : request.getHeaderString("oidc_claim_user_id");
     String userAgent = request.getHeaderString(HttpHeaderNames.USER_AGENT.toString()) == null ? "-"
@@ -67,6 +67,8 @@ public class ComplianceLogger implements ConsentLogger {
       String logMessage = MESSAGE
           .formatted(
               now,
+              sourceIp,
+              destinationIp,
               user.getDisplayName(),
               userId,
               userIdProvider,
@@ -78,8 +80,7 @@ public class ComplianceLogger implements ConsentLogger {
               user.getEmail(),
               dataset.getDatasetIdentifier(),
               user.getEraCommonsId(),
-              event)
-          .replace("\\R", " ");
+              event);
       logInfo(logMessage);
     });
   }
