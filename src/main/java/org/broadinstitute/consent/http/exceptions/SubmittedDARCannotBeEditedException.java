0176ab628bca9bf7b76ccab151919b31745a3b4c
Elliot Otchet <otchet@broadinstitute.org>
2025-04-23 14:52:07 -0400

    Fix typo in message.

--- src/main/java/org/broadinstitute/consent/http/exceptions/SubmittedDARCannotBeEditedException.java
+++ src/main/java/org/broadinstitute/consent/http/exceptions/SubmittedDARCannotBeEditedException.java
@@ -2,7 +2,7 @@ package org.broadinstitute.consent.http.exceptions;
 
 public class SubmittedDARCannotBeEditedException extends UnprocessableEntityException {
 
-  public static final String MESSAGE = "This data access request is already submitted for DAC consideration amd cannot be edited.";
+  public static final String MESSAGE = "This data access request is already submitted for DAC consideration and cannot be edited.";
 
   public SubmittedDARCannotBeEditedException() {
     super(MESSAGE);
