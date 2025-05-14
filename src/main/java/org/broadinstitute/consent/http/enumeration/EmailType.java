666d6da8b29df705d7795dc3cec918341da7d4ea
Elliot Otchet <otchet@broadinstitute.org>
2025-05-14 09:43:07 -0400

    Sem not comma.

--- src/main/java/org/broadinstitute/consent/http/enumeration/EmailType.java
+++ src/main/java/org/broadinstitute/consent/http/enumeration/EmailType.java
@@ -25,7 +25,7 @@ public enum EmailType {
   DAR_EXPIRATION_REMINDER(21, "dar-expiration-reminder.html"),
   NEW_PROGRESS_REPORT(22, "new-progress-report-request.html"),
   NEW_PROGRESS_REPORT_CASE(23, "new-progress-report-case.html"),
-  RESEARCHER_PROGRESS_REPORT_APPROVED(24, "researcher-progress-report-approved.html"),
+  RESEARCHER_PROGRESS_REPORT_APPROVED(24, "researcher-progress-report-approved.html");
 
   private final Integer typeInt;
   public final String templateName;
