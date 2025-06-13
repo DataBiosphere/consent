07ebe74ce5d8a7dc2ab7b5048bf13354ea0e4731
Elliot Otchet <otchet@broadinstitute.org>
2025-06-12 23:53:15 -0400

    Change the file loading method to work with the classloader.

--- src/main/java/org/broadinstitute/consent/http/util/CountryValidator.java
+++ src/main/java/org/broadinstitute/consent/http/util/CountryValidator.java
@@ -3,22 +3,26 @@ package org.broadinstitute.consent.http.util;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.JsonParseException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
 import java.util.HashSet;
 import java.util.Set;
 
 public class CountryValidator {
-  private static final String FILEPATH = "/src/main/resources/assets/ISO-3166-countries.json";
+  private static final String FILEPATH = "assets/ISO-3166-countries.json";
   private Set<String> countries;
   public CountryValidator() {
-    try (FileReader reader = new FileReader(System.getProperty ("user.dir") + FILEPATH)) {
-      Type type = new TypeToken<Set<String>>() {}.getType();
-      countries = new Gson().fromJson(reader, type);
+    try (InputStream is = getClass().getClassLoader().getResourceAsStream(FILEPATH)) {
+      if (is != null) {
+        Type type = new TypeToken<Set<String>>() {}.getType();
+        countries = new Gson().fromJson(new InputStreamReader(is, StandardCharsets.UTF_8), type);
+      }
     } catch (JsonParseException | IOException e) {
       countries = new HashSet<>();
-      throw new RuntimeException(e);
+      throw new IllegalStateException(e);
     }
   }
 
