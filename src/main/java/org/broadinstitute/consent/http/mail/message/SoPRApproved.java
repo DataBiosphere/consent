c2d0d57db83b2db85229f62fa7f4006db105ab4b
rushtong <grushton@broadinstitute.org>
2025-06-17 09:15:07 -0400

    feat: SO PR Approved message and tests

--- src/main/java/org/broadinstitute/consent/http/mail/message/SoPRApproved.java
+++ src/main/java/org/broadinstitute/consent/http/mail/message/SoPRApproved.java
@@ -1,5 +1,49 @@
 package org.broadinstitute.consent.http.mail.message;
 
-public class SoPRApproved {
+import java.util.List;
+import java.util.Map;
+import org.broadinstitute.consent.http.enumeration.EmailType;
+import org.broadinstitute.consent.http.models.Dataset;
+import org.broadinstitute.consent.http.models.User;
+
+public class SoPRApproved extends MailMessage {
+
+  private static final String SUBJECT = "Your Institutional Researcher's Progress Report %s is Approved";
+  private final String darCode;
+  private final User researcher;
+  private final String referenceId;
+  private final List<Dataset> datasets;
+  private final String dataUseRestriction;
+
+  public SoPRApproved(User toUser, String darCode, User researcher, String referenceId, List<Dataset> datasets, String dataUseRestriction) {
+    super(toUser, EmailType.SO_DAR_APPROVED);
+    this.darCode = darCode;
+    this.researcher = researcher;
+    this.referenceId = referenceId;
+    this.datasets = datasets;
+    this.dataUseRestriction = dataUseRestriction;
+  }
+
+  @Override
+  public String createSubject() {
+    return String.format(SUBJECT, darCode);
+  }
+
+  @Override
+  public Object createModel(String serverUrl) {
+    return Map.of(
+        "userName", toUser.getDisplayName(),
+        "darCode", darCode,
+        "researcherUserName", researcher.getDisplayName(),
+        "researcherEmail", researcher.getEmail(),
+        "datasets", datasets,
+        "dataUseRestriction", dataUseRestriction,
+        "serverUrl", serverUrl);
+  }
+
+  @Override
+  public String getEntityReferenceId() {
+    return referenceId;
+  }
 
 }
