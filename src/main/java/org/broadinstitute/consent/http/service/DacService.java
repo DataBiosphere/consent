b277e0ba8f7c66a39038f1f2257df49d5ab95fa0
Elliot Otchet <otchet@broadinstitute.org>
2025-06-19 10:28:53 -0400

    Use service method to populate needed information.

--- src/main/java/org/broadinstitute/consent/http/service/DacService.java
+++ src/main/java/org/broadinstitute/consent/http/service/DacService.java
@@ -11,6 +11,7 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.EnumSet;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -294,7 +295,9 @@ public class DacService implements ConsentLogger {
   }
 
   public Set<Dac> findByDatasetId(List<Integer> datasetIds) {
-    return dacDAO.findDacsForDatasetIds(datasetIds);
+    Set<Dac> returnSet = new HashSet<>();
+    dacDAO.findDacsForDatasetIds(datasetIds).forEach(dac -> returnSet.add(findById(dac.getDacId())));
+    return returnSet;
   }
 
 }
