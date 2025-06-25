601e4c462dc2e0388da71acb7229a6d24b158721
rushtong <grushton@broadinstitute.org>
2025-06-25 10:56:24 -0400

    feat: trim better

--- src/main/java/org/broadinstitute/consent/http/models/InstitutionDomainMap.java
+++ src/main/java/org/broadinstitute/consent/http/models/InstitutionDomainMap.java
@@ -25,7 +25,8 @@ public class InstitutionDomainMap {
    * @return the institution name if found, otherwise null
    */
   public String getInstitutionForEmail(String email) {
-    String domain = email.substring(email.indexOf('@') + 1).trim();
+    String trimmedEmail = email.trim();
+    String domain = trimmedEmail.substring(trimmedEmail.indexOf('@') + 1);
     return institutionDomainMap.entrySet().stream()
         .filter(entry -> entry.getValue().stream()
             .anyMatch(d -> d.equalsIgnoreCase(domain)))
