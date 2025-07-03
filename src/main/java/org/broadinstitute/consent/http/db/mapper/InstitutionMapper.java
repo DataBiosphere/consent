4924aaf4d249c0b2f721060dd78bf5fdb9003d6e
rushtong <grushton@broadinstitute.org>
2025-07-03 09:24:43 -0400

    feat: fix caching bug

--- src/main/java/org/broadinstitute/consent/http/db/mapper/InstitutionMapper.java
+++ src/main/java/org/broadinstitute/consent/http/db/mapper/InstitutionMapper.java
@@ -2,9 +2,6 @@ package org.broadinstitute.consent.http.db.mapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
-import org.apache.commons.lang3.StringUtils;
 import org.broadinstitute.consent.http.enumeration.OrganizationType;
 import org.broadinstitute.consent.http.models.Institution;
 import org.jdbi.v3.core.mapper.RowMapper;
@@ -12,21 +9,13 @@ import org.jdbi.v3.core.statement.StatementContext;
 
 public class InstitutionMapper implements RowMapper<Institution>, RowMapperHelper {
 
-  private final Map<Integer, Institution> institutionMap = new HashMap<>();
-
   @Override
   public Institution map(ResultSet resultSet, StatementContext statementContext)
       throws SQLException {
-
     Institution institution;
     int institutionId = resultSet.getInt("institution_id");
-
-    if (institutionMap.containsKey(institutionId)) {
-      institution = institutionMap.get(institutionId);
-    } else {
-      institution = new Institution();
-      institution.setId(institutionId);
-    }
+    institution = new Institution();
+    institution.setId(institutionId);
     setStringFieldValue(resultSet, "institution_name", institution::setName);
     setStringFieldValue(resultSet, "it_director_name", institution::setItDirectorName);
     setStringFieldValue(resultSet, "it_director_email", institution::setItDirectorEmail);
@@ -46,13 +35,6 @@ public class InstitutionMapper implements RowMapper<Institution>, RowMapperHelpe
           resultSet.getString("organization_type"));
       institution.setOrganizationType(type);
     }
-    if (hasColumn(resultSet, "domain")) {
-      String domain = resultSet.getString("domain");
-      if (!StringUtils.isBlank(domain)) {
-        institution.addDomain(domain);
-      }
-    }
-    institutionMap.put(institution.getId(), institution);
     return institution;
   }
 }
