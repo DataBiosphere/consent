169f56c35f4adf1bec76a153a3e2f3761b82e58c
rushtong <grushton@broadinstitute.org>
2025-07-31 11:20:46 -0400

    cleanup

--- src/test/java/org/broadinstitute/consent/http/db/DAOTestHelper.java
+++ src/test/java/org/broadinstitute/consent/http/db/DAOTestHelper.java
@@ -28,8 +28,6 @@ import org.jdbi.v3.gson2.Gson2Config;
 import org.jdbi.v3.gson2.Gson2Plugin;
 import org.jdbi.v3.guava.GuavaPlugin;
 import org.jdbi.v3.sqlobject.SqlObjectPlugin;
-import org.jdbi.v3.sqlobject.customizer.Bind;
-import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.platform.launcher.TestExecutionListener;
 import org.junit.platform.launcher.TestPlan;
@@ -260,17 +258,13 @@ public class DAOTestHelper extends AbstractTestHelper implements TestExecutionLi
     return institutionDAO.findInstitutionById(user.getInstitutionId());
   }
 
-  protected void updateVote(@Bind("vote") Boolean vote,
-      @Bind("rationale") String rationale,
-      @Bind("updateDate") Date updateDate,
-      @Bind("voteId") Integer voteId,
-      @Bind("reminderSent") boolean reminder,
-      @Bind("electionId") Integer electionId,
-      @Bind("createDate") Date createDate,
-      @Bind("hasConcerns") Boolean hasConcerns) {
+  protected void updateVote(Boolean vote, String rationale, Date updateDate, Integer voteId,
+      boolean reminder, Integer electionId, Date createDate, Boolean hasConcerns) {
     jdbi.useHandle(handle -> {
       String sql = """
-          update vote set vote = :vote, updateDate = :updateDate, rationale = :rationale, reminderSent = :reminderSent, createDate = :createDate, has_concerns = :hasConcerns where voteId = :voteId
+              UPDATE vote
+              SET vote = :vote, updateDate = :updateDate, rationale = :rationale, reminderSent = :reminderSent, createDate = :createDate, has_concerns = :hasConcerns
+              WHERE voteId = :voteId
           """;
       handle.createUpdate(sql)
           .bind("vote", vote)
