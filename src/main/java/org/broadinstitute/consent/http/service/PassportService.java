a1c8da0bead0cb27c64714537de10072016428f4
rushtong <grushton@broadinstitute.org>
2025-08-01 08:32:04 -0400

    use subject id for visa sub values

--- src/main/java/org/broadinstitute/consent/http/service/PassportService.java
+++ src/main/java/org/broadinstitute/consent/http/service/PassportService.java
@@ -9,14 +9,10 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 import org.broadinstitute.consent.http.db.DatasetDAO;
-import org.broadinstitute.consent.http.db.InstitutionDAO;
-import org.broadinstitute.consent.http.db.LibraryCardDAO;
 import org.broadinstitute.consent.http.db.SamDAO;
 import org.broadinstitute.consent.http.db.UserDAO;
 import org.broadinstitute.consent.http.models.ApprovedDataset;
 import org.broadinstitute.consent.http.models.AuthUser;
-import org.broadinstitute.consent.http.models.Institution;
-import org.broadinstitute.consent.http.models.LibraryCard;
 import org.broadinstitute.consent.http.models.User;
 import org.broadinstitute.consent.http.models.passport.AffiliationAndRole;
 import org.broadinstitute.consent.http.models.passport.ControlledAccessGrants;
@@ -24,23 +20,20 @@ import org.broadinstitute.consent.http.models.passport.PassportClaim;
 import org.broadinstitute.consent.http.models.passport.Visa;
 import org.broadinstitute.consent.http.models.passport.VisaClaim;
 import org.broadinstitute.consent.http.models.passport.VisaClaimType;
-import org.broadinstitute.consent.http.models.sam.UserStatusDiagnostics;
+import org.broadinstitute.consent.http.models.sam.UserStatusInfo;
+import org.broadinstitute.consent.http.util.ConsentLogger;
 
-public class PassportService {
+public class PassportService implements ConsentLogger {
 
   public static final String ISS = "https://duos.org";
 
   private final DatasetDAO datasetDAO;
-  private final InstitutionDAO institutionDAO;
-  private final LibraryCardDAO libraryCardDAO;
   private final UserDAO userDAO;
   private final SamDAO samDAO;
 
   @Inject
-  public PassportService(DatasetDAO datasetDAO, InstitutionDAO institutionDAO, LibraryCardDAO libraryCardDAO, UserDAO userDAO, SamDAO samDAO) {
+  public PassportService(DatasetDAO datasetDAO, UserDAO userDAO, SamDAO samDAO) {
     this.datasetDAO = datasetDAO;
-    this.institutionDAO = institutionDAO;
-    this.libraryCardDAO = libraryCardDAO;
     this.userDAO = userDAO;
     this.samDAO = samDAO;
   }
@@ -51,29 +44,24 @@ public class PassportService {
   // * LinkedIdentities
   public PassportClaim generatePassport(AuthUser authUser) throws Exception {
     User user = userDAO.findUserByEmail(authUser.getEmail());
-    // TODO: We need a fully fleshed out user with Library Cards, Institution, etc.
     if (user == null) {
       return new PassportClaim(List.of());
     }
-    LibraryCard libraryCards = libraryCardDAO.findLibraryCardByUserId(user.getUserId());
-    user.setLibraryCard(libraryCards);
-    if (user.getInstitutionId() != null) {
-      Institution i = institutionDAO.findInstitutionById(user.getInstitutionId());
-      user.setInstitution(i);
-    }
-
+    UserStatusInfo userStatusInfo = samDAO.getRegistrationInfo(authUser);
     // Affiliation and Role
-    Visa roleVisa = buildAffiliationAndRoleVisa(authUser, user);
+    Visa roleVisa = buildAffiliationAndRoleVisa(userStatusInfo, user);
 
     // Controlled Access Grants
     List<ApprovedDataset> approvedDatasets = datasetDAO.getApprovedDatasets(user.getUserId());
-    List<Visa> grantVisas = buildControlledAccessGrants(user, approvedDatasets);
+    List<Visa> grantVisas = buildControlledAccessGrants(userStatusInfo, approvedDatasets);
 
     List<Visa> allVisas = Stream.of(grantVisas, List.of(roleVisa)).flatMap(List::stream).toList();
-    return new PassportClaim(allVisas);
+    PassportClaim claim = new PassportClaim(allVisas);
+    logInfo("Generated PassportClaim for user: " + user.getEmail() + ": " + claim);
+    return claim;
   }
 
-  protected List<Visa> buildControlledAccessGrants(User user, List<ApprovedDataset> approvedDatasets) {
+  protected List<Visa> buildControlledAccessGrants(UserStatusInfo userStatusInfo, List<ApprovedDataset> approvedDatasets) {
     return approvedDatasets
         .stream()
         // A user can be approved for a dataset on multiple DARs so filter them here.
@@ -84,7 +72,7 @@ public class PassportService {
           Instant now = Instant.now();
           Long iat = now.toEpochMilli();
           Long exp = now.plusSeconds(3600).toEpochMilli();
-          return new Visa(ISS, user.getEmail(), iat, exp, claim);
+          return new Visa(ISS, userStatusInfo.getUserSubjectId(), iat, exp, claim);
         }).toList();
   }
 
@@ -93,27 +81,12 @@ public class PassportService {
     return t -> seen.add(keyExtractor.apply(t));
   }
 
-  // TODO: We need the Sam user id which is not collected in any current Sam call that
-  // Consent makes ... look into whatever endpoint provides that info and build it out
-  // so we can create a valid SUB value.
-  // Look at https://sam.dsde-dev.broadinstitute.org/api/users/v2/self
-  // which gives us a little more info:
-  // {
-  //  "allowed": <boolean>,
-  //  "azureB2CId": "<azure UUID>",
-  //  "createdAt": "1970-01-01T00:00:00Z",
-  //  "email": "<email>",
-  //  "googleSubjectId": "<google id>",
-  //  "id": "103740509117808340318", <--- I think this is the new Sam ID
-  //  "updatedAt": "2023-09-20T16:26:40.930521Z"
-  //}
-  protected Visa buildAffiliationAndRoleVisa(AuthUser authUser, User user) throws Exception {
-    UserStatusDiagnostics samUser = samDAO.getSelfDiagnostics(authUser);
+  protected Visa buildAffiliationAndRoleVisa(UserStatusInfo userStatusInfo, User user) {
     VisaClaimType affiliationAndRole = new AffiliationAndRole(user);
     VisaClaim affiliationClaim = new VisaClaim(affiliationAndRole.type(), affiliationAndRole.asserted(), affiliationAndRole.value(), affiliationAndRole.source(), affiliationAndRole.by());
     Instant now = Instant.now();
     Long iat = now.toEpochMilli();
     Long exp = now.plusSeconds(3600).toEpochMilli();
-    return new Visa(ISS, user.getEmail(), iat, exp, affiliationClaim);
+    return new Visa(ISS, userStatusInfo.getUserSubjectId(), iat, exp, affiliationClaim);
   }
 }
