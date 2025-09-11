b841a553539c1a35b9c8a4cb0b5e7d1c4ee94f4c
rushtong <grushton@broadinstitute.org>
2025-09-11 12:16:57 -0400

    feat: import fix

--- src/main/java/org/broadinstitute/consent/http/models/User.java
+++ src/main/java/org/broadinstitute/consent/http/models/User.java
@@ -7,7 +7,6 @@ import java.beans.Transient;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
-import java.util.EnumSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
