7680d1e7009ba6d49d8c0a384b733a7b190c318b
Florian Boulnois <fboulnoi@broadinstitute.org>
2025-09-22 08:50:43 -0400

    feat: validate query window size

--- src/test/java/org/broadinstitute/consent/http/service/ElasticSearchServiceTest.java
+++ src/test/java/org/broadinstitute/consent/http/service/ElasticSearchServiceTest.java
@@ -710,4 +710,87 @@ class ElasticSearchServiceTest extends AbstractTestHelper {
     verify(datasetServiceDAO, never()).updateDatasetIndex(any(), any(), any());
   }
 
+  @Test
+  void testInvalidResultWindow_ValidQueryWithinLimits() {
+    String query = "{\"size\": 100, \"from\": 0}";
+    assertFalse(service.invalidResultWindow(query));
+  }
+
+  @Test
+  void testInvalidResultWindow_DoubleValuesThrowClassCastException() {
+    String query = "{\"size\": 100.0, \"from\": 0.0}";
+    assertTrue(service.invalidResultWindow(query));
+  }
+
+  @Test
+  void testInvalidResultWindow_StringValuesThrowClassCastException() {
+    String query = "{\"size\": \"100\", \"from\": \"0\"}";
+    assertTrue(service.invalidResultWindow(query));
+  }
+
+  @Test
+  void testInvalidResultWindow_ValidQueryAtLimit() {
+    String query = "{\"size\": 5000, \"from\": 5000}";
+    assertFalse(service.invalidResultWindow(query));
+  }
+
+  @Test
+  void testInvalidResultWindow_ExceedsMaxResultWindow() {
+    String query = "{\"size\": 5000, \"from\": 5001}";
+    assertTrue(service.invalidResultWindow(query));
+  }
+
+  @Test
+  void testInvalidResultWindow_ExceedsMaxResultWindowLargeValues() {
+    String query = "{\"size\": 10000, \"from\": 1}";
+    assertTrue(service.invalidResultWindow(query));
+  }
+
+  @Test
+  void testInvalidResultWindow_DefaultValues() {
+    String query = "{}";
+    assertFalse(service.invalidResultWindow(query));
+  }
+
+  @Test
+  void testInvalidResultWindow_OnlySizeProvided() {
+    String query = "{\"size\": 50}";
+    assertFalse(service.invalidResultWindow(query));
+  }
+
+  @Test
+  void testInvalidResultWindow_OnlyFromProvided() {
+    String query = "{\"from\": 100}";
+    assertFalse(service.invalidResultWindow(query));
+  }
+
+  @Test
+  void testInvalidResultWindow_InvalidJsonFormat() {
+    String query = "{invalid json}";
+    assertTrue(service.invalidResultWindow(query));
+  }
+
+  @Test
+  void testInvalidResultWindow_NullQuery() {
+    assertTrue(service.invalidResultWindow(null));
+  }
+
+  @Test
+  void testInvalidResultWindow_EmptyString() {
+    String query = "";
+    assertTrue(service.invalidResultWindow(query));
+  }
+
+  @Test
+  void testInvalidResultWindow_NonJsonString() {
+    String query = "not a json string";
+    assertTrue(service.invalidResultWindow(query));
+  }
+
+  @Test
+  void testInvalidResultWindow_ExtraFieldsInQuery() {
+    String query = "{\"size\": 100, \"from\": 50, \"query\": {\"match_all\": {}}}";
+    assertFalse(service.invalidResultWindow(query));
+  }
+
 }
