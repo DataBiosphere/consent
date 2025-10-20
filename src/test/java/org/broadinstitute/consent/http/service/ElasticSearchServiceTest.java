bd7f6d5a06bee578fd723b7df7bc439b4c5516fc
Florian Boulnois <fboulnoi@broadinstitute.org>
2025-10-20 16:18:06 -0400

    feat: persist assets into elasticsearch

--- src/test/java/org/broadinstitute/consent/http/service/ElasticSearchServiceTest.java
+++ src/test/java/org/broadinstitute/consent/http/service/ElasticSearchServiceTest.java
@@ -31,6 +31,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.Map;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpVersion;
 import org.apache.http.StatusLine;
@@ -340,6 +341,33 @@ class ElasticSearchServiceTest extends AbstractTestHelper {
     assertEquals(custodianProp.get().getValue().toString(), termCustodians);
     assertEquals(datasetRecord.study.getPublicVisibility(), term.getStudy().getPublicVisibility());
     assertEquals(datasetRecord.study.getDataTypes(), term.getStudy().getDataTypes());
+  }
+
+  @Test
+  void testToDatasetTerm_StudyAssets() {
+    DatasetRecord datasetRecord = createDatasetRecord();
+    Map<String, Object> assetsMap = Map.of(
+        "workspace_id", "c7b96ac5-5568-441c-a3f4-2e82e45e3e6d",
+        "name", "Test Workspace",
+        "platform", "Terra"
+    );
+    String assetsJson = GsonUtil.getInstance().toJson(assetsMap);
+    StudyProperty assetsProp = new StudyProperty();
+    assetsProp.setStudyId(datasetRecord.study.getStudyId());
+    assetsProp.setKey("assets");
+    assetsProp.setType(PropertyType.Json);
+    assetsProp.setValue(GsonUtil.getInstance().fromJson(assetsJson, Object.class));
+    datasetRecord.study.addProperty(assetsProp);
+    
+    when(userDao.findUserById(datasetRecord.createUser.getUserId())).thenReturn(
+        datasetRecord.createUser);
+    when(userDao.findUserById(datasetRecord.updateUser.getUserId())).thenReturn(
+        datasetRecord.updateUser);
+    when(dacDAO.findById(any())).thenReturn(datasetRecord.dac);
+
+    DatasetTerm term = service.toDatasetTerm(datasetRecord.dataset);
+    
+    assertEquals(assetsMap, term.getStudy().getAssets());
   }
 
   @Test
