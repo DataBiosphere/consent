154cd783acd202a47c52b317f7064c3910c77ba2
Florian Boulnois <fboulnoi@broadinstitute.org>
2025-10-21 13:16:44 -0400

    fix: set correct object for elasticsearch asset map

--- src/test/java/org/broadinstitute/consent/http/service/ElasticSearchServiceTest.java
+++ src/test/java/org/broadinstitute/consent/http/service/ElasticSearchServiceTest.java
@@ -346,10 +346,15 @@ class ElasticSearchServiceTest extends AbstractTestHelper {
   @Test
   void testToDatasetTerm_StudyAssets() {
     DatasetRecord datasetRecord = createDatasetRecord();
-    Map<String, Object> assetsMap = Map.of(
-        "workspace_id", "c7b96ac5-5568-441c-a3f4-2e82e45e3e6d",
-        "name", "Test Workspace",
-        "platform", "Terra"
+    Map<String, Object> assetsMap = Map.of("workspaces", List.of(Map.of(
+      "workspace_id", "c7b96ac5-5568-441c-a3f4-2e82e45e3e6d",
+      "name", "Cardiometabolic GWAS Analysis Workspace",
+      "platform", "Terra",
+      "authors", List.of(
+          Map.of("user", "john", "email", "john@example.com"),
+          Map.of("user", "emma", "email", "emma@example.com")
+      ),
+      "tags", List.of("GWAS", "Terra", "featured")))
     );
     String assetsJson = GsonUtil.getInstance().toJson(assetsMap);
     StudyProperty assetsProp = new StudyProperty();
