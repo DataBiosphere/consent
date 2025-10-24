af52b0bdca95207a64e6cdac755cf6ba6c7048b2
Kevin Marete <kevinmmarete@gmail.com>
2025-10-24 00:05:37 -0400

    Updated Presentation

--- src/main/java/org/broadinstitute/consent/http/models/Presentation.java
+++ src/main/java/org/broadinstitute/consent/http/models/Presentation.java
@@ -1,7 +1,20 @@
 package org.broadinstitute.consent.http.models;
 
-public record Presentation(String title, String link, String date, String authors,
+import java.util.List;
+
+public record Presentation(String title,
+                           String url,
+                           String date,
+                           String authors,
                            String datasetCitation,
-                           Boolean citation) {
+                           Boolean citation,
+                           String presentationId,
+                           String studyId,
+                           Presenter presenter,
+                           String event,
+                           String location,
+                           String format,
+                           String access,
+                           List<String> tags) {
 
 }
