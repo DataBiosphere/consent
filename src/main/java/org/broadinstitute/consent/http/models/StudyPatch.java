fe585c1623dfed4ea81351b0a3d819972cf8ee93
rushtong <grushton@broadinstitute.org>
2025-12-03 09:16:06 -0500

    address sonar warnings

--- src/main/java/org/broadinstitute/consent/http/models/StudyPatch.java
+++ src/main/java/org/broadinstitute/consent/http/models/StudyPatch.java
@@ -21,65 +21,88 @@ public record StudyPatch(
     String alternativeDataSharingPlanTargetPublicReleaseDate,
     Boolean publicVisibility) {
 
+  public static final String STUDY_TYPE = "studyType";
+  public static final String PHENOTYPE_INDICATION = "phenotypeIndication";
+  public static final String SPECIES_TYPE = "species";
+  public static final String DATA_CUSTODIAN_EMAIL = "dataCustodianEmail";
+  public static final String ALTERNATIVE_DATA_SHARING_PLAN_TARGET_DELIVERY_DATE = "alternativeDataSharingPlanTargetDeliveryDate";
+  public static final String ALTERNATIVE_DATA_SHARING_PLAN_TARGET_PUBLIC_RELEASE_DATE = "alternativeDataSharingPlanTargetPublicReleaseDate";
+
   // Utility method to determine if any patch values differ from the provided Study entity
   public boolean isPatchable(Study study) {
-    if (name() != null && !name().equals(study.getName()) && !name().isBlank()) {
-      return true;
-    }
+    List<Boolean> checks = new ArrayList<>();
+    checks.add(checkName(study));
+    checks.add(checkStudyType(study));
+    checks.add(checkDescription(study));
+    checks.add(checkDataTypes(study));
+    checks.add(checkPhenotypeIndication(study));
+    checks.add(checkSpecies(study));
+    checks.add(checkPiName(study));
+    checks.add(checkDataCustodians(study));
+    checks.add(checkTargetDate(study));
+    checks.add(checkTargetReleaseDate(study));
+    checks.add(checkPublicVisibility(study));
+    return checks.stream().anyMatch(Boolean::booleanValue);
+  }
 
+  private boolean checkName(Study study) {
+    return name() != null && !name().equals(study.getName()) && !name().isBlank();
+  }
+
+  private boolean checkStudyType(Study study) {
     Optional<StudyProperty> studyTypeProp =
-        study.getProperties().stream().filter(p -> p.getKey().equals("studyType")).findFirst();
+        study.getProperties().stream().filter(p -> p.getKey().equals(STUDY_TYPE)).findFirst();
     if (studyType() != null) {
-      if (studyTypeProp.isEmpty()) {
-        return true;
-      }
-      if (!studyType().value().equals(studyTypeProp.get().getValue())) {
-        return true;
-      }
+      return studyTypeProp
+          .map(studyProperty -> !studyType().value().equals(studyProperty.getValue()))
+          .orElse(true);
     }
+    return false;
+  }
 
-    if (description() != null
+  private boolean checkDescription(Study study) {
+    return description() != null
         && !description().equals(study.getDescription())
-        && !description().isBlank()) {
-      return true;
-    }
+        && !description().isBlank();
+  }
 
-    if (dataTypes() != null
-        && !CollectionUtils.isEqualCollection(dataTypes(), study.getDataTypes())) {
-      return true;
-    }
+  private boolean checkDataTypes(Study study) {
+    return dataTypes() != null
+        && !CollectionUtils.isEqualCollection(dataTypes(), study.getDataTypes());
+  }
 
+  private boolean checkPhenotypeIndication(Study study) {
     Optional<StudyProperty> phenoProp =
         study.getProperties().stream()
-            .filter(p -> p.getKey().equals("phenotypeIndication"))
+            .filter(p -> p.getKey().equals(PHENOTYPE_INDICATION))
             .findFirst();
     if (phenotypeIndication() != null) {
-      if (phenoProp.isEmpty()) {
-        return true;
-      }
-      if (!phenotypeIndication().equals(phenoProp.get().getValue())) {
-        return true;
-      }
+      return phenoProp
+          .map(studyProperty -> !phenotypeIndication().equals(studyProperty.getValue()))
+          .orElse(true);
     }
+    return false;
+  }
 
+  private boolean checkSpecies(Study study) {
     Optional<StudyProperty> speciesProp =
-        study.getProperties().stream().filter(p -> p.getKey().equals("species")).findFirst();
+        study.getProperties().stream().filter(p -> p.getKey().equals(SPECIES_TYPE)).findFirst();
     if (species() != null) {
-      if (speciesProp.isEmpty()) {
-        return true;
-      }
-      if (!species().equals(speciesProp.get().getValue())) {
-        return true;
-      }
+      return speciesProp
+          .map(studyProperty -> !species().equals(studyProperty.getValue()))
+          .orElse(true);
     }
+    return false;
+  }
 
-    if (piName() != null && !piName().equals(study.getPiName()) && !piName().isBlank()) {
-      return true;
-    }
+  private boolean checkPiName(Study study) {
+    return piName() != null && !piName().equals(study.getPiName()) && !piName().isBlank();
+  }
 
+  private boolean checkDataCustodians(Study study) {
     Optional<StudyProperty> custodiansProp =
         study.getProperties().stream()
-            .filter(p -> p.getKey().equals("dataCustodianEmail"))
+            .filter(p -> p.getKey().equals(DATA_CUSTODIAN_EMAIL))
             .findFirst();
     if (dataCustodianEmail() != null) {
       if (custodiansProp.isEmpty()) {
@@ -90,38 +113,43 @@ public record StudyPatch(
               .fromJson(
                   custodiansProp.get().getValue().toString(),
                   new TypeToken<ArrayList<String>>() {}.getType());
-      if (!CollectionUtils.isEqualCollection(dataCustodianEmail(), existingCustodians)) {
-        return true;
-      }
+      return !CollectionUtils.isEqualCollection(dataCustodianEmail(), existingCustodians);
     }
+    return false;
+  }
 
+  private boolean checkTargetDate(Study study) {
     Optional<StudyProperty> targetDateProp =
         study.getProperties().stream()
-            .filter(p -> p.getKey().equals("alternativeDataSharingPlanTargetDeliveryDate"))
+            .filter(p -> p.getKey().equals(ALTERNATIVE_DATA_SHARING_PLAN_TARGET_DELIVERY_DATE))
             .findFirst();
     if (alternativeDataSharingPlanTargetDeliveryDate() != null) {
-      if (targetDateProp.isEmpty()) {
-        return true;
-      }
-      if (!alternativeDataSharingPlanTargetDeliveryDate().equals(targetDateProp.get().getValue())) {
-        return true;
-      }
+      return targetDateProp
+          .map(
+              studyProperty ->
+                  !alternativeDataSharingPlanTargetDeliveryDate().equals(studyProperty.getValue()))
+          .orElse(true);
     }
+    return false;
+  }
 
+  private boolean checkTargetReleaseDate(Study study) {
     Optional<StudyProperty> targetReleaseProp =
         study.getProperties().stream()
-            .filter(p -> p.getKey().equals("alternativeDataSharingPlanTargetPublicReleaseDate"))
+            .filter(p -> p.getKey().equals(ALTERNATIVE_DATA_SHARING_PLAN_TARGET_PUBLIC_RELEASE_DATE))
             .findFirst();
     if (alternativeDataSharingPlanTargetPublicReleaseDate() != null) {
-      if (targetReleaseProp.isEmpty()) {
-        return true;
-      }
-      if (!alternativeDataSharingPlanTargetPublicReleaseDate()
-          .equals(targetReleaseProp.get().getValue())) {
-        return true;
-      }
+      return targetReleaseProp
+          .map(
+              studyProperty ->
+                  !alternativeDataSharingPlanTargetPublicReleaseDate()
+                      .equals(studyProperty.getValue()))
+          .orElse(true);
     }
+    return false;
+  }
 
+  private boolean checkPublicVisibility(Study study) {
     return publicVisibility() != null && !publicVisibility().equals(study.getPublicVisibility());
   }
 }
