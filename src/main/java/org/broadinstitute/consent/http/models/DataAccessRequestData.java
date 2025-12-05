0f40cc64d66ff5c0efe239d3ea6e68eb5dd522d4
Kevin Marete <kevinmmarete@gmail.com>
2025-12-05 13:11:04 -0500

    Revert "Use GsonUtil with deserializers"
    This reverts commit 513297e6b2e4404d9253734319f6f9d69b564838.

--- src/main/java/org/broadinstitute/consent/http/models/DataAccessRequestData.java
+++ src/main/java/org/broadinstitute/consent/http/models/DataAccessRequestData.java
@@ -11,7 +11,6 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import org.broadinstitute.consent.http.util.gson.GsonUtil;
 
 @JsonInclude(Include.NON_NULL)
 public class DataAccessRequestData {
@@ -169,8 +168,7 @@ public class DataAccessRequestData {
   }
 
   public static DataAccessRequestData fromString(String jsonString) {
-    DataAccessRequestData data =
-        GsonUtil.getInstance().fromJson(jsonString, DataAccessRequestData.class);
+    DataAccessRequestData data = new Gson().fromJson(jsonString, DataAccessRequestData.class);
     validateOntologyEntries(data);
     return data;
   }
