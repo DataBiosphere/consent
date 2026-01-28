8e2caf4e88f194c3ed86f50d96766442335dae5d
rushtong <grushton@broadinstitute.org>
2026-01-28 16:59:06 -0500

    feat: tests

---
+++ src/test/java/org/broadinstitute/consent/http/resources/OntologyResourceTest.java
@@ -0,0 +1,118 @@
+package org.broadinstitute.consent.http.resources;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
+
+import com.google.api.client.http.HttpStatusCodes;
+import com.google.gson.JsonObject;
+import jakarta.ws.rs.core.Response;
+import java.util.Collections;
+import java.util.List;
+import org.broadinstitute.consent.http.AbstractTestHelper;
+import org.broadinstitute.consent.http.enumeration.OntologyType;
+import org.broadinstitute.consent.http.models.AuthUser;
+import org.broadinstitute.consent.http.models.DuosUser;
+import org.broadinstitute.consent.http.models.User;
+import org.broadinstitute.consent.http.service.OntologyService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class OntologyResourceTest extends AbstractTestHelper {
+
+  @Mock private OntologyService ontologyService;
+
+  private final AuthUser authUser = new AuthUser("test@test.com");
+
+  private OntologyResource resource;
+
+  @Test
+  void testIndexOntologyTermsSuccess() throws Exception {
+    User admin = new User();
+    admin.setAdminRole();
+    DuosUser duosUser = new DuosUser(authUser, admin);
+
+    doNothing().when(ontologyService).indexOntology(any(), any(), any());
+
+    resource = new OntologyResource(ontologyService);
+    try (Response response = resource.indexOntologyTerms(duosUser, OntologyType.DOID.name())) {
+      assertEquals(HttpStatusCodes.STATUS_CODE_OK, response.getStatus());
+    }
+  }
+
+  @Test
+  void testIndexOntologyTermsInvalidType() {
+    User admin = new User();
+    admin.setAdminRole();
+    DuosUser duosUser = new DuosUser(authUser, admin);
+
+    resource = new OntologyResource(ontologyService);
+    try (Response response = resource.indexOntologyTerms(duosUser, "INVALID_TYPE")) {
+      assertEquals(HttpStatusCodes.STATUS_CODE_BAD_REQUEST, response.getStatus());
+    }
+  }
+
+  @Test
+  void testIndexOntologyTermsException() throws Exception {
+    User admin = new User();
+    admin.setAdminRole();
+    DuosUser duosUser = new DuosUser(authUser, admin);
+
+    doThrow(new RuntimeException()).when(ontologyService).indexOntology(any(), any(), any());
+
+    resource = new OntologyResource(ontologyService);
+    try (Response response = resource.indexOntologyTerms(duosUser, OntologyType.DOID.name())) {
+      assertEquals(HttpStatusCodes.STATUS_CODE_SERVER_ERROR, response.getStatus());
+    }
+  }
+
+  @Test
+  void testSearchTermSuccess() {
+    JsonObject term1 = new JsonObject();
+    term1.addProperty("id", "DOID_1234");
+    JsonObject term2 = new JsonObject();
+    term2.addProperty("id", "DOID_5678");
+
+    when(ontologyService.findByTermIds("DOID_1234,DOID_5678")).thenReturn(List.of(term1, term2));
+
+    resource = new OntologyResource(ontologyService);
+    try (Response response = resource.searchTerm("DOID_1234,DOID_5678")) {
+      assertEquals(HttpStatusCodes.STATUS_CODE_OK, response.getStatus());
+    }
+  }
+
+  @Test
+  void testSearchTermNotFound() {
+    when(ontologyService.findByTermIds(any())).thenReturn(Collections.emptyList());
+
+    resource = new OntologyResource(ontologyService);
+    try (Response response = resource.searchTerm("INVALID_0000")) {
+      assertEquals(HttpStatusCodes.STATUS_CODE_NOT_FOUND, response.getStatus());
+    }
+  }
+
+  @Test
+  void testSearchTermNullResult() {
+    when(ontologyService.findByTermIds(any())).thenReturn(null);
+
+    resource = new OntologyResource(ontologyService);
+    try (Response response = resource.searchTerm("DOID_1234")) {
+      assertEquals(HttpStatusCodes.STATUS_CODE_NOT_FOUND, response.getStatus());
+    }
+  }
+
+  @Test
+  void testSearchTermException() {
+    when(ontologyService.findByTermIds(any())).thenThrow(new RuntimeException());
+
+    resource = new OntologyResource(ontologyService);
+    try (Response response = resource.searchTerm("DOID_1234")) {
+      assertEquals(HttpStatusCodes.STATUS_CODE_SERVER_ERROR, response.getStatus());
+    }
+  }
+}
