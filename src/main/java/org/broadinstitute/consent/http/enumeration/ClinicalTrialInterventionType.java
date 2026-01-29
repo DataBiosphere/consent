9f80008276d08ca82f26559cbba4378c45a29f6c
Kevin Marete <kevinmmarete@gmail.com>
2026-01-29 15:43:30 -0500

    Added enums types

---
+++ src/main/java/org/broadinstitute/consent/http/enumeration/ClinicalTrialInterventionType.java
@@ -0,0 +1,15 @@
+package org.broadinstitute.consent.http.enumeration;
+
+public enum ClinicalTrialInterventionType {
+  BEHAVIORAL,
+  BIOLOGICAL,
+  COMBINATION_PRODUCT,
+  DEVICE,
+  DIAGNOSTIC_TEST,
+  DIETARY_SUPPLEMENT,
+  DRUG,
+  GENETIC,
+  PROCEDURE,
+  RADIATION,
+  OTHER
+}
---
+++ src/main/java/org/broadinstitute/consent/http/enumeration/ClinicalTrialPhase.java
@@ -0,0 +1,10 @@
+package org.broadinstitute.consent.http.enumeration;
+
+public enum ClinicalTrialPhase {
+  NA,
+  EARLY_PHASE1,
+  PHASE1,
+  PHASE2,
+  PHASE3,
+  PHASE4
+}
