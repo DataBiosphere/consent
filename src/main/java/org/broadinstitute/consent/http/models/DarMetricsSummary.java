5b2b966beca7238f854561fe3925f76101715bf6
rushtong <grushton@broadinstitute.org>
2026-02-25 10:33:33 -0500

    PR feedback

--- src/main/java/org/broadinstitute/consent/http/models/DarMetricsSummary.java
+++ src/main/java/org/broadinstitute/consent/http/models/DarMetricsSummary.java
@@ -1,9 +1,6 @@
 package org.broadinstitute.consent.http.models;
 
 import java.sql.Timestamp;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
 
 public record DarMetricsSummary(
     Timestamp updateDate,
@@ -20,17 +17,6 @@ public record DarMetricsSummary(
         dar != null ? dar.getDarCode() : null,
         dar != null && dar.getData() != null ? dar.getData().getNonTechRus() : null,
         dar != null ? dar.getReferenceId() : null,
-        computeExpired(dar));
-  }
-
-  private static Boolean computeExpired(DataAccessRequest dar) {
-    // If the DAR or its submission date is null, we consider it expired for metrics purposes
-    if (dar == null || dar.getSubmissionDate() == null) {
-      return true;
-    }
-    LocalDateTime oneYearAgo = LocalDateTime.now().minusYears(1);
-    ZonedDateTime zonedDateTime = oneYearAgo.atZone(ZoneId.systemDefault());
-    Timestamp lastYear = Timestamp.from(zonedDateTime.toInstant());
-    return dar.getSubmissionDate().before(lastYear);
+        dar == null || dar.getSubmissionDate() == null || dar.getExpired());
   }
 }
