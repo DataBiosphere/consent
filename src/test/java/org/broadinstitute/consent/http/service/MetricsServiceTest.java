18f10976fbe3dbea9ff27e61d96ee8073a4dfb18
rushtong <grushton@broadinstitute.org>
2026-02-26 13:58:56 -0500

    test fixes

--- src/test/java/org/broadinstitute/consent/http/service/MetricsServiceTest.java
+++ src/test/java/org/broadinstitute/consent/http/service/MetricsServiceTest.java
@@ -1,15 +1,24 @@
 package org.broadinstitute.consent.http.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import jakarta.ws.rs.NotFoundException;
 import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 import org.broadinstitute.consent.http.AbstractTestHelper;
 import org.broadinstitute.consent.http.db.DataAccessRequestDAO;
 import org.broadinstitute.consent.http.db.DatasetDAO;
+import org.broadinstitute.consent.http.models.DarMetricsSummary;
 import org.broadinstitute.consent.http.models.DataAccessRequest;
 import org.broadinstitute.consent.http.models.DataAccessRequestData;
 import org.broadinstitute.consent.http.models.Dataset;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -26,6 +35,30 @@ class MetricsServiceTest extends AbstractTestHelper {
   @BeforeEach
   void initService() {
     service = new MetricsService(dataSetDAO, darDAO);
+  }
+
+  @Test
+  void testGenerateDarSummaries() {
+    DataAccessRequest dar = generateDar();
+    Dataset dataset = generateDataset();
+
+    when(dataSetDAO.findDatasetById(dataset.getDatasetId())).thenReturn(dataset);
+    when(darDAO.findSummaryMetricApprovedDARsByDatasetIdIncludesExpired(any()))
+        .thenReturn(List.of(dar));
+
+    List<DarMetricsSummary> metrics = service.generateDarSummaries(dataset.getDatasetId());
+
+    assertEquals(dar.getData().getProjectTitle(), metrics.getFirst().projectTitle());
+    assertEquals(dar.getDarCode(), metrics.getFirst().darCode());
+    verify(dataSetDAO).findDatasetById(dataset.getDatasetId());
+    verify(darDAO).findSummaryMetricApprovedDARsByDatasetIdIncludesExpired(dataset.getDatasetId());
+  }
+
+  @Test
+  void testGenerateDatasetMetricsNotFound() {
+    when(dataSetDAO.findDatasetById(any())).thenReturn(null);
+
+    assertThrows(NotFoundException.class, () -> service.generateDarSummaries(1));
   }
 
   private DataAccessRequest generateDar() {
