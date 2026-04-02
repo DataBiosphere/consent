8b0d2fdebb7afc777b6c85974cc6ecad0a2855bd
rushtong <grushton@broadinstitute.org>
2026-04-02 10:16:52 -0400

    feat: co-pilot feedback

--- src/main/java/org/broadinstitute/consent/http/db/mapper/DaaAuditMapper.java
+++ src/main/java/org/broadinstitute/consent/http/db/mapper/DaaAuditMapper.java
@@ -10,10 +10,11 @@ import org.jdbi.v3.core.statement.StatementContext;
 public class DaaAuditMapper implements RowMapper<DaaAudit>, RowMapperHelper {
   @Override
   public DaaAudit map(ResultSet rs, StatementContext ctx) throws SQLException {
+    int dacId = hasColumn(rs, "dac_id") ? rs.getInt("dac_id") : 0;
     return new DaaAudit(
         rs.getLong("id"),
         rs.getInt("daa_id"),
-        rs.getInt("dac_id"),
+        dacId > 0 ? dacId : null,
         rs.getInt("user_id"),
         AuditActions.valueOf(rs.getString("action").toUpperCase()),
         rs.getTimestamp("action_date").toInstant());
