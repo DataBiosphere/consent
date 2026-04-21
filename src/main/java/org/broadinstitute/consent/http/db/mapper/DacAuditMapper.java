39d0d7ef6d309114a6881349a718ded9ecfd4e45
rushtong <grushton@broadinstitute.org>
2026-04-21 11:59:24 -0400

    feat: copilot PR feedback

--- src/main/java/org/broadinstitute/consent/http/db/mapper/DacAuditMapper.java
+++ src/main/java/org/broadinstitute/consent/http/db/mapper/DacAuditMapper.java
@@ -10,14 +10,12 @@ import org.jdbi.v3.core.statement.StatementContext;
 public class DacAuditMapper implements RowMapper<DacAudit>, RowMapperHelper {
   @Override
   public DacAudit map(ResultSet rs, StatementContext ctx) throws SQLException {
-    int affectedUserId = hasColumn(rs, "affected_user_id") ? rs.getInt("affected_user_id") : 0;
-    int roleId = hasColumn(rs, "role_id") ? rs.getInt("role_id") : 0;
     return new DacAudit(
         rs.getLong("id"),
         rs.getInt("dac_id"),
         rs.getInt("user_id"),
-        affectedUserId > 0 ? affectedUserId : null,
-        roleId > 0 ? roleId : null,
+        hasNonZeroColumn(rs, "affected_user_id") ? rs.getInt("affected_user_id") : null,
+        hasNonZeroColumn(rs, "role_id") ? rs.getInt("role_id") : null,
         AuditActions.valueOf(rs.getString("action").toUpperCase()),
         rs.getTimestamp("action_date").toInstant());
   }
