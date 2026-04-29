29d5340d0cf53030b59c86ce968dda46e348ee62
rushtong <grushton@broadinstitute.org>
2026-04-29 16:15:07 -0400

    feat: convert to record class and simplified insert

--- src/main/java/org/broadinstitute/consent/http/db/MailMessageDAO.java
+++ src/main/java/org/broadinstitute/consent/http/db/MailMessageDAO.java
@@ -1,39 +1,29 @@
 package org.broadinstitute.consent.http.db;
 
-import java.time.Instant;
 import java.util.Date;
 import java.util.List;
-import javax.annotation.Nullable;
 import org.broadinstitute.consent.http.db.mapper.MailMessageMapper;
 import org.broadinstitute.consent.http.models.mail.MailMessage;
 import org.jdbi.v3.sqlobject.config.RegisterRowMapper;
 import org.jdbi.v3.sqlobject.customizer.Bind;
-import org.jdbi.v3.sqlobject.statement.GetGeneratedKeys;
+import org.jdbi.v3.sqlobject.customizer.BindMethods;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
-import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 import org.jdbi.v3.sqlobject.transaction.Transactional;
 
 @RegisterRowMapper(MailMessageMapper.class)
 public interface MailMessageDAO extends Transactional<MailMessageDAO> {
 
-  @SqlUpdate(
+  @SqlQuery(
       """
-      INSERT INTO email_entity
+      WITH insterted_row AS (
+        INSERT INTO email_entity
           (entity_reference_id, vote_id, user_id, email_type, date_sent, email_text, sendgrid_response, sendgrid_status, create_date)
-      VALUES
-          (:entityReferenceId, :voteId, :userId, :emailType, :dateSent, :emailText, :sendGridResponse, :sendGridStatus, :createDate)
+        VALUES
+          (:entityReferenceId, :voteId, :userId, :emailType, :dateSent, :emailText, :sendgridResponse, :sendgridStatus, :createDate)
+        RETURNING *)
+      SELECT * FROM insterted_row
       """)
-  @GetGeneratedKeys
-  Integer insert(
-      @Nullable @Bind("entityReferenceId") String entityReferenceId,
-      @Nullable @Bind("voteId") Integer voteId,
-      @Bind("userId") Integer userId,
-      @Bind("emailType") Integer emailType,
-      @Nullable @Bind("dateSent") Instant dateSent,
-      @Bind("emailText") String emailText,
-      @Nullable @Bind("sendGridResponse") String sendGridResponse,
-      @Nullable @Bind("sendGridStatus") Integer sendGridStatus,
-      @Bind("createDate") Instant createDate);
+  MailMessage insert(@BindMethods MailMessage mail);
 
   @SqlQuery(
       """
--- src/main/java/org/broadinstitute/consent/http/models/mail/MailMessage.java
+++ src/main/java/org/broadinstitute/consent/http/models/mail/MailMessage.java
@@ -2,119 +2,14 @@ package org.broadinstitute.consent.http.models.mail;
 
 import java.util.Date;
 
-public class MailMessage {
-
-  private String entityReferenceId;
-  private Integer emailId;
-  private Integer voteId;
-  private Integer userId;
-  private Integer emailType;
-  private Date dateSent;
-  private String emailText;
-  private String sendgridResponse;
-  private Integer sendgridStatus;
-  private Date createDate;
-
-  public MailMessage(
-      String entityReferenceId,
-      Integer emailId,
-      Integer voteId,
-      Integer userId,
-      Integer emailType,
-      Date dateSent,
-      String emailText,
-      String sendgridResponse,
-      Integer sendgridStatus,
-      Date createDate) {
-    this.entityReferenceId = entityReferenceId;
-    this.emailId = emailId;
-    this.voteId = voteId;
-    this.userId = userId;
-    this.emailType = emailType;
-    this.dateSent = dateSent;
-    this.emailText = emailText;
-    this.sendgridResponse = sendgridResponse;
-    this.sendgridStatus = sendgridStatus;
-    this.createDate = createDate;
-  }
-
-  public String getEntityReferenceId() {
-    return entityReferenceId;
-  }
-
-  public void setEntityReferenceId(String entityReferenceId) {
-    this.entityReferenceId = entityReferenceId;
-  }
-
-  public Integer getEmailId() {
-    return emailId;
-  }
-
-  public void setEmailId(Integer emailId) {
-    this.emailId = emailId;
-  }
-
-  public Integer getVoteId() {
-    return voteId;
-  }
-
-  public void setVoteId(Integer voteId) {
-    this.voteId = voteId;
-  }
-
-  public Integer getUserId() {
-    return userId;
-  }
-
-  public void setUserId(Integer userId) {
-    this.userId = userId;
-  }
-
-  public Integer getEmailType() {
-    return emailType;
-  }
-
-  public void setEmailType(Integer emailType) {
-    this.emailType = emailType;
-  }
-
-  public Date getDateSent() {
-    return dateSent;
-  }
-
-  public void setDateSent(Date dateSent) {
-    this.dateSent = dateSent;
-  }
-
-  public String getEmailText() {
-    return emailText;
-  }
-
-  public void setEmailText(String emailText) {
-    this.emailText = emailText;
-  }
-
-  public String getSendgridResponse() {
-    return sendgridResponse;
-  }
-
-  public void setSendgridResponse(String sendgridResponse) {
-    this.sendgridResponse = sendgridResponse;
-  }
-
-  public Integer getSendgridStatus() {
-    return sendgridStatus;
-  }
-
-  public void setSendgridStatus(Integer sendgridStatus) {
-    this.sendgridStatus = sendgridStatus;
-  }
-
-  public Date getCreateDate() {
-    return createDate;
-  }
-
-  public void setCreateDate(Date createDate) {
-    this.createDate = createDate;
-  }
-}
+public record MailMessage(
+    String entityReferenceId,
+    Integer emailId,
+    Integer voteId,
+    Integer userId,
+    Integer emailType,
+    Date dateSent,
+    String emailText,
+    String sendgridResponse,
+    Integer sendgridStatus,
+    Date createDate) {}
