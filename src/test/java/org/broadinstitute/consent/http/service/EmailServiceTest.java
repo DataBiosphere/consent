a758f9783763c7ee23c4ed97b01ab57fbd7a02e7
Gregory Rushton <grushton@broadinstitute.org>
2026-05-18 12:36:57 -0400

    test: simplify test condition

--- src/test/java/org/broadinstitute/consent/http/service/EmailServiceTest.java
+++ src/test/java/org/broadinstitute/consent/http/service/EmailServiceTest.java
@@ -2,6 +2,7 @@ package org.broadinstitute.consent.http.service;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
@@ -202,7 +203,7 @@ class EmailServiceTest extends AbstractTestHelper {
     var captor = ArgumentCaptor.forClass(Mail.class);
     verify(sendGridAPI).sendMessage(captor.capture(), eq(user.getEmail()));
     var mail = captor.getValue();
-    assertTrue(mail.getCategories() == null || mail.getCategories().isEmpty());
+    assertNull(mail.getCategories());
 
     verify(emailDAO)
         .insert(
