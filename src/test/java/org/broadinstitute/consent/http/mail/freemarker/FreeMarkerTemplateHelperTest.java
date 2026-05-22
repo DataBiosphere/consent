b4163681dc39de03439f811213f80ce79bef9e7e
Gregory Rushton <grushton@broadinstitute.org>
2026-05-22 08:24:56 -0400

    feat: template test

--- src/test/java/org/broadinstitute/consent/http/mail/freemarker/FreeMarkerTemplateHelperTest.java
+++ src/test/java/org/broadinstitute/consent/http/mail/freemarker/FreeMarkerTemplateHelperTest.java
@@ -1,12 +1,15 @@
 package org.broadinstitute.consent.http.mail.freemarker;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockConstruction;
 import static org.mockito.Mockito.when;
 
 import freemarker.template.Configuration;
 import freemarker.template.Template;
+import java.io.StringWriter;
+import java.util.Map;
 import org.broadinstitute.consent.http.configurations.FreeMarkerConfiguration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -40,5 +43,20 @@ class FreeMarkerTemplateHelperTest {
     var expectedPath = "freemarker/%s".formatted(templateName);
     when(mockedConfiguration.getTemplate(expectedPath)).thenReturn(template);
     assertEquals(template, freeMarkerTemplateHelper.getTemplate(templateName));
+  }
+
+  @Test
+  void renderedTemplatePreservesRawAsmUnsubscribeToken() throws Exception {
+    FreeMarkerConfiguration fmConfig = new FreeMarkerConfiguration();
+    fmConfig.setTemplateDirectory("freemarker");
+    fmConfig.setDefaultEncoding("UTF-8");
+
+    FreeMarkerTemplateHelper realTemplateHelper = new FreeMarkerTemplateHelper(fmConfig);
+    Template template = realTemplateHelper.getTemplate("dar-expired.ftl");
+    StringWriter out = new StringWriter();
+
+    template.process(Map.of("researcherName", "Synthetic User", "darCode", "DAR-123"), out);
+
+    assertTrue(out.toString().contains("href=\"<%asm_group_unsubscribe_raw_url%>\""));
   }
 }
