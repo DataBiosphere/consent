37d3cc88df6ab5d3fda1f1d20fb1fa2619c8739c
Gregory Rushton <rushtong@users.noreply.github.com>
2026-06-24 10:31:06 -0400

    DT-3598: Remove unused schema api, docs, and tests (#2949)

--- src/main/java/org/broadinstitute/consent/http/ConsentApplication.java
+++ src/main/java/org/broadinstitute/consent/http/ConsentApplication.java
@@ -72,7 +72,6 @@ import org.broadinstitute.consent.http.resources.OntologyResource;
 import org.broadinstitute.consent.http.resources.PassportResource;
 import org.broadinstitute.consent.http.resources.PublicFeatureFlagResource;
 import org.broadinstitute.consent.http.resources.SamResource;
-import org.broadinstitute.consent.http.resources.SchemaResource;
 import org.broadinstitute.consent.http.resources.StatusResource;
 import org.broadinstitute.consent.http.resources.StudyResource;
 import org.broadinstitute.consent.http.resources.SupportResource;
@@ -181,7 +180,6 @@ public class ConsentApplication extends Application<ConsentConfiguration> {
     env.jersey().register(injector.getInstance(OntologyResource.class));
     env.jersey().register(injector.getInstance(PassportResource.class));
     env.jersey().register(injector.getInstance(SamResource.class));
-    env.jersey().register(injector.getInstance(SchemaResource.class));
     env.jersey().register(injector.getInstance(SwaggerResource.class));
     env.jersey().register(injector.getInstance(StatusResource.class));
     env.jersey().register(injector.getInstance(StudyResource.class));
