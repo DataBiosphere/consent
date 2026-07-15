b7209d3efa4b9994a5ba8b0afc8bff00f1e341d3
Gregory Rushton <grushton@broadinstitute.org>
2026-07-15 07:29:40 -0400

    doc: query comments

--- src/main/java/org/broadinstitute/consent/http/db/ElectionDAO.java
+++ src/main/java/org/broadinstitute/consent/http/db/ElectionDAO.java
@@ -66,6 +66,7 @@ public interface ElectionDAO extends Transactional<ElectionDAO> {
       INNER JOIN vote v ON v.election_id = e.election_id AND
           CASE
               WHEN LOWER(e.election_type) = 'dataaccess' THEN 'final'
+              -- Note that `dataset` elections are deprecated but maintained for legacy elections
               WHEN LOWER(e.election_type) = 'dataset' THEN 'data_owner'
               ELSE 'chairperson'
           END = LOWER(v.type)
@@ -122,6 +123,7 @@ public interface ElectionDAO extends Transactional<ElectionDAO> {
            LEFT JOIN vote v ON e.election_id = v.election_id AND
                CASE
                WHEN LOWER(e.election_type) = 'dataaccess' THEN 'final'
+               -- Note that `dataset` elections are deprecated but maintained for legacy elections
                WHEN LOWER(e.election_type) = 'dataset' THEN 'data_owner'
                ELSE 'chairperson'
                END = LOWER(v.type)
